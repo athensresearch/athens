cf2b030d0222015967f8feb8eb6d2ee54219e52d
Aaron Schlesinger <arschles@gmail.com>
2018-02-25 16:19:15 -0800

    adding some doc

--- pkg/storage/rev_info.go
+++ pkg/storage/rev_info.go
@@ -4,6 +4,11 @@ import (
 	"time"
 )
 
+// RevInfo is json-encodable into the response body for
+// GET baseURL/module/@v/version.info
+//
+// This struct is taken directly from https://research.swtch.com/vgo-module
+// (see "Download Protocol" header)
 type RevInfo struct {
 	Version string    `json:"Version"` // version string
 	Name    string    `json:"Name"`    // complete ID in underlying repository
