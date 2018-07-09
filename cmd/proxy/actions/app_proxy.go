c3e3dee4d8b9d6d1740b8a7f4a6a5517339f05f4
Aaron Schlesinger <arschles@gmail.com>
2018-07-09 13:44:58 -0700

    WIP: Removing the CLI (#227)
    * Removing the CLI
    
    Still TODO:
    
    - Removing CLI references from docs
    
    * removing the payloads.Upload struct
    
    since it's not used anywhere

--- cmd/proxy/actions/app_proxy.go
+++ cmd/proxy/actions/app_proxy.go
@@ -12,7 +12,6 @@ func addProxyRoutes(app *buffalo.App, storage storage.Backend, mf *module.Filter
 	app.GET("/{module:.+}/@v/{version}.info", cacheMissHandler(versionInfoHandler(storage), app.Worker, mf))
 	app.GET("/{module:.+}/@v/{version}.mod", cacheMissHandler(versionModuleHandler(storage), app.Worker, mf))
 	app.GET("/{module:.+}/@v/{version}.zip", cacheMissHandler(versionZipHandler(storage), app.Worker, mf))
-	app.POST("/admin/upload/{module:[a-zA-Z./]+}/{version}", uploadHandler(storage))
 	app.POST("/admin/fetch/{module:[a-zA-Z./]+}/{owner}/{repo}/{ref}/{version}", fetchHandler(storage))
 	return nil
 }
--- pkg/payloads/upload.go
+++ pkg/payloads/upload.go
@@ -2,13 +2,6 @@ package payloads
 
 import "github.com/gomods/athens/pkg/eventlog"
 
-// Upload is used to send a module (zip and mod file) via POST request to the storage backend and save it there.
-type Upload struct {
-	Module []byte `json:"module"`
-	Zip    []byte `json:"zip"`
-	Info   []byte `json:"info"`
-}
-
 // Module is used by proxy to send info about cache miss to Olympus
 type Module struct {
 	Name    string `json:"name"`
