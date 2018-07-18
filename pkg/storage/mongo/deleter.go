016b99923c486095f34da4421945e17ad0333b89
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-07-18 13:23:58 -0400

    storage: return ErrVersionNotFound from mongo Deleter (#297)

--- pkg/storage/mongo/deleter.go
+++ pkg/storage/mongo/deleter.go
@@ -2,10 +2,17 @@ package mongo
 
 import (
 	"github.com/globalsign/mgo/bson"
+	"github.com/gomods/athens/pkg/storage"
 )
 
 // Delete removes a specific version of a module
-func (s *ModuleStore) Delete(module, vsn string) error {
+func (s *ModuleStore) Delete(module, version string) error {
+	if !s.Exists(module, version) {
+		return storage.ErrVersionNotFound{
+			Module:  module,
+			Version: version,
+		}
+	}
 	c := s.s.DB(s.d).C(s.c)
-	return c.Remove(bson.M{"module": module, "version": vsn})
+	return c.Remove(bson.M{"module": module, "version": version})
 }
