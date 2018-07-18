39e1a1758292e26678904765b5ed84927215902a
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-07-18 11:38:51 -0400

    storage: return ErrVersionNotFound from fs Deleter (#294)
    Add an optional extended description…

--- pkg/storage/deleter.go
+++ pkg/storage/deleter.go
@@ -2,6 +2,7 @@ package storage
 
 // Deleter deletes module metadata and its source from underlying storage
 type Deleter interface {
-	// Delete must return ErrNotFound if the module/version are not found
+	// Delete must return ErrVersionNotFound if the module/version are not
+	// found.
 	Delete(module, vsn string) error
 }
--- pkg/storage/fs/deleter.go
+++ pkg/storage/fs/deleter.go
@@ -1,7 +1,17 @@
 package fs
 
-// Delete removes a specific version of a module
+import (
+	"github.com/gomods/athens/pkg/storage"
+)
+
+// Delete removes a specific version of a module.
 func (v *storageImpl) Delete(module, version string) error {
 	versionedPath := v.versionLocation(module, version)
+	if !v.Exists(module, version) {
+		return storage.ErrVersionNotFound{
+			Module:  module,
+			Version: version,
+		}
+	}
 	return v.filesystem.RemoveAll(versionedPath)
 }
