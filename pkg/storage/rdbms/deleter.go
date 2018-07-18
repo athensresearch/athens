cd75b11801e5974791f23d35663eb4477157c455
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-07-18 16:47:40 -0400

    storage: return ErrVersionNotFound from rdbms Deleter (#298)
    This also fixes a bug caused by calling conn.Destroy
    on a non-pointer value.

--- pkg/storage/rdbms/deleter.go
+++ pkg/storage/rdbms/deleter.go
@@ -1,14 +1,21 @@
 package rdbms
 
 import (
+	"github.com/gomods/athens/pkg/storage"
 	"github.com/gomods/athens/pkg/storage/rdbms/models"
 )
 
-// Delete removes a specific version of a module
-func (r *ModuleStore) Delete(module, vsn string) error {
-	result := models.Module{}
-	query := r.conn.Where("module = ?", module).Where("version = ?", vsn)
-	if err := query.First(&result); err != nil {
+// Delete removes a specific version of a module.
+func (r *ModuleStore) Delete(module, version string) error {
+	if !r.Exists(module, version) {
+		return storage.ErrVersionNotFound{
+			Module:  module,
+			Version: version,
+		}
+	}
+	result := &models.Module{}
+	query := r.conn.Where("module = ?", module).Where("version = ?", version)
+	if err := query.First(result); err != nil {
 		return err
 	}
 	return r.conn.Destroy(result)
