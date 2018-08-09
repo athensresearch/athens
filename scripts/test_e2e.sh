dd16ce86f183857665d0c47dc967dd4688c878ef
Rob j Loranger <robjloranger@protonmail.com>
2018-08-09 16:09:39 -0700

    Fix up e2e script (#410)
    * small fixes
    
    * set GO111MODULES=auto
    
    * set GO111MODULES=on after running buffalo
    
    * use travis for go1.11beta3 not build from source
    
    * set GO111MODULES=on before running buffalo
    
    * revert GO111MODULE placement to after buffalo dev, comment w/ issue link
    
    * oops
    
    * spaces too

--- scripts/test_e2e.sh
+++ scripts/test_e2e.sh
@@ -7,13 +7,8 @@ set -xeuo pipefail
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/.."
 
-# Use a version of Go that supports Go Modules
-export GO111MODULES=on
 GOMOD_CACHE=$(go env GOPATH)/pkg/mod
-GO_SOURCE=${GO_SOURCE:=$(go env GOPATH)/src/golang.org/x/go}
-export GOROOT=${GO_SOURCE}
-export PATH=${GO_SOURCE}/bin:${REPO_DIR}/bin:${PATH}
-go version
+export PATH=${REPO_DIR}/bin:${PATH}
 
 clearGoModCache () {
   # The sudo is a necessary workaround until go is fixed
@@ -28,20 +23,22 @@ teardown () {
 trap teardown EXIT
 
 # Start the proxy in the background and wait for it to be ready
-export GO_BINARY_PATH=${GO_SOURCE}/bin/go
 cd $REPO_DIR/cmd/proxy
 pkill buffalo || true # cleanup old buffalos
 buffalo dev &
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 5; done
 
 # Clone our test repo
-TEST_SOURCE=${TMPDIR}go-depmgmt-testrepo
+TEST_SOURCE=${TMPDIR}happy-path
 rm -fr ${TEST_SOURCE} 2> /dev/null || true
 git clone https://github.com/athens-artifacts/happy-path.git ${TEST_SOURCE}
 pushd ${TEST_SOURCE}
 
 clearGoModCache
 
+# set modules on after running buffalo dev, not sure why
+# issue https://github.com/gomods/athens/issues/412
+export GO111MODULE=on
 # Make sure that our test repo works without the GOPROXY first
 unset GOPROXY
 go run .
