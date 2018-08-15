211eb9310cc1b659dd5b646e9eb429af21b9a817
Marwan Sulaiman <marwan.sameer@gmail.com>
2018-08-15 09:10:54 -0400

    pkg/log: log msg if no data given (#484)

--- pkg/log/buffalo_formatter.go
+++ pkg/log/buffalo_formatter.go
@@ -18,7 +18,6 @@ func (buffaloFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	statusCode, _ := entry.Data["status"].(int)
 	status := fmt.Sprint(statusCode)
-
 	switch {
 	case statusCode < 400:
 		status = color.GreenString("%v", status)
@@ -27,16 +26,27 @@ func (buffaloFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	default:
 		status = color.HiRedString("%v", status)
 	}
+	method, hasMethod := entry.Data["method"].(string)
+	path, hasPath := entry.Data["path"].(string)
+	if !hasMethod || !hasPath {
+		return fmtBuffaloMsg(entry.Message), nil
+	}
 
-	str := fmt.Sprintf(
+	return fmtBuffaloRequest(method, path, status), nil
+}
+
+func fmtBuffaloRequest(method, path, status string) []byte {
+	return []byte(fmt.Sprintf(
 		"%v %v %v [%v]\n",
 		color.CyanString("handler:"),
-		entry.Data["method"],
-		entry.Data["path"],
+		method,
+		path,
 		status,
-	)
+	))
+}
 
-	return []byte(str), nil
+func fmtBuffaloMsg(msg string) []byte {
+	return []byte(fmt.Sprintf("%s %s\n", color.CyanString("buffalo:"), msg))
 }
 
 func fmtBuffaloErr(msg string) []byte {
