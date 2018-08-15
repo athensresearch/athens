43d7501420fc573e36672393ed5a857f9b43a81c
Rob j Loranger <robjloranger@protonmail.com>
2018-08-15 10:16:14 -0700

    Update e2e tests to not use sudo (#477)
    * Update e2e tests to not use sudo
    
    Using a system temporary directory via mktemp we can place our GOMOD_CACHE
    in a place that does not require root permission to clear.
    
    As well we use the environment variable for GO_BINARY_PATH to point to
    a module compatible go version. At this time, 1.11beta3.
    
    * add GO_BINARY_PATH to travis
    
    * Use conditional GO_BINARY_PATH
    
    Co-authored-by: robbie <hello@robloranger.ca>
    Co-authored-by: carolyn <me@carolynvanslyck.com>
    
    * no longer needs to set GO_BINARY_PATH in env

--- scripts/test_e2e.sh
+++ scripts/test_e2e.sh
@@ -6,13 +6,13 @@
 set -xeuo pipefail
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/.."
-
-GOMOD_CACHE=$(go env GOPATH)/pkg/mod
+export GO_BINARY_PATH=${GO_BINARY_PATH:-go}
+TMPDIR=$(mktemp -d)
+GOMOD_CACHE=$TMPDIR/pkg/mod
 export PATH=${REPO_DIR}/bin:${PATH}
 
 clearGoModCache () {
-  # The sudo is a necessary workaround until go is fixed
-  sudo rm -fr ${GOMOD_CACHE}
+  rm -fr ${GOMOD_CACHE}
 }
 
 teardown () {
@@ -29,8 +29,7 @@ buffalo dev &
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 5; done
 
 # Clone our test repo
-TMPDIR=$(mktemp -d)
-TEST_SOURCE=${TMPDIR}happy-path
+TEST_SOURCE=${TMPDIR}/happy-path
 rm -fr ${TEST_SOURCE} 2> /dev/null || true
 git clone https://github.com/athens-artifacts/happy-path.git ${TEST_SOURCE}
 pushd ${TEST_SOURCE}
@@ -42,10 +41,10 @@ clearGoModCache
 export GO111MODULE=on
 # Make sure that our test repo works without the GOPROXY first
 unset GOPROXY
-go run .
+$GO_BINARY_PATH run .
 
 clearGoModCache
 
 # Verify that the test works against the proxy
 export GOPROXY=http://localhost:3000
-go run .
+$GO_BINARY_PATH run .
