5a55c0d8489008a90557fb32d9aaa9baf7bf8127
Smitty <smitty@vanbodegom.ca>
2020-05-14 15:46:38 -0400

    style: alternate gray and white on pages table

--- src/cljs/athens/style.cljs
+++ src/cljs/athens/style.cljs
@@ -1,4 +1,5 @@
-(ns athens.style (:require [garden.core :refer [css]]))
+(ns athens.style (:require [garden.core :refer [css]]
+                           [garden.selectors :refer [nth-child]]))
 
 (defn loading-css
   []
@@ -11,5 +12,10 @@
   [:style
    (css [:body {:font-family "sans-serif"}]
         [:.pages-table [:th {:font-weight "bold"
-                             :min-width "11em"}]]
+                             :min-width "11em"}]
+                       [:td {:padding "2px"}]
+                       [:tr
+                        [:&
+                         [(garden.selectors/& (nth-child :even)) {:background-color "#e8e8e8"}]]]
+                       [:& {:border-spacing "0"}]]
         [:.unknown-date {:color "#595959"}])])
