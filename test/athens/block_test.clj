91dcfffbd2c355b8b28f88f297ddf7d592f772a9
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-18 14:28:04 -0400

    Performed cljsstyle fix on root directory

--- test/athens/block_test.clj
+++ test/athens/block_test.clj
@@ -1,22 +1,23 @@
 (ns athens.block-test
-  (:require [athens.blocks :as blocks]
-            [clojure.test :refer [deftest is]]))
+  (:require
+    [athens.blocks :as blocks]
+    [clojure.test :refer [deftest is]]))
 
 
 (deftest sort-block-test
   (is (= {:block/children [{:block/order 1
-                            :block/children [{:block/order 3 }
+                            :block/children [{:block/order 3}
                                              {:block/order 4}
                                              {:block/order 6}]}
                            {:block/order 2
-                            :block/children [{:block/order 3 }
+                            :block/children [{:block/order 3}
                                              {:block/order 4}
                                              {:block/order 5}]}]}
          (blocks/sort-block {:block/children [{:block/order 2
                                                :block/children [{:block/order 4}
-                                                                {:block/order 3 }
+                                                                {:block/order 3}
                                                                 {:block/order 5}]}
                                               {:block/order 1
                                                :block/children [{:block/order 4}
-                                                                {:block/order 3 }
+                                                                {:block/order 3}
                                                                 {:block/order 6}]}]}))))
