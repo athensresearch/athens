9dd441e22c293e4ebd1347790a8c1a9453aa5efe
Jeffery Tang <tangj1122@gmail.com>
2020-05-28 21:18:41 -0400

    chore: migrate to posh from re-posh/re-frame

--- src/cljs/athens/devcards.cljs
+++ src/cljs/athens/devcards.cljs
@@ -2,64 +2,14 @@
   (:require
     [athens.devcards.db]
     [athens.devcards.sci-boxes]
+    [athens.devcards.block]
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :as devcards :include-macros true :refer [defcard]]
     [reagent.core :as r :include-macros true]))
 
-
-(def bmi-data (r/atom {:height 180 :weight 80}))
-
-
-(defn calc-bmi
-  [bmi-data]
-  (let [{:keys [height weight bmi] :as data} bmi-data
-        h (/ height 100)]
-    (if (nil? bmi)
-      (assoc data :bmi (/ weight (* h h)))
-      (assoc data :weight (* bmi h h)))))
-
-
-(defn slider
-  [bmi-data param value min max]
-  [:input {:type "range" :value value :min min :max max
-           :style {:width "100%"}
-           :on-change (fn [e]
-                        (swap! bmi-data assoc param (.. e -target -value))
-                        (when (not= param :bmi)
-                          (swap! bmi-data assoc :bmi nil)))}])
-
-
-(defn bmi-component
-  [bmi-data]
-  (let [{:keys [weight height bmi]} (calc-bmi @bmi-data)
-        [color diagnose] (cond
-                           (< bmi 18.5) ["orange" "underweight"]
-                           (< bmi 25) ["inherit" "normal"]
-                           (< bmi 30) ["orange" "overweight"]
-                           :else ["red" "obese"])]
-    [:div
-     [:h3 "BM calculator"]
-     [:div
-      "Height: " (int height) "cm"
-      [slider bmi-data :height height 100 220]]
-     [:div
-      "Weight: " (int weight) "kg"
-      [slider bmi-data :weight weight 30 150]]
-     [:div
-      "BMI: " (int bmi) " "
-      [:span {:style {:color color}} diagnose]
-      [slider bmi-data :bmi bmi 10 50]]]))
-
-
-(defcard bmi-calculator
-  "*Code taken from the Reagent readme.*"
-  (devcards/reagent bmi-component)
-  bmi-data
-  {:inspect-data true
-   :frame true
-   :history true})
-
+(defcard hello-world
+  "DevCards API and examples: http://rigsomelight.com/devcards/")
 
 (defn ^:export main
   []
---
+++ src/cljs/athens/devcards/block.cljs
@@ -0,0 +1,66 @@
+(ns athens.devcards.block
+  (:require
+    [athens.db :as db]
+    [cljsjs.react]
+    [cljsjs.react.dom]
+    [cljs.core.async :refer [go <!]]
+    [devcards.core :refer [defcard defcard-rg]]
+    [reagent.core :as rg]
+    [posh.reagent :refer [pull q posh! transact!]]
+    [datascript.core :as d]
+    [cljs-http.client :as http]
+    [athens.style :as style]))
+
+(def log js/console.log)
+
+(defn trace
+  [x]
+  (log x) x)
+
+(posh! db/dsdb)
+
+(defn left-sidebar
+  "TODO: Don't know how we want to handle global subscriptions or dispatches (i.e. routing) now that we are in reagent"
+  []
+  (let [favorites (q '[:find ?order ?title ?bid
+                       :where
+                       [?e :page/sidebar ?order]
+                       [?e :node/title ?title]
+                       [?e :block/uid ?bid]]
+                     db/dsdb)
+        sorted-favorites (->> @favorites
+                              (into [])
+                              (sort-by first))
+        ;;current-route (subscribe [:current-route])
+        ]
+    [:div {:style {:margin "0 10px" :max-width 250}}
+     ;;[:div [:a {:href (rfee/href :pages)} "All /pages"]]
+     ;;[:div [:span {:style {}} "Current Route: " [:b (-> @current-route :path)]]]
+     [:div {:style {:border-bottom "1px solid gray" :margin "10px 0"}}]
+     [:ul (style/+left-sidebar {})
+      (for [[_order title bid] sorted-favorites]
+        ;;^{:key bid} [:li [:a {:href (rfee/href :page {:id bid})} title]]
+        ^{:key bid} [:li [:span title]])
+      ]]))
+
+(defcard-rg left-sidebar
+  [left-sidebar])
+
+(defcard athens-dsdb
+  "Indices are omitted because they include all the datoms
+  Could probably use this and `load-db` as generic helpers for any componenets that use posh."
+  (dissoc @db/dsdb :eavt :aevt :avet))
+
+(defn load-db
+  []
+  (go
+    (let [res (<! (http/get db/athens-url {:with-credentials? false}))
+          {:keys [success body]} res]
+      (if success
+        (transact! db/dsdb (db/str-to-db-tx body))
+        (js/alert "Failed to retrieve data from GitHub")))))
+
+
+(defn -main []
+  (load-db))
+(-main)
