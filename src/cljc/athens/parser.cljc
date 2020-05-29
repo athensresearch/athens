5b762d40800fdc5e266185ce85f9f590a0c83768
Rory O’Kane <rory@roryokane.com>
2020-05-29 14:35:14 -0400

    refactor(parsing): merge parse-transform-helper into parser
    Now that the .cljc parser does transforms itself, rather than the .cljs parse-renderer, I don’t need a separate .cljc file to store the `combine-adjacent-strings` function.

--- src/cljc/athens/parser.cljc
+++ src/cljc/athens/parser.cljc
@@ -1,6 +1,5 @@
 (ns athens.parser
   (:require
-    [athens.parse-transform-helper :refer [combine-adjacent-strings]]   
     #?(:cljs [instaparse.core :as insta :refer-macros [defparser]]
        :clj [instaparse.core :as insta :refer [defparser]])))
 
@@ -31,6 +30,21 @@
    
    <any-char> = #'\\w|\\W'
    ")
+
+
+(defn combine-adjacent-strings
+  "In a sequence of strings mixed with other values, returns the same sequence with adjacent strings concatenated.
+   (If the sequence contains only strings, use clojure.string/join instead.)"
+  [coll]
+  (reduce
+    (fn [elements-so-far elmt]
+      (if (and (string? elmt) (string? (peek elements-so-far)))
+        (let [previous-elements (pop elements-so-far)
+              combined-last-string (str (peek elements-so-far) elmt)]
+          (conj previous-elements combined-last-string))
+        (conj elements-so-far elmt)))
+    []
+    coll))
 
 
 (defn transform-to-ast
--- test/athens/parser_test.clj
+++ test/athens/parser_test.clj
@@ -1,7 +1,7 @@
 (ns athens.parser-test
   (:require
-    [athens.parser :refer [parse-to-ast]]
-    [clojure.test :refer [deftest is]]))
+    [athens.parser :refer [parse-to-ast combine-adjacent-strings]]
+    [clojure.test :refer [deftest is are]]))
 
 
 (deftest block-parser-tests
@@ -12,3 +12,17 @@
   ;; Not including tests for every type of syntax because I expect the trees they are parsed to to change soon.
   ;; For now, additional tests would probably be more annoying than useful.
   )
+
+
+(deftest combine-adjacent-strings-tests
+  (are [x y] (= x (combine-adjacent-strings y))
+    []
+    []
+    ["some text"]
+    ["some" " " "text"]
+    ["some text" [:link] "around a link"]
+    ["some" " " "text" [:link] "around " "a link"]
+    [{:something nil} "more text" [:link] "between elements" 39]
+    [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
+    [{:a 1 :b 2} 3 ["leave" "intact"]]
+    [{:a 1 :b 2} 3 ["leave" "intact"]]))
