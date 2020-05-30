83ede819681f71ae1c31b0eb722462d84477d2b7
Rory O’Kane <rory@roryokane.com>
2020-05-30 18:22:39 -0400

    style: undo wrong auto-formatting in parser_test.clj
    When I was making the changes in commit 19f607d85a8116bea02f326e8a07e636b8b1c863, my editor auto-formatted on paste despite being configured not to. I have submitted a bug report about that: https://github.com/BetterThanTomorrow/calva/issues/656.

--- test/athens/parser_test.clj
+++ test/athens/parser_test.clj
@@ -19,12 +19,12 @@
 (deftest combine-adjacent-strings-tests
   (are [x y] (= x (combine-adjacent-strings y))
     []
-    []
+    , []
     ["some text"]
-    ["some" " " "text"]
+    , ["some" " " "text"]
     ["some text" [:link] "around a link"]
-    ["some" " " "text" [:link] "around " "a link"]
+    , ["some" " " "text" [:link] "around " "a link"]
     [{:something nil} "more text" [:link] "between elements" 39]
-    [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
+    , [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
     [{:a 1 :b 2} 3 ["leave" "intact"]]
-    [{:a 1 :b 2} 3 ["leave" "intact"]]))
+    , [{:a 1 :b 2} 3 ["leave" "intact"]]))
