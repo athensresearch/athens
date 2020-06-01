a24c9583f4bdcadf4a685abc1dc694754fb9ce1e
Rory O’Kane <rory@roryokane.com>
2020-06-01 19:23:52 -0400

    style: nicer indentation of expected/input values in tests
    Configure cljstyle to let us use indents within `are` blocks instead of having to faux-indent by prefixing a comma.

--- test/athens/parser_test.clj
+++ test/athens/parser_test.clj
@@ -7,28 +7,28 @@
 (deftest block-parser-tests
   (are [x y] (= x (parse-to-ast y))
     [:block]
-    , ""
+      ""
     [:block "OK? Yes."]
-    , "OK? Yes."
+      "OK? Yes."
     [:block [:page-link "link"]]
-    , "[[link]]"
+      "[[link]]"
     [:block "A " [:page-link "link"] "."]
-    , "A [[link]]."
+      "A [[link]]."
     [:block "[[text"]
-    , "[[text"
+      "[[text"
     [:block [:url-link {:url "https://example.com/"} "an example"]]
-    , "[an example](https://example.com/)"))
+      "[an example](https://example.com/)"))
 
 
 (deftest combine-adjacent-strings-tests
   (are [x y] (= x (combine-adjacent-strings y))
     []
-    , []
+      []
     ["some text"]
-    , ["some" " " "text"]
+      ["some" " " "text"]
     ["some text" [:link] "around a link"]
-    , ["some" " " "text" [:link] "around " "a link"]
+      ["some" " " "text" [:link] "around " "a link"]
     [{:something nil} "more text" [:link] "between elements" 39]
-    , [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
+      [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
     [{:a 1 :b 2} 3 ["leave" "intact"]]
-    , [{:a 1 :b 2} 3 ["leave" "intact"]]))
+      [{:a 1 :b 2} 3 ["leave" "intact"]]))
