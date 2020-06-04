0208ad3b07f47ba75e098b38a777bbc0df376698
Jeffery Tang <tangj1122@gmail.com>
2020-06-04 08:37:50 -0400

    fix: style

--- src/cljs/athens/devcards/db.cljs
+++ src/cljs/athens/devcards/db.cljs
@@ -31,7 +31,6 @@
       [:button.primary {:disabled @pressed? :on-click handler} "Load Real Data"])))
 
 
-
 (defcard-rg Load-Real-DB
   "Downloads the ego db. Takes a few seconds."
   [load-real-db-button db/dsdb])
