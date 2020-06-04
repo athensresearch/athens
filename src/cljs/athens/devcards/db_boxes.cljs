8bd307a807d15af93e33151679dc84eea41a90e7
Jelmer de Ronde <jelmer.deronde@surfnet.nl>
2020-06-04 21:20:33 +0200

    Enable browsing for :db/id's

--- src/cljs/athens/devcards/db_boxes.cljs
+++ src/cljs/athens/devcards/db_boxes.cljs
@@ -1,16 +1,18 @@
 (ns athens.devcards.db-boxes
   (:require
+    [athens.db :as db]
+    [cljs-http.client :as http]
+    [cljs.core.async :refer [<!]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [clojure.string :as str]
     [datascript.core :as d]
     [devcards.core :as devcards :refer [defcard defcard-rg]]
-    [reagent.core :as rg]
-    [sci.core :as sci]
-    [cljs-http.client :as http]
-    [athens.db :as db]
-    [cljs.core.async :refer [<!]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+    [garden.core :refer [css]]
+    [reagent.core :as r]
+    [sci.core :as sci])
+  (:require-macros
+    [cljs.core.async.macros :refer [go]]))
 
 
 (def log js/console.log)
@@ -22,53 +24,23 @@
 
 
 (defcard "
-  # An experiment in querying the datascript database")
-
-
-(def schema
-  {:block/uid      {:db/unique :db.unique/identity}
-   :node/title     {:db/unique :db.unique/identity}
-   :attrs/lookup   {:db/cardinality :db.cardinality/many}
-   :block/children {:db/cardinality :db.cardinality/many
-                    :db/valueType :db.type/ref}})
-
-
-(defonce dsdb (d/create-conn schema))
-
-
-(defonce loading? (atom true))
-
-
-(when @loading?
-  (go
-    (let [response (<! (http/get db/athens-url
-                                 {:with-credentials? false}))]
-      (->> response
-           :body
-           db/str-to-db-tx
-           (d/transact dsdb))
-
-      (reset! loading? false))))
+  # An experiment in browsing the datascript database")
 
 
 (def initial-box
   {:str-content
-    "(d/q '[:find ?e ?title
-            :where [?e :node/title ?title]]
-       @athens/db)"})
-
-(comment (d/q '[:find [(pull ?e [:node/title :block/uid]) ...]
-                :where [?e :node/title ?title]]
-            @athens/db))
+   "(d/q '[:find [(pull ?e [*]) ...]
+            :where [?e :node/title]]
+      @athens/db)"})
 
 
 (defonce box-state*
-  (rg/atom initial-box))
+  (r/atom initial-box))
 
 
 (defn eval-box
   [{:keys [str-content] :as box}]
-  (let [bindings {'athens/db dsdb
+  (let [bindings {'athens/db db/dsdb
                   'd/q d/q
                   'd/pull d/pull
                   'd/pull-many d/pull-many}
@@ -78,6 +50,37 @@
     (-> box
         (assoc :result result)
         (assoc :error (not ok?)))))
+
+
+(defn load-real-db!
+  [conn]
+  (go
+    (let [res (<! (http/get db/athens-url {:with-credentials? false}))
+          {:keys [success body]} res]
+      (if success
+        (do (d/transact! conn (db/str-to-db-tx body))
+            (swap! box-state* eval-box))
+        (js/alert "Failed to retrieve data from GitHub")))))
+
+
+(defn load-real-db-button
+  [conn]
+  (let [pressed? (r/atom false)
+        handler (fn []
+                  (swap! pressed? not)
+                  (load-real-db! conn))]
+    (fn []
+      [:button {:disabled @pressed? :on-click handler} "Load Real Data"])))
+
+
+(defcard-rg Load-Real-DB
+  "Downloads the ego db. Takes a few seconds."
+  [load-real-db-button db/dsdb])
+
+
+(defcard-rg Modify-Devcards
+  "Increase width to 90% for table"
+  [:style (css [:.com-rigsomelight-devcards-container {:width "90%"}])])
 
 
 (defn headings
@@ -94,18 +97,57 @@
                              (distinct)))))
 
 
-
+; there is code here that makes me weep. have to refactor this
 (defn rows
   [data mode]
   (case mode
-    :coll (map-indexed vector data)
-    :map (seq data)
-    :tuples (map-indexed (fn [idx data] (into [idx] data))  data)
+    :coll (->> data
+               (map-indexed vector)
+               (map (fn [x] {:item x})))
+
+    :map (->> data
+              (map (fn [[k v]] [{:item k} {:item v :attr k}])))
+
+    :tuples (->> data
+                 (map-indexed (fn [idx v]
+                                (into [{:item idx}]
+                                      (map (fn [x] {:item x}) v)))))
+
     :maps (let [hs (headings data mode)]
             (for [row-idx (-> data count range)]
-              (into [row-idx]
-                (for [h (rest hs)]
-                  (get-in data [row-idx h] "")))))))
+              (into [{:item row-idx}]
+                    (for [h (rest hs)]
+                      (when-let [item (get-in data [row-idx h])]
+                        {:item item
+                         :attr h})))))))
+
+
+;; When mode is :map or :maps we can look at the keys to determine the operation
+;; When mode is :tuples, :coll or :else we might have to analyze the query
+
+
+(do db/schema)
+
+
+(defn update-box!
+  [s]
+  (swap! box-state*
+         #(-> %
+              (assoc :str-content s)
+              (eval-box))))
+
+
+(defn pull-entity-str
+  [id]
+  (str "(d/pull @athens/db '[*] " id ")"))
+
+
+(defn cell
+  [{:keys [item attr]}]
+  (if (= :db/id attr)
+    [:a {:on-click #(update-box! (pull-entity-str item))}
+     (str item)]
+    (str item)))
 
 
 (defn table-view
@@ -114,9 +156,10 @@
     [:table
      [:tr (for [h hs] [:th (str h)])]
      (for [row (rows data mode)]
-       [:tr (for [cell row]
-              [:td (str cell)])])]))
-
+       [:tr (for [item row]
+              (if item
+                [:td (cell item)]
+                [:td ""]))])]))
 
 
 (defn coll-of-maps?
@@ -150,18 +193,18 @@
           :else
           (str result))])
 
+
 (defn error-component
   [error]
   [:div {:style {:color "red"}}
    (str error)])
 
+
 (defn handle-box-change!
   [e]
   (let [value (-> e .-target .-value)]
-    (swap! box-state*
-           #(-> %
-                (assoc :str-content value)
-                (eval-box)))))
+    (update-box! value)))
+
 
 (defn box-component
   []
