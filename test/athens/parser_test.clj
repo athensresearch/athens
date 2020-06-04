8658e4eec446a83c04615862d00929ee9ef7b5a5
Rory O’Kane <rory@roryokane.com>
2020-06-04 06:37:31 -0400

    style: reformat and restructure tests in parser_test.clj (#108)
    * style: undo wrong auto-formatting in parser_test.clj
    
    When I was making the changes in commit 19f607d85a8116bea02f326e8a07e636b8b1c863, my editor auto-formatted on paste despite being configured not to. I have submitted a bug report about that: https://github.com/BetterThanTomorrow/calva/issues/656.
    
    * style: make block-parser-tests more readable using `are`
    
    I think I understand the requirements of parsing enough to confirm that the current AST structure is appropriate, or at least would be easy to change everywhere if that were needed. So it’s okay to also remove the comments about not needing more tests.
    
    * style: nicer indentation of expected/input values in tests
    
    Configure cljstyle to let us use indents within `are` blocks instead of having to faux-indent by prefixing a comma.
    
    * Revert "style: nicer indentation of expected/input values in tests"
    
    This reverts commit a24c9583f4bdcadf4a685abc1dc694754fb9ce1e.
    
    * style: comma-less formatting of expected/input values in tests
    
    This style uses more vertical space, but makes each individual test case easier to read and is more idiomatic to Clojure. It also doesn’t require changing cljstyle configuration.

--- test/athens/parser_test.clj
+++ test/athens/parser_test.clj
@@ -5,26 +5,39 @@
 
 
 (deftest block-parser-tests
-  (is (= [:block] (parse-to-ast "")))
-  (is (= [:block "OK? Yes."] (parse-to-ast "OK? Yes.")))
-  (is (= [:block [:page-link "link"]] (parse-to-ast "[[link]]")))
-  (is (= [:block "A " [:page-link "link"] "."] (parse-to-ast "A [[link]].")))
-  (is (= [:block "[[text"] (parse-to-ast "[[text")))
-  (is (= [:block [:url-link {:url "https://example.com/"} "an example"]] (parse-to-ast "[an example](https://example.com/)")))
-  ;; Not including tests for every type of syntax because I expect the trees they are parsed to to change soon.
-  ;; For now, additional tests would probably be more annoying than useful.
-  )
+  (are [x y] (= x (parse-to-ast y))
+    [:block]
+    ""
+
+    [:block "OK? Yes."]
+    "OK? Yes."
+
+    [:block [:page-link "link"]]
+    "[[link]]"
+
+    [:block "A " [:page-link "link"] "."]
+    "A [[link]]."
+
+    [:block "[[text"]
+    "[[text"
+
+    [:block [:url-link {:url "https://example.com/"} "an example"]]
+    "[an example](https://example.com/)"))
 
 
 (deftest combine-adjacent-strings-tests
   (are [x y] (= x (combine-adjacent-strings y))
     []
     []
+
     ["some text"]
     ["some" " " "text"]
+
     ["some text" [:link] "around a link"]
     ["some" " " "text" [:link] "around " "a link"]
+
     [{:something nil} "more text" [:link] "between elements" 39]
     [{:something nil} "more" " " "text" [:link] "between" " " "elements" 39]
+
     [{:a 1 :b 2} 3 ["leave" "intact"]]
     [{:a 1 :b 2} 3 ["leave" "intact"]]))
