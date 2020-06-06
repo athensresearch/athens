efb8708b3b92612d74a09f496f6bdfcec0d18f54
shanberg <shanberg@users.noreply.github.com>
2020-06-05 21:10:52 -0400

    feat(icons): adding example icons and styles

--- src/cljs/athens/devcards/icons.cljs
+++ src/cljs/athens/devcards/icons.cljs
@@ -2,18 +2,23 @@
   (:require
     ["@material-ui/icons" :as mui-icons]
     [athens.db]
-    [athens.style :refer [style-guide-css]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [reagent.core :as r]
     [devcards.core :refer-macros [defcard-rg]]))
 
 
-(defcard-rg Face
+(defcard-rg Standard_Icons
   [:div
-   [:icon (r/create-element mui-icons/Face)]])
+   [:icon (r/create-element mui-icons/Face)]
+   [:icon (r/create-element mui-icons/Settings)]
+   [:icon (r/create-element mui-icons/Search)]])
 
 
-(defcard-rg Settings
+(defcard-rg Icon_Styles
   [:div
-   [:icon (r/create-element mui-icons/Settings)]])
+   [:icon (r/create-element mui-icons/Directions)]
+   [:icon (r/create-element mui-icons/DirectionsOutlined)]
+   [:icon (r/create-element mui-icons/DirectionsRounded)]
+   [:icon (r/create-element mui-icons/DirectionsTwoTone)]
+   [:icon (r/create-element mui-icons/DirectionsSharp)]])
