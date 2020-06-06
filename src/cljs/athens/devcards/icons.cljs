3b10c7f55fdeb298e8cc7da1f7f1e0ccc270450f
shanberg <shanberg@users.noreply.github.com>
2020-06-05 21:16:12 -0400

    feat(icons): more consistent style + help text

--- src/cljs/athens/devcards/icons.cljs
+++ src/cljs/athens/devcards/icons.cljs
@@ -8,14 +8,15 @@
     [devcards.core :refer-macros [defcard-rg]]))
 
 
-(defcard-rg Standard_Icons
+(defcard-rg Standard-Icons
   [:div
    [:icon (r/create-element mui-icons/Face)]
    [:icon (r/create-element mui-icons/Settings)]
    [:icon (r/create-element mui-icons/Search)]])
 
 
-(defcard-rg Icon_Styles
+(defcard-rg Icon-Styles
+  "Use different icon styles by appending one of Outlined, Rounded, TwoTone, or Sharp."
   [:div
    [:icon (r/create-element mui-icons/Directions)]
    [:icon (r/create-element mui-icons/DirectionsOutlined)]
