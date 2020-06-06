a738220c7f8556ec8437b767a8d1be0a618c6490
Stuart Hanberg <shanberg@users.noreply.github.com>
2020-06-06 17:41:07 -0400

    refactor(icons): show macro and styling examples in devcard (#131)
    * refactor(icons): use macro for icons in examples where possible
    
    * feat(icons): add additional examples of icon styling
    
    * chore(icons): remove unused import
    
    * chore(icons): fix linter issues
    
    Co-authored-by: shanberg <98312+shanberg@users.noreply.github.com>
    Co-authored-by: jeff <tangj1122@gmail.com>

--- src/cljs/athens/devcards/icons.cljs
+++ src/cljs/athens/devcards/icons.cljs
@@ -2,6 +2,8 @@
   (:require
     ["@material-ui/icons" :as mui-icons]
     [athens.db]
+    [athens.lib.dom.attributes :refer [with-styles]]
+    [athens.style :refer [COLORS OPACITIES]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :refer-macros [defcard-rg]]
@@ -10,16 +12,31 @@
 
 (defcard-rg Standard-Icons
   [:div
-   [:span (r/create-element mui-icons/Face)]
-   [:span (r/create-element mui-icons/Settings)]
-   [:span (r/create-element mui-icons/Search)]])
+   [:> mui-icons/Face]
+   [:> mui-icons/Settings]
+   [:> mui-icons/Search]])
 
 
-(defcard-rg Icon-Styles
-  "Use different icon styles by appending one of Outlined, Rounded, TwoTone, or Sharp."
+(defcard-rg Icon-Types
+  "Use the different built-in icon types by appending one of `Outlined`, `Rounded`, `TwoTone`, or `Sharp` to the icon name."
   [:div
-   [:span (r/create-element mui-icons/Directions)]
-   [:span (r/create-element mui-icons/DirectionsOutlined)]
-   [:span (r/create-element mui-icons/DirectionsRounded)]
-   [:span (r/create-element mui-icons/DirectionsTwoTone)]
-   [:span (r/create-element mui-icons/DirectionsSharp)]])
+   [:> mui-icons/Directions]
+   [:> mui-icons/DirectionsOutlined]
+   [:> mui-icons/DirectionsRounded]
+   [:> mui-icons/DirectionsTwoTone]
+   [:> mui-icons/DirectionsSharp]])
+
+
+(defcard-rg Styling-icons
+  "Color, opacity, and other properties can be applied to icons by placing them in an element with those styles applied."
+  [:div
+   [:span (with-styles {:color (:link-color COLORS)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:color (:highlight-color COLORS)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:color (:warning-color COLORS)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:color (:confirmation-color COLORS)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:color (:body-text-color COLORS)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:opacity (nth OPACITIES 0)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:opacity (nth OPACITIES 1)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:opacity (nth OPACITIES 2)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:opacity (nth OPACITIES 3)}) (r/create-element mui-icons/Face)]
+   [:span (with-styles {:color (:body-text-color COLORS)}) (r/create-element mui-icons/Face)]])
