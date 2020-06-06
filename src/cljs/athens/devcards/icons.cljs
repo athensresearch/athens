a4f051adb0d02e6e33c038735bac99947ca68150
shanberg <98312+shanberg@users.noreply.github.com>
2020-06-06 10:49:23 -0400

    feat(icons): add additional examples of icon styling

--- src/cljs/athens/devcards/icons.cljs
+++ src/cljs/athens/devcards/icons.cljs
@@ -2,6 +2,8 @@
   (:require
     ["@material-ui/icons" :as mui-icons]
     [athens.db]
+    [athens.lib.dom.attributes :refer [with-styles with-attributes]]
+    [athens.style :refer [COLORS OPACITIES]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :refer-macros [defcard-rg]]
@@ -15,11 +17,25 @@
    [:> mui-icons/Search]])
 
 
-(defcard-rg Icon-Styles
-  "Use different icon styles by appending one of Outlined, Rounded, TwoTone, or Sharp."
+(defcard-rg Icon-Types
+  "Use the different built-in icon types by appending one of `Outlined`, `Rounded`, `TwoTone`, or `Sharp` to the icon name."
   [:div
    [:> mui-icons/Directions]
    [:> mui-icons/DirectionsOutlined]
    [:> mui-icons/DirectionsRounded]
    [:> mui-icons/DirectionsTwoTone]
    [:> mui-icons/DirectionsSharp]])
+
+(defcard-rg Styling-icons
+  "Color, opacity, and other properties can be applied to icons by placing them in an element with those styles applied."
+  [:div
+    [:span (with-styles {:color (:link-color COLORS)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:color (:highlight-color COLORS)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:color (:warning-color COLORS)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:color (:confirmation-color COLORS)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:color (:body-text-color COLORS)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:opacity (nth OPACITIES 0)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:opacity (nth OPACITIES 1)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:opacity (nth OPACITIES 2)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:opacity (nth OPACITIES 3)}) (r/create-element mui-icons/Face)]
+    [:span (with-styles {:color (:body-text-color COLORS)}) (r/create-element mui-icons/Face)]])
