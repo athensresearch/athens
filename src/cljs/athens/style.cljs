b108c6250b49eabf1fe79a5d746c1bb5f1bf753e
shanberg <98312+shanberg@users.noreply.github.com>
2020-06-10 22:19:54 -0400

    feat(styles): add shadows from style guide

--- src/cljs/athens/style.cljs
+++ src/cljs/athens/style.cljs
@@ -15,6 +15,12 @@
    :panel-color        "#EFEDEB"
    :app-bg-color       "#FFFFFF"})
 
+(def DEPTH-SHADOWS
+  {:4                  "0px 1.6px 3.6px rgba(0, 0, 0, 0.13), 0px 0.3px 0.9px rgba(0, 0, 0, 0.1)"
+   :8                  "0px 3.2px 7.2px rgba(0, 0, 0, 0.13), 0px 0.6px 1.8px rgba(0, 0, 0, 0.1)"
+   :16                 "0px 6.4px 14.4px rgba(0, 0, 0, 0.13), 0px 1.2px 3.6px rgba(0, 0, 0, 0.1)"
+   :64                 "0px 24px 60px rgba(0, 0, 0, 0.15), 0px 5px 12px rgba(0, 0, 0, 0.1)"})
+
 
 (def HSL-COLORS
   (reduce-kv #(assoc %1 %2 (hex->hsl %3)) {} COLORS))
