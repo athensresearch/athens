1177011ccf07c862d797ef38421c7b43883728a5
shanberg <98312+shanberg@users.noreply.github.com>
2020-06-10 22:20:14 -0400

    fix(style): remove default input font style

--- src/cljs/athens/style.cljs
+++ src/cljs/athens/style.cljs
@@ -129,6 +129,8 @@
                   :letter-spacing "0.08em"
                   :text-transform "uppercase"}]
             [:.MuiSvgIcon-root {:font-size "24px"}]
+            [:input {
+                     :font-family "inherit"}]
             [:span
              [:.block-ref {:border-bottom [["1px" "solid" (:highlight-color COLORS)]]}
               [:&:hover {:background-color (opacify (:highlight-color HSL-COLORS) (first OPACITIES))
