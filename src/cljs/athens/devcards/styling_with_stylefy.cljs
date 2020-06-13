8e41ffed4612434f5fe50a54a6a536387c380631
shanberg <98312+shanberg@users.noreply.github.com>
2020-06-13 11:58:15 -0400

    feat(docs): more readable styling docs

--- src/cljs/athens/devcards/styling_with_stylefy.cljs
+++ src/cljs/athens/devcards/styling_with_stylefy.cljs
@@ -4,8 +4,8 @@
     [athens.style :refer [base-styles]]
     [cljsjs.react]
     [cljsjs.react.dom]
-    [devcards.core :refer-macros [defcard-doc]]))
-
+    [devcards.core :refer-macros [defcard-doc mkdn-pprint-source]]
+    [stylefy.core :as stylefy :refer [use-style]]))
 
 (defcard-doc
   "# Styling in Athens
@@ -13,7 +13,9 @@
   Components in Athens are styled using [Stylefy](https://github.com/Jarzka/stylefy).
   
   Behind the scenes, Stylefy creates classes and links them to each rendered component. This avoids polluting browser markup with inline styles, provides clear abstractions for reasoning about components, and allows for style reuse.
+")
 
+(defcard-doc "
   ## Creating Styles
 
   It's helpful to distinguish style definitions from other types of definitions. Append `-style` to the name of your component when defining the style: `(def blocks-style)`, `(page-style)`.
@@ -25,7 +27,9 @@
     {:cursor \"pointer\"
      :padding \"6px 10px\"})
   ```
+")
 
+(defcard-doc "
   Style pseudo-elements and element modes with `::stylefy/mode` inside the style definition.
 
   ```clojure
@@ -35,7 +39,9 @@
       ::stylefy/mode [[:hover {:background-color \"blue\"}]
                       [:after {:content \"''\"}]]})
   ```
+")
 
+(defcard-doc "
   Create styles for inner components with Stylefy's \"sub-styles\". Use these when creating sub-components that won't be used separately.
 
   ```clojure
@@ -43,7 +49,9 @@
     {:display \"flex\"
      ::stylefy/sub-styles {:footer {:margin-top \"auto\"}}})
   ```
+")
 
+(defcard-doc "
   Create deeper selections with `::stylefy/manual`.
 
   ```clojure
@@ -52,7 +60,9 @@
      :height \"32px\"
      ::stylefy/manual [[:&.open {:color \"blue\"}]})
   ```
+")
 
+(defcard-doc "
   Create more complex selections where necessary by combining manual mode with [Garden](https://github.com/noprompt/garden)'s advanced selectors.
 
   ```clojure
@@ -66,7 +76,9 @@
   ```
 
   Check out Stylefy's [documentation](https://github.com/Jarzka/stylefy) to learn about composing styles together, creating dynamic style functions, and more.
+")
 
+(defcard-doc "
   ### Applying Styles
 
   Connect your new style with the component with Stylefy's `(use-style)` function. `(use-style)` accepts two arguments: the style to add, and attributes to apply to the component.
@@ -79,14 +91,17 @@
 
   (defn box-component
     [:div (use-style box-style)])
-  ```
+  ```")
 
+(defcard-doc "
   Provide attributes to the element by adding them to `(use-style)` after the style.
 
   ```clojure
   [:a (use-style link-style {:href \"https://athensresearch.github.io/athens\"} \"Athens\")]
   ```
+")
 
+(defcard-doc "
   Apply sub-styles with Stylefy's `(use-sub-style)` function.
 
   ```clojure
@@ -99,12 +114,13 @@
     [:div (use-style box-style)
       [:div (use-sub-style box-style :box-child)]])
   ```
-  
+  ")
+
+(defcard-doc "
   Avoid creating styles that will be frequently updated, because this forces Stylefy to create a new class for each update.
   
   In these cases, pass the style directly to the element to update it inline.
   
   ```clojure
   [:div (use-style cursor-trail-style) {:style {:left x :top y}}]
-  ```"
-  [base-styles])
+  ```")
