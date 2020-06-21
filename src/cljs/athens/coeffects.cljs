409c83beb34c9cb10a2ea06f03e91c040f84fa99
jeff <tangj1122@gmail.com>
2020-06-21 10:31:52 -0400

    Update coeffects.cljs

--- src/cljs/athens/coeffects.cljs
+++ src/cljs/athens/coeffects.cljs
@@ -1,14 +1,4 @@
-(ns athens.coeffects
-  #_(:require
-    [athens.db :as db]
-    [cljs-http.client :as http]
-    [cljs.core.async :refer [go <!]]
-    [datascript.core :as d]
-    [datascript.transit :as dt]
-    [day8.re-frame.async-flow-fx]
-    [day8.re-frame.tracing :refer-macros [fn-traced]]
-    [posh.reagent :refer [transact! pull #_q #_pull-many]]
-    [re-frame.core :refer [dispatch reg-fx reg-event-db reg-event-fx]]))
+(ns athens.coeffects)
 
 
 ;;; Coeffects
