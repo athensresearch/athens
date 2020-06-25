e9b50a4b2f1c22ca5025b38290a8c1bb2ca5860f
Jeffery Tang <tangj1122@gmail.com>
2020-06-24 20:37:37 -0400

    feat(page): editable titles
    - add listeners for titles
    - remove instances of `dbid`. can just use [:block/uid uid]
    - do block destructuring as a parameter rather than in `let` block

--- src/cljs/athens/devcards/block_page.cljs
+++ src/cljs/athens/devcards/block_page.cljs
@@ -6,7 +6,9 @@
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :refer-macros [defcard-rg]]
-    [posh.reagent :refer [transact! pull]]))
+    [komponentit.autosize :as autosize]
+    [posh.reagent :refer [transact! pull]]
+    [re-frame.core :refer [subscribe]]))
 
 
 ;;; Globals
@@ -67,29 +69,44 @@
 
 ;; TODO: replace " > " with an icon. Get a TypeError when doing this, though. Maybe same problem as "->" issue in Athena results
 (defn block-page-el
-  [block parents]
-  (let [{:block/keys [string children]} block]
-    [:div
-     [:span {:style {:color "gray"}}
-      (interpose
-        " > "
-        (for [p parents]
-          (let [{:keys [node/title block/uid block/string]} p]
-            [:span {:key uid :style {:cursor "pointer"} :on-click #(navigate-uid uid)} (or string title)])))]
-     [:h1 (str "• " string)]
-     [:div (for [child children]
-             (let [{:keys [db/id]} child]
-               ^{:key id} [block-el child]))]]))
+  [{:block/keys [string children uid]} parents editing-uid]
+
+  [:div
+   ;; Parent Context
+   [:span {:style {:color "gray"}}
+    (interpose
+      " > "
+      (for [p parents]
+        (let [{:keys [node/title block/uid block/string]} p]
+          [:span {:key uid :style {:cursor "pointer"} :on-click #(navigate-uid uid)} (or string title)])))]
+
+   ;; Header
+   [:div {:data-uid uid :class "block-header"}
+    (if (= uid editing-uid)
+      [:h1
+       [autosize/textarea
+        {:value       string
+         :style       {:width "100%"}
+         :auto-focus  true
+         :on-change   (fn [e]
+                        ;;(prn (.. e -target -value))
+                        (transact! db/dsdb [[:db/add [:block/uid uid] :block/string (.. e -target -value)]]))}]]
+      [:h1 (str "• " string)])]
+
+   ;; Children
+   [:div (for [child children]
+           (let [{:keys [db/id]} child]
+             ^{:key id} [block-el child]))]])
 
 
 (defn block-page-component
-  ""
   [ident]
   (let [block   @(pull db/dsdb db/block-pull-pattern ident)
         parents (->> @(pull db/dsdb db/parents-pull-pattern ident)
-                     (db/shape-parent-query))]
+                     (db/shape-parent-query))
+        editing-uid @(subscribe [:editing-uid])]
     ;;(prn block parents)
-    [block-page-el block parents]))
+    [block-page-el block parents editing-uid]))
 
 
 ;;; Devcards
