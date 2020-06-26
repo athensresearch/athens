2e9904e22ae2747ea6830faa748c656677e01262
shanberg <98312+shanberg@users.noreply.github.com>
2020-06-25 21:11:08 -0400

    chore: fix lint issues

--- src/cljs/athens/devcards/blocks.cljs
+++ src/cljs/athens/devcards/blocks.cljs
@@ -4,7 +4,7 @@
     [athens.db :as db]
     [athens.parse-renderer :refer [parse-and-render]]
     [athens.router :refer [navigate-uid]]
-    [athens.style :refer [color OPACITIES]]
+    [athens.style :refer [color OPACITIES DEPTH-SHADOWS]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :refer-macros [defcard-rg]]
