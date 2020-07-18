a1ef4526270d60eb9d5e7f405a263f00efaac8f1
Stuart Hanberg <shanberg@users.noreply.github.com>
2020-07-18 18:35:40 -0400

    refactor(buttons, textinput, breadcrumbs): simpler component apis (#278)
    * refactor(buttons): new api and simpler component
    
    * feat(buttons): minor polish to button contents
    
    * refactor(breadcrumbs): simpler api for breadcrumbs
    
    * refactor(textinput): simpler api for textinput
    
    * fix: revert accidental file change
    
    * chore: fix lint issues
    
    * refactor(blocks): just use buttons in menus
    
    * chore: fix lint issues
    
    * chore: fix lint issues
    
    * chore: fix and delete minor goobers
    
    * fix(blocks): add missing button active toggle
    
    Co-authored-by: shanberg <98312+shanberg@users.noreply.github.com>

--- src/cljs/athens/devcards/athena.cljs
+++ src/cljs/athens/devcards/athena.cljs
@@ -4,20 +4,19 @@
     [athens.devcards.db :refer [load-real-db-button]]
     [athens.subs]
     [athens.views.athena :refer [athena-prompt-el athena-component]]
-    [athens.views.buttons :refer [button-primary]]
+    [athens.views.buttons :refer [button]]
     [datascript.core :as d]
     [devcards.core :refer-macros [defcard-rg]]))
 
 
 (defcard-rg Create-Page
   "Press button and then search \"test\" "
-  [button-primary {:on-click-fn (fn []
-                                  (let [n       (inc (:max-eid @db/dsdb))
-                                        n-child (inc n)]
-                                    (d/transact! db/dsdb [{:node/title     (str "Test Page " n)
-                                                           :block/uid      (str "uid-" n)
-                                                           :block/children [{:block/string (str "Test Block" n-child) :block/uid (str "uid-" n-child)}]}])))
-                   :label       "Create Test Pages and Blocks"}])
+  [button {:on-click (fn []
+                       (let [n       (inc (:max-eid @db/dsdb))
+                             n-child (inc n)]
+                         (d/transact! db/dsdb [{:node/title     (str "Test Page " n)
+                                                :block/uid      (str "uid-" n)
+                                                :block/children [{:block/string (str "Test Block" n-child) :block/uid (str "uid-" n-child)}]}])))} "Create Test Pages and Blocks"])
 
 
 (defcard-rg Load-Real-DB
--- src/cljs/athens/devcards/left_sidebar.cljs
+++ src/cljs/athens/devcards/left_sidebar.cljs
@@ -1,18 +1,18 @@
 (ns athens.devcards.left-sidebar
   (:require
     [athens.db :as db]
-    [athens.views.buttons :refer [button-primary]]
+    [athens.views.buttons :refer [button]]
     [athens.views.left-sidebar :refer [left-sidebar]]
     [devcards.core :refer [defcard-rg]]
     [posh.reagent :refer [transact!]]))
 
 
 (defcard-rg Create-Shortcut
-  [button-primary {:on-click-fn (fn []
-                                  (let [n (:max-eid @db/dsdb)]
-                                    (transact! db/dsdb [{:page/sidebar n
-                                                         :node/title   (str "Page " n)
-                                                         :block/uid    (str "uid" n)}]))) :label "Create Shortcut"}])
+  [button {:on-click (fn []
+                       (let [n (:max-eid @db/dsdb)]
+                         (transact! db/dsdb [{:page/sidebar n
+                                              :node/title   (str "Page " n)
+                                              :block/uid    (str "uid" n)}])))} "Create Shortcut"])
 
 
 (defcard-rg Left-Sidebar
