3b12d58d036adccb03f7918c496d1159f5a253c6
Adrien Lacquemant <github@alaq.io>
2020-07-24 17:57:21 -0400

    feat(slash commands): scroll slash commands with arrow keys
    Co-authored-by: itsrainingmani <tsmanikandan@protonmail.com>
    Co-authored-by: nthd3gr33 <codinginenglish@gmail.com>

--- src/cljs/athens/keybindings.cljs
+++ src/cljs/athens/keybindings.cljs
@@ -2,7 +2,7 @@
   (:require
     ["@material-ui/icons" :as mui-icons]
     [athens.db :as db]
-    [athens.util :refer [scroll-if-needed get-day]]
+    [athens.util :refer [scroll-if-needed get-day is-beyond-rect?]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [goog.dom.selection :refer [setStart setEnd getText setCursorPosition getEndPoints]]
@@ -12,6 +12,7 @@
     (goog.events
       KeyCodes)))
 
+(declare slash-options)
 
 (defn modifier-keys
   [e]
@@ -101,10 +102,21 @@
       (= type :slash) (cond
                         (= :up direction) (do
                                             (.. e preventDefault)
-                                            (swap! state update :search/index dec))
+                                            (swap! state update :search/index #(dec (if (zero? %) (count slash-options) %)))
+                                            (let [cur-index (:search/index @state)
+                                                  container-el (.. e -target -parentNode -parentNode -nextSibling -firstChild)
+                                                  next-el (nth (array-seq (.. container-el -children)) cur-index)]
+                                              (when (is-beyond-rect? next-el container-el)
+                                                (.. next-el (scrollIntoView (not= cur-index (dec (count slash-options))) {:behavior "auto"})))))
+
                         (= :down direction) (do
                                               (.. e preventDefault)
-                                              (swap! state update :search/index inc)))
+                                              (swap! state update :search/index #(if (= % (dec (count slash-options))) 0 (inc %)))
+                                              (let [cur-index (:search/index @state)
+                                                    container-el (.. e -target -parentNode -parentNode -nextSibling -firstChild)
+                                                    next-el (nth (array-seq (.. container-el -children)) cur-index)]
+                                                (when (is-beyond-rect? next-el container-el)
+                                                  (.. next-el (scrollIntoView (zero? cur-index) {:behavior "auto"}))))))
 
       (or (= type :page) (= type :block))
       (cond
