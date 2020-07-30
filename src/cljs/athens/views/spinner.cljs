9db5faf393d1a33c70acba4df8e60f02fde99278
Ulf Ninow <mail@fnumatic.de>
2020-07-30 19:23:59 +0200

    rfct: use goog dom getElement

--- src/cljs/athens/views/spinner.cljs
+++ src/cljs/athens/views/spinner.cljs
@@ -4,6 +4,7 @@
     [athens.style :refer [color OPACITIES]]
     [cljsjs.react]
     [cljsjs.react.dom]
+    [goog.dom :refer [getElement]]
     [reagent.core :as r]
     [stylefy.core :as stylefy :refer [use-style]]))
 
@@ -97,4 +98,4 @@
   []
   (stylefy/init)
   (r/render [initial-spinner-component]
-            (.getElementById js/document "app")))
+            (getElement "app")))
