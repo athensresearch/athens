8708a923667714f23360ac24ef138297a6bb269e
jeff <tangj1122@gmail.com>
2020-08-18 20:54:26 -0400

    feat(electron): add logging. add app and update listeners

--- src/cljs/athens/main/core.cljs
+++ src/cljs/athens/main/core.cljs
@@ -1,9 +1,23 @@
 (ns athens.main.core
   (:require
-    ["electron" :refer [app BrowserWindow crashReporter]]))
+    ["electron" :refer [app BrowserWindow crashReporter]]
+    ["electron-updater" :refer [autoUpdater]]))
+
+(def log (js/require "electron-log"))
+
+(set! (.. autoUpdater -logger) log)
+(set! (.. autoUpdater -logger -transports -file -level) "info")
+(.. log (info "App starting..."))
+
 
 
 (def main-window (atom nil))
+
+
+(defn send-status-to-window
+  [text]
+  (.. log (info text))
+  (.. @main-window -webContents (send text)))
 
 
 (defn init-browser
@@ -19,16 +33,52 @@
   (.on @main-window "closed" #(reset! main-window nil)))
 
 
+(defn init-updater
+  []
+  (.on autoUpdater "checking-for-update"
+       (fn []
+         (send-status-to-window "Checking for update...")))
+
+  (.on autoUpdater "update-available"
+       (fn [_]
+         (send-status-to-window "Update available.")))
+
+  (.on autoUpdater "update-not-available"
+       (fn [_]
+         (send-status-to-window "Update not available.")))
+
+  (.on autoUpdater "error"
+       (fn [e]
+         (send-status-to-window (str "Error in auto-updater. " e))))
+
+  (.on autoUpdater "download-progress"
+       (fn [progress-obj]
+         (let [progress-clj (js->clj progress-obj)
+               {:keys [bytesPerSecond percent transferred total]} progress-clj
+               msg (str "Download speed: " bytesPerSecond
+                        " - Downloaded " percent "%"
+                        " (" transferred "/" total ")")]
+           (send-status-to-window msg))))
+
+  (.on autoUpdater "update-downloaded"
+       (fn [_]
+         (send-status-to-window "Update downloaded."))))
+
+
 (defn main
   []
   ; CrashReporter can just be omitted
-  (.start crashReporter
-          (clj->js
-            {:companyName "MyAwesomeCompany"
-             :productName "MyAwesomeApp"
-             :submitURL "https://example.com/submit-url"
-             :autoSubmit false}))
+  ;(.start crashReporter
+  ;        (clj->js
+  ;          {:companyName "MyAwesomeCompany"
+  ;           :productName "MyAwesomeApp"
+  ;           :submitURL "https://example.com/submit-url"
+  ;           :autoSubmit false}))
 
   (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
                                   (.quit app)))
-  (.on app "ready" init-browser))
+  (.on app "ready" init-browser)
+  (.on app "ready" (fn []
+                     (init-updater)
+                     (.. autoUpdater checkForUpdatesAndNotify))))
+
