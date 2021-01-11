11d95083def585090df73b5b34c2b3e10e37628d
jeff <tangj1122@gmail.com>
2021-01-11 10:51:42 -0800

    add :page/refs to walker; separate namespace

--- test/athens/walk_test.clj
+++ test/athens/walk_test.clj
@@ -1,2 +1,27 @@
 (ns athens.walk-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer [deftest is are run-tests]]
+            [datascript.core :as d]
+            [athens.walk :as walk]))
+
+
+(deftest db-test
+  (are [x y] (= (walk/walk-string x) y)
+
+             "[[hey]]"
+             {:node/titles ["hey"] :page/refs [[:node/title "hey"]]}
+
+             "#hola"
+             {:node/titles ["hola"] :page/refs [[:node/title "hola"]]}
+
+             ;; order matters
+             ;; ["ma" "ni hao"] != ["ni hao" "ma"]
+             ;;"[[ni hao]] #ma"
+             ;;{:node/titles ["ma" "ni hao"]}
+
+             "#[[aloha]]"
+             {:node/titles ["aloha"]}
+
+             "((uid123))"
+             {:block/refs ["uid123"]}))
+
+(run-tests)
