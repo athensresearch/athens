f975c49c25624c58ff2c539fd118821bdb70af5f
jeff <tangj1122@gmail.com>
2021-01-27 20:09:39 -0800

    lint

--- src/cljs/athens/views.cljs
+++ src/cljs/athens/views.cljs
@@ -1,13 +1,11 @@
 (ns athens.views
   (:require
-    ["@material-ui/icons" :as mui-icons]
     [athens.db :as db]
     [athens.subs]
     [athens.views.all-pages :refer [table]]
     [athens.views.app-toolbar :refer [app-toolbar]]
     [athens.views.athena :refer [athena-component]]
     [athens.views.block-page :refer [block-page-component]]
-    [athens.views.buttons :refer [button]]
     [athens.views.daily-notes :refer [daily-notes-panel db-scroll-daily-notes]]
     [athens.views.devtool :refer [devtool-component]]
     [athens.views.filesystem :as filesystem]
@@ -18,7 +16,6 @@
     [athens.views.spinner :refer [initial-spinner-component]]
     [posh.reagent :refer [pull]]
     [re-frame.core :refer [subscribe dispatch]]
-    [reagent.core :as r]
     [stylefy.core :as stylefy :refer [use-style]]))
 
 
@@ -66,36 +63,6 @@
 
 
 ;; Panels
-
-
-(defn settings-panel
-  []
-  (let [opted-out (r/atom (.. js/window -posthog has_opted_out_capturing))]
-    (fn []
-      [:div {:style {:display "flex"
-                     :margin "0vh 5vw"
-                     :flex-direction "column"}}
-       [:h1 "Settings"]
-       (if @opted-out
-         [:h5 "Opted Out of Analytics"]
-         [:h5 "Opted Into Analytics"])
-       [:div {:style {:margin "10px 0"}}
-        [button {:primary (false? @opted-out)
-                 :on-click (fn []
-                             (if @opted-out
-                               (.. js/window -posthog opt_in_capturing)
-                               (.. js/window -posthog opt_out_capturing))
-                             (swap! opted-out not))}
-         (if @opted-out
-           [:div {:style {:display "flex"}}
-            [:> mui-icons/ToggleOn]
-            [:span "\uD83D\uDE41 We understand."]]
-           [:div {:style {:display "flex"}}
-            [:> mui-icons/ToggleOff]
-            [:span "\uD83D\uDE00 Thanks for helping make Athens better!"]])]]
-       [:span "Analytics are anonymized and delivered by "
-        [:a {:href "https://posthog.com" :target "_blank"} "Posthog"]
-        ", an open-source provider of product analytics. This lets the designers and engineers at Athens know if we're really making something people love!"]])))
 
 
 (defn pages-panel
