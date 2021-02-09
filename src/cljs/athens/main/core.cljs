00e019fb9fdb94822227c3ce226da7982d822331
Junius Free B. Fontamillas <juniusfree@users.noreply.github.com>
2021-02-09 14:30:25 +0800

    fix: open web links using default browser (#617)
    Co-authored-by: jeff <tangj1122@gmail.com>

--- src/cljs/athens/main/core.cljs
+++ src/cljs/athens/main/core.cljs
@@ -1,6 +1,6 @@
 (ns athens.main.core
   (:require
-    ["electron" :refer [app BrowserWindow ipcMain]]
+    ["electron" :refer [app BrowserWindow ipcMain shell]]
     ["electron-updater" :refer [autoUpdater]]))
 
 
@@ -38,7 +38,10 @@
                                                    :nodeIntegrationWorker true}})))
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL ^js @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on ^js @main-window "closed" #(reset! main-window nil)))
+  (.on ^js @main-window "closed" #(reset! main-window nil))
+  (.. ^js @main-window -webContents (on "new-window" (fn [e url]
+                                                       (.. e preventDefault)
+                                                       (.. shell (openExternal url))))))
 
 
 (defn init-updater
