339c9ec4eff2247aff2ed683ab741cde8cae4d79
jeff <tangj1122@gmail.com>
2021-02-15 15:38:27 -0800

    comment

--- src/cljs/athens/views/app_toolbar.cljs
+++ src/cljs/athens/views/app_toolbar.cljs
@@ -99,8 +99,8 @@
          [button {:on-click #(router/navigate :graph)
                   :active   (= @route-name :graph)} [:> mui-icons/BubbleChart]]
          ;; below is used for testing error tracking
-         [button {:on-click #(throw (js/Error "error"))
-                  :style {:border "1px solid red"}} [:> mui-icons/Warning]]
+         #_[button {:on-click #(throw (js/Error "error"))
+                    :style {:border "1px solid red"}} [:> mui-icons/Warning]]
          [button {:on-click #(dispatch [:athena/toggle])
                   :style    {:width "14rem" :margin-left "1rem" :background (color :background-minus-1)}
                   :active   @(subscribe [:athena/open])}
