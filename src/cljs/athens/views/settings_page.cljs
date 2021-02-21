75e8dcad73867117b26d246a8ac62ffdf4178a4c
Manikandan Sundararajan <mani@hey.com>
2021-02-21 02:23:39 -0500

    Create option to remember window size in settings

--- src/cljs/athens/views/settings_page.cljs
+++ src/cljs/athens/views/settings_page.cljs
@@ -1,8 +1,8 @@
 (ns athens.views.settings-page
   (:require
-    ["@material-ui/icons" :as mui-icons]
-    [athens.views.buttons :refer [button]]
-    [reagent.core :as r]))
+   ["@material-ui/icons" :as mui-icons]
+   [athens.views.buttons :refer [button]]
+   [reagent.core :as r]))
 
 
 (defn opt-out
@@ -21,6 +21,25 @@
   (reset! opted-out? false))
 
 
+(defn remember-ws
+  [remember-window-size?]
+  (js/localStorage.setItem "rememberws" "on")
+  (reset! remember-window-size? true))
+
+
+(defn dont-remember-ws
+  [remember-window-size?]
+  (js/localStorage.setItem "rememberws" "off")
+  (reset! remember-window-size? false))
+
+
+(defn handle-ws
+  [remember-window-size?]
+  (if @remember-window-size?
+    (dont-remember-ws remember-window-size?)
+    (remember-ws remember-window-size?)))
+
+
 (defn handle-click
   [opted-out?]
   (if @opted-out?
@@ -30,7 +49,8 @@
 
 (defn settings-page
   []
-  (let [opted-out? (r/atom (.. js/window -posthog has_opted_out_capturing))]
+  (let [opted-out? (r/atom (.. js/window -posthog has_opted_out_capturing))
+        remember-window-size? (r/atom false)]
     (fn []
       [:div {:style {:display "flex"
                      :margin "0vh 5vw"
@@ -53,6 +73,18 @@
         [:a {:href "https://posthog.com" :target "_blank"} "Posthog"]
         " and " [:a {:href "https://sentry.io" :target "_blank"} "Sentry"]
         ", open-source solutions to measure retention, performance, and crashes.
-         This lets the designers and engineers at Athens know if we're really making something people love!"]])))
+         This lets the designers and engineers at Athens know if we're really making something people love!"]
+       [:br]
+       [:h5 "Remember the last Window Size on Athens Startup"]
+       [:div {:style {:margin "10px 0"}}
+        [button {:primary (true? @remember-window-size?)
+                 :on-click #(handle-ws remember-window-size?)}
+         (if @remember-window-size?
+           [:div {:style {:display "flex"}}
+            [:> mui-icons/ToggleOff]
+            [:span "\uD83D\uDE00 Yes!"]]
+           [:div {:style {:display "flex"}}
+            [:> mui-icons/ToggleOn]
+            [:span "\uD83D\uDE41 No."]])]]])))
 
 
