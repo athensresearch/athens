76e22d66ef9e77dda3ecbccea702b7573d41bf76
jeff <tangj1122@gmail.com>
2021-02-22 22:26:35 -0800

    feat(settings): let user auth with OpenCollective email

--- src/cljs/athens/views/settings_page.cljs
+++ src/cljs/athens/views/settings_page.cljs
@@ -3,8 +3,11 @@
     ["@material-ui/icons" :as mui-icons]
     [athens.electron :as electron]
     [athens.views.buttons :refer [button]]
+    [cljs-http.client :as http]
+    [cljs.core.async :refer [<!]]
     [goog.functions :as goog-functions]
-    [reagent.core :as r]))
+    [reagent.core :as r])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 
 (defn opt-out
@@ -38,9 +41,38 @@
     (js/localStorage.setItem "debounce-save-time" value)))
 
 
+(defn handle-change-email
+  [email value]
+  (reset! email value))
+
+
+(def a (atom nil))
+
+(defn handle-click-email
+  [email authed?]
+  (let [api "https://dhx9n94ty5.execute-api.us-east-1.amazonaws.com/Prod/hello"
+        email-qs "?email="
+        url (str api email-qs email)]
+
+    (go (let [resp (<! (http/get url))]
+          (if (and (:success resp) (:email_exists (:body resp)))
+            (prn "success")
+            (prn "fail")))
+        #_(if false
+            (do
+              (prn "localStorage set email" email)
+              ;;(js/localStorage.setItem "email/address" email)
+              (reset! authed? true))
+            (js/alert "Your email was invalid.")))))
+
+
 (defn settings-page
   []
   (let [opted-out?          (r/atom (.. js/window -posthog has_opted_out_capturing))
+        authed?             (r/atom false)
+        ;; on start find email in localStorage
+        email               (r/atom "")
+        ;; if none, then user can't opt out
         debounce-save-time! (r/atom (js/Number (js/localStorage.getItem "debounce-save-time")))]
     (fn []
       [:div {:style {:display        "flex"
@@ -48,12 +80,23 @@
                      :flex-direction "column"}}
        [:h2 "Settings"]
 
+       [:div {:style {:margin "10px 0"}}
+        [:h5 "Email"]
+        [:div {:style {:margin "5px 0" :display "flex" :justify-content "space-between"}}
+         [:input {:type "email" :value @email :placeholder "Open Collective Email"
+                  :on-change #(handle-change-email email (.. % -target -value))}]
+         [button {:on-click #(handle-click-email @email authed?)
+                  :disabled (not (re-find #"@" @email))
+                  :primary  true} "Submit"]]]
+
        ;; Analytics
+
        (if @opted-out?
          [:h5 "Opted Out of Analytics"]
          [:h5 "Opted Into Analytics"])
        [:div {:style {:margin "10px 0"}}
         [button {:primary  (false? @opted-out?)
+                 :disabled (not @authed?)
                  :on-click #(handle-click opted-out?)}
          (if @opted-out?
            [:div {:style {:display "flex"}}
@@ -75,7 +118,6 @@
                        :margin "10px 0"}}
          [:input {:style {:width "4em"}
                   :type  "number" :value @debounce-save-time! :on-change #(handle-debounce-save-input (js/Number (.. % -target -value)) debounce-save-time!)}]
-         (prn "type" @debounce-save-time! (js/Number @debounce-save-time!))
          (case @debounce-save-time!
            0 [:span (str "Athens will save and create a backup after each edit.")]
            1 [:span (str "Athens will save and create a backup " @debounce-save-time! " second after your last edit.")]
