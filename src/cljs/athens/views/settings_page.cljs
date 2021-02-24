904fa61c3430e87ba936a8a150b52b486f801c47
jeff <tangj1122@gmail.com>
2021-02-24 15:29:35 -0800

    fix indent

--- src/cljs/athens/views/settings_page.cljs
+++ src/cljs/athens/views/settings_page.cljs
@@ -91,63 +91,63 @@
                        :margin         "0vh 5vw"
                        :width          "90vw"
                        :flex-direction "column"}}
-         [:h2 "Settings"
+         [:h2 "Settings"]
 
-           (if @authed?
-             [:span (str "Thank you for using and backing us, " @email " ❤️")]
-             [:span "You are using the free version of Athens. You are hosting your own data. Please be careful!"])
+         (if @authed?
+           [:span (str "Thank you for using and backing us, " @email " ❤️")]
+           [:span "You are using the free version of Athens. You are hosting your own data. Please be careful!"])
 
-           [:div {:style {:margin "10px 0"}}
-            [:h5 "Email"]
-            [:div {:style {:margin "5px 0" :display "flex" :justify-content "space-between"}}
-             [:input {:style {:width "15em"} :type "email" :value @email :placeholder "Open Collective Email"
-                      :on-change #(handle-change-email email (.. % -target -value))}]
-             [button {:on-click #(handle-click-email @email authed? sending-request)
-                      :disabled submit-disabled
-                      :primary  true} "Submit"]]]
+         [:div {:style {:margin "10px 0"}}
+          [:h5 "Email"]
+          [:div {:style {:margin "5px 0" :display "flex" :justify-content "space-between"}}
+           [:input {:style {:width "15em"} :type "email" :value @email :placeholder "Open Collective Email"
+                    :on-change #(handle-change-email email (.. % -target -value))}]
+           [button {:on-click #(handle-click-email @email authed? sending-request)
+                    :disabled submit-disabled
+                    :primary  true} "Submit"]]]
 
-           ;; Analytics
+         ;; Analytics
 
+         (if @opted-out?
+           [:h5 "Opted Out of Analytics"]
+           [:h5 "Opted Into Analytics"])
+         [:div {:style {:margin "10px 0"}}
+          [button {:primary  (false? @opted-out?)
+                   :disabled (not @authed?)
+                   :on-click #(handle-click opted-out?)}
            (if @opted-out?
-             [:h5 "Opted Out of Analytics"]
-             [:h5 "Opted Into Analytics"])
-           [:div {:style {:margin "10px 0"}}
-            [button {:primary  (false? @opted-out?)
-                     :disabled (not @authed?)
-                     :on-click #(handle-click opted-out?)}
-             (if @opted-out?
-               [:div {:style {:display "flex"}}
-                [:> mui-icons/ToggleOn]
-                [:span "\uD83D\uDE41 We understand."]]
-               [:div {:style {:display "flex"}}
-                [:> mui-icons/ToggleOff]
-                [:span "\uD83D\uDE00 Thanks for helping make Athens better!"]])]]
+             [:div {:style {:display "flex"}}
+              [:> mui-icons/ToggleOn]
+              [:span "\uD83D\uDE41 We understand."]]
+             [:div {:style {:display "flex"}}
+              [:> mui-icons/ToggleOff]
+              [:span "\uD83D\uDE00 Thanks for helping make Athens better!"]])]]
 
-           [:span "Analytics are delivered by "
-            [:a {:href "https://posthog.com" :target "_blank"} "Posthog"]
-            " and " [:a {:href "https://sentry.io" :target "_blank"} "Sentry"]
-            "." (when (false? @authed?)
-                  [:span
-                   " In order to opt-out of analytics, please become a User or Sponsor through "
-                   [:a {:href "https://opencollective.com/athens" :target "_blank"}
-                    "OpenCollective."]])]
+         [:span "Analytics are delivered by "
+          [:a {:href "https://posthog.com" :target "_blank"} "Posthog"]
+          " and " [:a {:href "https://sentry.io" :target "_blank"} "Sentry"]
+          "." (when (false? @authed?)
+                [:span
+                 " In order to opt-out of analytics, please become a User or Sponsor through "
+                 [:a {:href "https://opencollective.com/athens" :target "_blank"}
+                  "OpenCollective."]])]
 
-           [:div {:style {:margin-top "15px"}}
-            [:h5 "Remote Backups"]
-            [:div {:style {:margin "5px 0" :display "flex" :justify-content "space-between"}}
-             [button {:disabled true}
-              "Backup my DB to the cloud"]
-             [:span "Coming soon to " [:a {:href "https://opencollective.com/athens" :target "_blank"}
-                                       "paid Users and Sponsors"]]]]
+         [:div {:style {:margin-top "15px"}}
+          [:h5 "Remote Backups"]
+          [:div {:style {:margin "5px 0" :display "flex" :justify-content "space-between"}}
+           [button {:disabled true}
+            "Backup my DB to the cloud"]
+           [:span "Coming soon to " [:a {:href "https://opencollective.com/athens" :target "_blank"}
+                                     "paid Users and Sponsors"]]]]
 
-           ;; Auto-save
-           [:div {:style {:margin "20px 0"}}
-            [:h5 "Auto-save"]
-            [:div {:style {:display "flex" :justify-content "space-between"
-                           :margin "10px 0"}}
-             [:input {:style {:width "4em"}
-                      :type  "number" :value @debounce-save-time! :on-change #(handle-debounce-save-input (js/Number (.. % -target -value)) debounce-save-time!)}]
-             (case @debounce-save-time!
-               0 [:span (str "Athens will save and create a local backup after each edit.")]
-               1 [:span (str "Athens will save and create a local backup " @debounce-save-time! " second after your last edit.")]
-               [:span (str "Athens will save and create a local backup " @debounce-save-time! " seconds after your last edit.")])]]]]))))
+         ;; Auto-save
+         [:div {:style {:margin "20px 0"}}
+          [:h5 "Auto-save"]
+          [:div {:style {:display "flex" :justify-content "space-between"
+                         :margin "10px 0"}}
+           [:input {:style {:width "4em"}
+                    :type  "number" :value @debounce-save-time! :on-change #(handle-debounce-save-input (js/Number (.. % -target -value)) debounce-save-time!)}]
+           (case @debounce-save-time!
+             0 [:span (str "Athens will save and create a local backup after each edit.")]
+             1 [:span (str "Athens will save and create a local backup " @debounce-save-time! " second after your last edit.")]
+             [:span (str "Athens will save and create a local backup " @debounce-save-time! " seconds after your last edit.")])]]]))))
