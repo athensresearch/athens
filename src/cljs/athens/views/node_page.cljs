athensresearch/athens

commit a51e001cd4543b389460b1def17446230f51062c

fix(title): shift-click on title should open in right sidebar (#802)
close #775

Co-authored-by: jeff <tangj1122@gmail.com>

src/cljs/athens/views/node_page.cljs
@@ -535,7 +535,7 @@
                           :class    "page-header"
                           :on-click (fn [e]
                                       (.. e preventDefault)
-                                      (if daily-note?
+                                      (if (.. e -shiftKey)
                                         (navigate-uid uid e)
                                         (dispatch [:editing/uid uid])))})
           ;; Prevent editable textarea if a node/title is a date

src/cljs/athens/views/block_page.cljs
@@ -126,7 +126,9 @@
                 (use-style title-style {:data-uid uid :class "block-header"})
                 {:on-click (fn [e]
                              (.. e preventDefault)
-                             (dispatch [:editing/uid uid]))})
+                             (if (.. e -shiftKey)
+                               (navigate-uid uid e)
+                               (dispatch [:editing/uid uid])))})
           [autosize/textarea
            {:id          (str "editable-uid-" uid)
             :value       (:string/local @state)

commit 8f04d351c9f0fd74b6cefbf8f3211dcc4237b0ce

fix: clicking the date doesn't open the date page
fixing an issue introduced in #802

src/cljs/athens/views/node_page.cljs
@@ -535,7 +535,7 @@
                           :class    "page-header"
                           :on-click (fn [e]
                                       (.. e preventDefault)
-                                      (if (.. e -shiftKey)
+                                      (if (or daily-note? (.. e -shiftKey))
                                         (navigate-uid uid e)
                                         (dispatch [:editing/uid uid])))})
           ;; Prevent editable textarea if a node/title is a date