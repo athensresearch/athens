ed3813a46ee2f9ff28dccff97c2a5902a1a9fd93
Alex Hubert Iwaniuk <alex@happyhacking.nl>
2021-04-03 18:45:46 +0200

    paragraphs

--- src/cljc/athens/parser/impl.cljc
+++ src/cljc/athens/parser/impl.cljc
@@ -5,48 +5,64 @@
   2nd pass: inline structure
   3rd pass: raw urls"
   (:require
-   [clojure.string :as string]
-   #?(:cljs [instaparse.core :as insta :refer-macros [defparser]]
-      :clj  [instaparse.core :as insta :refer [defparser]])))
+    [clojure.string :as string]
+    #?(:cljs [instaparse.core :as insta :refer-macros [defparser]]
+       :clj  [instaparse.core :as insta :refer [defparser]])))
+
 
 (defparser block-parser
   "block = (thematic-break / 
             heading / 
             indented-code-block /
-            fenced-code-block)*
+            fenced-code-block /
+            paragraph-text)*
    thematic-break = #'[*_-]{3}'
    heading = #'[#]+' <space> paragraph-text
    indented-code-block = (<'    '> code-text)+
    fenced-code-block = <'```'> #'(?s).+(?=(```|\\n))'+ <'```'>
 
-   paragraph-text = #'.+' <blankline | newline>?
+   paragraph-text = (<#' *'> #'.+' <newline>?)+ <blankline>?
    code-text = #'.+' <newline>?
    space = ' '
    blankline = #'\\n\\n'
    newline = #'\\n'")
 
-(defn- transform-heading [atx p-text]
+
+(defn- transform-heading
+  [atx p-text]
   [:heading {:n (count atx)}
    p-text])
 
-(defn- transform-indented-code-block [& code-texts]
+
+(defn- transform-indented-code-block
+  [& code-texts]
   [:indented-code-block
    [:code-text (->> code-texts
                     (map second)
                     (string/join "\n"))]])
 
-(defn- transform-fenced-code-block [code-text]
+
+(defn- transform-fenced-code-block
+  [code-text]
   (let [lang (-> code-text
                  (string/split #"\n")
                  first)
         text (string/join
-              "\n"
-              (-> code-text
-                  (string/split #"\n")
-                  rest))]
+               "\n"
+               (-> code-text
+                   (string/split #"\n")
+                   rest))]
     (println "fenced:" (pr-str code-text) :-> (pr-str lang) (pr-str text))
     [:fenced-code-block {:lang lang}
      [:code-text text]]))
+
+
+(defn- transform-paragraph-text
+  [& strings]
+  [:paragraph-text (->> strings
+                        (map string/trim)
+                        (string/join "\n"))])
+
 
 (defn block-parser->ast
   "Parse `in` string with `block-parser`."
@@ -55,4 +71,5 @@
        block-parser
        (insta/transform {:heading             transform-heading
                          :indented-code-block transform-indented-code-block
-                         :fenced-code-block   transform-fenced-code-block})))
+                         :fenced-code-block   transform-fenced-code-block
+                         :paragraph-text      transform-paragraph-text})))
--- test/athens/parser/impl_test.clj
+++ test/athens/parser/impl_test.clj
@@ -1,67 +1,114 @@
 (ns athens.parser.impl-test
   (:require
-   [athens.parser.impl :as sut]
-   [clojure.test :as t :refer [deftest is are testing]]
-   [instaparse.core :as insta]))
+    [athens.parser.impl :as sut]
+    [clojure.test :as t :refer [deftest is are testing]]
+    [instaparse.core :as insta]))
+
+
+(defmacro parses-to
+  [parser & tests]
+  `(t/are [in# out#] (= out# (~parser in#))
+     ~@tests))
+
 
 (t/deftest block-structure
 
   (t/testing "that headings are parsed"
 
-    (t/are [in out] (= out (sut/block-parser->ast in))
+    (parses-to sut/block-parser->ast
 
-      "# Heading"
-      [:block [:heading {:n 1}
-               [:paragraph-text "Heading"]]]
+               "# Heading"
+               [:block [:heading {:n 1}
+                        [:paragraph-text "Heading"]]]
 
-      "# Heading\n\n"
-      [:block [:heading {:n 1}
-               [:paragraph-text "Heading"]]]
+               "# Heading\n\n"
+               [:block [:heading {:n 1}
+                        [:paragraph-text "Heading"]]]
 
-      "### Heading\n"
-      [:block [:heading {:n 3}
-               [:paragraph-text "Heading"]]]))
+               "### Heading\n"
+               [:block [:heading {:n 3}
+                        [:paragraph-text "Heading"]]]))
 
   (t/testing "that thematic-breaks are parsed"
 
-    (t/are [in out] (= out (sut/block-parser->ast in))
-      "***"
-      [:block [:thematic-break "***"]]
+    (parses-to sut/block-parser->ast
 
-      "---"
-      [:block [:thematic-break "---"]]
+               "***"
+               [:block [:thematic-break "***"]]
 
-      "___"
-      [:block [:thematic-break "___"]]))
+               "---"
+               [:block [:thematic-break "---"]]
+
+               "___"
+               [:block [:thematic-break "___"]]))
 
   (t/testing "that indented-code-blocks are parsed"
 
-    (t/are [in out] (= out (sut/block-parser->ast in))
+    (parses-to sut/block-parser->ast
 
-      "    some code"
-      [:block [:indented-code-block
-               [:code-text "some code"]]]
+               "    some code"
+               [:block [:indented-code-block
+                        [:code-text "some code"]]]
 
-      "    multiline\n    code"
-      [:block [:indented-code-block
-               [:code-text "multiline\ncode"]]]
+               "    multiline\n    code"
+               [:block [:indented-code-block
+                        [:code-text "multiline\ncode"]]]
 
-      "    multiline\n    code\n      with indentation"
-      [:block [:indented-code-block
-               [:code-text "multiline\ncode\n  with indentation"]]]))
+               "    multiline\n    code\n      with indentation"
+               [:block [:indented-code-block
+                        [:code-text "multiline\ncode\n  with indentation"]]]))
 
   (t/testing "that fenced-code-blocks are parsed"
 
-    (t/are [in out] (= out (sut/block-parser->ast in))
+    (parses-to sut/block-parser->ast
 
-      "```\nsome code```"
-      [:block [:fenced-code-block {:lang ""}
-               [:code-text "some code"]]]
+               "```\nsome code```"
+               [:block [:fenced-code-block {:lang ""}
+                        [:code-text "some code"]]]
 
-      "```javascript\nvar a = 1;\n```"
-      [:block [:fenced-code-block {:lang "javascript"}
-               [:code-text "var a = 1;"]]]
+               "```javascript\nvar a = 1;\n```"
+               [:block [:fenced-code-block {:lang "javascript"}
+                        [:code-text "var a = 1;"]]]
 
-      "```javascript\nvar a = \"with` ticks`\";\nand multiline```"
-      [:block [:fenced-code-block {:lang "javascript"}
-               [:code-text "var a = \"with` ticks`\";\nand multiline"]]])))
+               "```javascript\nvar a = \"with` ticks`\";\nand multiline```"
+               [:block [:fenced-code-block {:lang "javascript"}
+                        [:code-text "var a = \"with` ticks`\";\nand multiline"]]]))
+
+  (t/testing "that paragraphs are parsed"
+
+    (parses-to sut/block-parser->ast
+
+               "aaa"
+               [:block [:paragraph-text "aaa"]]
+
+               "aaa\n\nbbb"
+               [:block
+                [:paragraph-text "aaa"]
+                [:paragraph-text "bbb"]]
+
+               "aaa\nbbb\n\nccc\nddd"
+               [:block
+                [:paragraph-text "aaa\nbbb"]
+                [:paragraph-text "ccc\nddd"]]
+
+               "aaa\n\n\nbbb"
+               [:block
+                [:paragraph-text "aaa"]
+                [:paragraph-text "bbb"]]
+
+               "  aaa\n bbb" ;; leading spaces are skipped
+               [:block [:paragraph-text "aaa\nbbb"]]
+
+               "aaa\n    bbb\n        ccc"
+               [:block [:paragraph-text "aaa\nbbb\nccc"]]
+
+               "   aaa\nbbb" ;; 3 spaces max
+               [:block [:paragraph-text "aaa\nbbb"]]
+
+               "    aaa\nbbb" ;; or code block is triggered
+               [:block
+                [:indented-code-block [:code-text "aaa"]]
+                [:paragraph-text "bbb"]]
+
+               "aaa    \nbbb    " ;; final spaces are stripped
+               [:block [:paragraph-text "aaa\nbbb"]])))
