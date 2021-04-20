4fff7048c9a295f69be0debaa8a657f12961b573
jeff <tangj1122@gmail.com>
2021-04-19 18:46:49 -0700

    fix(right-sidebar): scroll to top if open another page on right (#992)

--- src/cljs/athens/events.cljs
+++ src/cljs/athens/events.cljs
@@ -265,9 +265,15 @@
                                               (compare
                                                 [(get-in new-items [k1 :index]) k2]
                                                 [(get-in new-items [k2 :index]) k1]))) inc-items)]
-      (cond-> {:db (assoc db :right-sidebar/items sorted-items)}
-        (not (:right-sidebar/open db))
-        (assoc :dispatch [:right-sidebar/toggle])))))
+      {:db         (assoc db :right-sidebar/items sorted-items)
+       :dispatch-n [(when (not (:right-sidebar/open db)) [:right-sidebar/toggle])
+                    [:right-sidebar/scroll-top]]})))
+
+
+(reg-event-fx
+  :right-sidebar/scroll-top
+  (fn []
+    {:right-sidebar/scroll-top nil}))
 
 
 (reg-event-fx
