17fe6838db7abebbd183c467880b2760b351da31
shanberg <shanberg@users.noreply.github.com>
2021-04-24 16:42:28 -0400

    rfct: minor refactor of all-pages styles

--- src/cljs/athens/views/pages/all_pages.cljs
+++ src/cljs/athens/views/pages/all_pages.cljs
@@ -29,40 +29,40 @@
    :margin "0 2rem"
    :text-align "left"
    :border-collapse "collapse"
-   ::stylefy/sub-styles {:th-date {:text-align "right"}
-                         :td-title {:color (color :link-color)
-                                    :width "15vw"
-                                    :cursor "pointer"
-                                    :min-width "10em"
-                                    :word-break "break-word"
-                                    :font-weight "500"
-                                    :font-size "1.3125em"
-                                    :line-height "1.28"}
-                         :td-links {:font-size "1em"
-                                    :text-align "center"}
-                         :body-preview {:white-space "wrap"
-                                        :word-break "break-word"
-                                        :overflow "hidden"
-                                        :text-overflow "ellipsis"
-                                        :display "-webkit-box"
-                                        :-webkit-line-clamp "3"
-                                        :-webkit-box-orient "vertical"}
-                         :td-date {:text-align "right"
-                                   :opacity (:opacity-high OPACITIES)
-                                   :font-size "0.75em"
-                                   :min-width "9em"}}
    ::stylefy/manual [[:tbody {:vertical-align "top"}
                       [:tr {:transition "background 0.1s ease"}
                        [:td {:border-top (str "1px solid " (color :border-color))
                              :transition "box-shadow 0.1s ease"}
+                        [:&.title {:color (color :link-color)
+                                   :width "15vw"
+                                   :cursor "pointer"
+                                   :min-width "10em"
+                                   :word-break "break-word"
+                                   :font-weight "500"
+                                   :font-size "1.3125em"
+                                   :line-height "1.28"}]
+                        [:&.links {:font-size "1em"
+                                   :text-align "center"}]
+                        [:&.body-preview {:white-space "wrap"
+                                          :word-break "break-word"
+                                          :overflow "hidden"
+                                          :text-overflow "ellipsis"
+                                          :display "-webkit-box"
+                                          :-webkit-line-clamp "3"
+                                          :-webkit-box-orient "vertical"}
+                         [:span {:border "1px solid"}]]
+                        [:&.date {:text-align "right"
+                                   :opacity (:opacity-high OPACITIES)
+                                   :font-size "0.75em"
+                                   :min-width "9em"}]
                         [(selectors/& (selectors/first-child)) {:border-radius "0.5rem 0 0 0.5rem"
                                                                 :box-shadow "-1rem 0 transparent"}]
                         [(selectors/& (selectors/last-child)) {:border-radius "0 0.5rem 0.5rem 0"
                                                                :box-shadow "1rem 0 transparent"}]]
                        [:&:hover {:background-color (color :background-minus-1 :opacity-med)
                                   :border-radius "0.5rem"}
-                        [:td [(selectors/& (selectors/first-child)) {:box-shadow [["-1rem 0 " (color :background-minus-1 :opacity-med)]]}]]
-                        [:td [(selectors/& (selectors/last-child)) {:box-shadow [["1rem 0 " (color :background-minus-1 :opacity-med)]]}]]]]]
+                        [:td [(selectors/& (selectors/first-child)) {:box-shadow [["-1rem 0 " (color :backguround-minus-1 :opacity-med)]]}]]
+                        [:td [(selectors/& (selectors/last-child)) {:box-shadow [["1rem 0 " (color :backgrond-minus-1 :opacity-med)]]}]]]]]
                      [:td :th {:padding "0.5rem"}]
                      [:th [:h5 {:opacity (:opacity-med OPACITIES)}]]]})
 
@@ -95,8 +95,8 @@
            (for [page @pages]
              (let [{:keys [block/uid node/title block/children block/_refs] modified :edit/time created :create/time} page]
                [:tr {:key uid}
-                [:td (use-sub-style table-style :td-title {:on-click #(navigate-uid uid %)}) title]
-                [:td (use-sub-style table-style :td-links) (count _refs)]
-                [:td [:div (use-sub-style table-style :body-preview) (str/join " ") (map #(str "• " (:block/string %)) children)]]
-                [:td (use-sub-style table-style :td-date) (date-string modified)]
-                [:td (use-sub-style table-style :td-date) (date-string created)]])))]]])))
+                [:td {:class "title" :on-click #(navigate-uid uid %)} title]
+                [:td {:class "links"} (count _refs)]
+                [:td [:div {:class "body-preview"} (map #(vector [:span :block/string %]) children)]]
+                [:td {:class "date"} (date-string modified)]
+                [:td {:class "date"} (date-string created)]])))]]])))
