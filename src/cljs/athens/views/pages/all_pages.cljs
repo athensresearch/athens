6a308c5ab024502f1955a9965bc6ba15c5d77578
Stuart Hanberg <shanberg@users.noreply.github.com>
2021-04-24 21:25:42 -0400

    enhance(all-pages): hide empty block bullets in all pages view (#1040)
    Co-authored-by: shanberg <98312+shanberg@users.noreply.github.com>
    Co-authored-by: jeff <tangj1122@gmail.com>

--- src/cljs/athens/views/pages/all_pages.cljs
+++ src/cljs/athens/views/pages/all_pages.cljs
@@ -6,7 +6,6 @@
     [athens.util :refer [date-string]]
     [cljsjs.react]
     [cljsjs.react.dom]
-    [clojure.string :as str]
     [datascript.core :as d]
     [garden.selectors :as selectors]
     [posh.reagent :as p]
@@ -29,40 +28,44 @@
    :margin "0 2rem"
    :text-align "left"
    :border-collapse "collapse"
-   ::stylefy/sub-styles {:th-date {:text-align "right"}
-                         :td-title {:color (color :link-color)
-                                    :width "15vw"
-                                    :cursor "pointer"
-                                    :min-width "10em"
-                                    :word-break "break-word"
-                                    :font-weight "500"
-                                    :font-size "1.3125em"
-                                    :line-height "1.28"}
-                         :td-links {:font-size "1em"
-                                    :text-align "center"}
-                         :body-preview {:white-space "wrap"
-                                        :word-break "break-word"
-                                        :overflow "hidden"
-                                        :text-overflow "ellipsis"
-                                        :display "-webkit-box"
-                                        :-webkit-line-clamp "3"
-                                        :-webkit-box-orient "vertical"}
-                         :td-date {:text-align "right"
-                                   :opacity (:opacity-high OPACITIES)
-                                   :font-size "0.75em"
-                                   :min-width "9em"}}
    ::stylefy/manual [[:tbody {:vertical-align "top"}
                       [:tr {:transition "background 0.1s ease"}
                        [:td {:border-top (str "1px solid " (color :border-color))
                              :transition "box-shadow 0.1s ease"}
-                        [(selectors/& (selectors/first-child)) {:border-radius "0.5rem 0 0 0.5rem"
-                                                                :box-shadow "-1rem 0 transparent"}]
-                        [(selectors/& (selectors/last-child)) {:border-radius "0 0.5rem 0.5rem 0"
-                                                               :box-shadow "1rem 0 transparent"}]]
+                        [:&.title {:color (color :link-color)
+                                   :width "15vw"
+                                   :cursor "pointer"
+                                   :min-width "10em"
+                                   :word-break "break-word"
+                                   :font-weight "500"
+                                   :font-size "1.3125em"
+                                   :line-height "1.28"}]
+                        [:&.links {:font-size "1em"
+                                   :text-align "center"}]
+                        [:&.body-preview {:word-break "break-word"
+                                          :overflow "hidden"
+                                          :text-overflow "ellipsis"
+                                          :display "-webkit-box"
+                                          :-webkit-mask "linear-gradient(to bottom, #fff calc(100% - 1em), transparent)"
+                                          :-webkit-line-clamp "3"
+                                          :-webkit-box-orient "vertical"}
+                         [:span:empty {:display "none"}]
+                         [(selectors/+ :span :span)
+                          [:&:before {:content "'•'"
+                                      :margin-inline "0.5em"
+                                      :opacity (:opacity-low OPACITIES)}]]]
+                        [:&.date {:text-align "right"
+                                  :opacity (:opacity-high OPACITIES)
+                                  :font-size "0.75em"
+                                  :min-width "9em"}]
+                        [:&:first-child {:border-radius "0.5rem 0 0 0.5rem"
+                                         :box-shadow "-1rem 0 transparent"}]
+                        [:&:last-child {:border-radius "0 0.5rem 0.5rem 0"
+                                        :box-shadow "1rem 0 transparent"}]]
                        [:&:hover {:background-color (color :background-minus-1 :opacity-med)
                                   :border-radius "0.5rem"}
-                        [:td [(selectors/& (selectors/first-child)) {:box-shadow [["-1rem 0 " (color :background-minus-1 :opacity-med)]]}]]
-                        [:td [(selectors/& (selectors/last-child)) {:box-shadow [["1rem 0 " (color :background-minus-1 :opacity-med)]]}]]]]]
+                        [:td [:&:first-child {:box-shadow [["-1rem 0 " (color :background-minus-1 :opacity-med)]]}]]
+                        [:td [:&:last-child {:box-shadow [["1rem 0 " (color :background-minus-1 :opacity-med)]]}]]]]]
                      [:td :th {:padding "0.5rem"}]
                      [:th [:h5 {:opacity (:opacity-med OPACITIES)}]]]})
 
@@ -95,8 +98,8 @@
            (for [page @pages]
              (let [{:keys [block/uid node/title block/children block/_refs] modified :edit/time created :create/time} page]
                [:tr {:key uid}
-                [:td (use-sub-style table-style :td-title {:on-click #(navigate-uid uid %)}) title]
-                [:td (use-sub-style table-style :td-links) (count _refs)]
-                [:td [:div (use-sub-style table-style :body-preview) (str/join " ") (map #(str "• " (:block/string %)) children)]]
-                [:td (use-sub-style table-style :td-date) (date-string modified)]
-                [:td (use-sub-style table-style :td-date) (date-string created)]])))]]])))
+                [:td {:class "title" :on-click #(navigate-uid uid %)} title]
+                [:td {:class "links"} (count _refs)]
+                [:td {:class "body-preview"} (map (fn [child] [:span (:block/string child)]) children)]
+                [:td {:class "date"} (date-string modified)]
+                [:td {:class "date"} (date-string created)]])))]]])))
