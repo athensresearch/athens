6890535f6cf001153d4a942f1071b4eec7adf32f
shanberg <98312+shanberg@users.noreply.github.com>
2021-05-16 15:54:59 -0400

    fix: revert whitespace change

--- src/cljs/athens/electron.cljs
+++ src/cljs/athens/electron.cljs
@@ -375,57 +375,57 @@
                                                    #_[:local-storage/navigate]]}
 
                                      ;; whether first or nth time, update athens pages
-                                    #_{:when       :seen-any-of?
-                                       :events     [:fs/create-new-db :reset-conn]
-                                       :dispatch-n [[:db/retract-athens-pages]
-                                                    [:db/transact-athens-pages]]}
+                                     #_{:when       :seen-any-of?
+                                        :events     [:fs/create-new-db :reset-conn]
+                                        :dispatch-n [[:db/retract-athens-pages]
+                                                     [:db/transact-athens-pages]]}
 
                                     ;; bind windows toolbar electron buttons
-                                    {:when       :seen-any-of?
-                                     :events     [:fs/create-new-db :reset-conn]
-                                     :dispatch   [:bind-win-listeners]}
+                                     {:when       :seen-any-of?
+                                      :events     [:fs/create-new-db :reset-conn]
+                                      :dispatch   [:bind-win-listeners]}
 
 
-                                    {:when        :seen-any-of?
-                                     :events      [:fs/create-new-db :reset-conn]
+                                     {:when        :seen-any-of?
+                                      :events      [:fs/create-new-db :reset-conn]
                                       ;; if schema is nil, update to 1 and reparse all block/string's for links
-                                     :dispatch-fn (fn [_]
-                                                    (let [schemas    (d/q '[:find ?e ?v
-                                                                            :where [?e :schema/version ?v]]
-                                                                          @db/dsdb)
-                                                          schema-cnt (count schemas)]
-                                                      (cond
-                                                        (= 0 schema-cnt) (let [linked-ref-pattern      (patterns/linked ".*")
-                                                                               blocks-with-plain-links (d/q '[:find ?u ?s
-                                                                                                              :keys block/uid block/string
-                                                                                                              :in $ ?pattern
-                                                                                                              :where
-                                                                                                              [?e :block/uid ?u]
-                                                                                                              [?e :block/string ?s]
-                                                                                                              [(re-find ?pattern ?s)]]
-                                                                                                            @db/dsdb
-                                                                                                            linked-ref-pattern)
-                                                                               blocks-orig             (map (fn [{:block/keys [uid string]}]
-                                                                                                              {:db/id [:block/uid uid] :block/string string})
-                                                                                                            blocks-with-plain-links)
-                                                                               blocks-temp             (map (fn [{:block/keys [uid]}]
-                                                                                                              {:db/id [:block/uid uid] :block/string ""})
-                                                                                                            blocks-with-plain-links)]
+                                      :dispatch-fn (fn [_]
+                                                     (let [schemas    (d/q '[:find ?e ?v
+                                                                             :where [?e :schema/version ?v]]
+                                                                           @db/dsdb)
+                                                           schema-cnt (count schemas)]
+                                                       (cond
+                                                         (= 0 schema-cnt) (let [linked-ref-pattern      (patterns/linked ".*")
+                                                                                blocks-with-plain-links (d/q '[:find ?u ?s
+                                                                                                               :keys block/uid block/string
+                                                                                                               :in $ ?pattern
+                                                                                                               :where
+                                                                                                               [?e :block/uid ?u]
+                                                                                                               [?e :block/string ?s]
+                                                                                                               [(re-find ?pattern ?s)]]
+                                                                                                             @db/dsdb
+                                                                                                             linked-ref-pattern)
+                                                                                blocks-orig             (map (fn [{:block/keys [uid string]}]
+                                                                                                               {:db/id [:block/uid uid] :block/string string})
+                                                                                                             blocks-with-plain-links)
+                                                                                blocks-temp             (map (fn [{:block/keys [uid]}]
+                                                                                                               {:db/id [:block/uid uid] :block/string ""})
+                                                                                                             blocks-with-plain-links)]
                                                                             ;; give all blocks empty string - clears refs
                                                                             ;; give all blocks their original string - adds refs (for the period of time where block/refs were not added to db
                                                                             ;; update schema version, so this doesn't need to happen again
-                                                                           (dispatch [:transact blocks-temp])
-                                                                           (dispatch [:transact blocks-orig])
-                                                                           (dispatch [:transact [[:db/add -1 :schema/version 1]]]))
-                                                        (= 1 schema-cnt) (let [schema-version (-> schemas first second)]
-                                                                           (case schema-version
-                                                                             1 (prn (str "Schema version " schema-version))
-                                                                             (js/alert (js/Error (str "No matching case clause for schema version: " schema-version)))))
-                                                        (< 1 schema-cnt)
-                                                        (js/alert (js/Error (str "Multiple schema versions: " schemas))))
+                                                                            (dispatch [:transact blocks-temp])
+                                                                            (dispatch [:transact blocks-orig])
+                                                                            (dispatch [:transact [[:db/add -1 :schema/version 1]]]))
+                                                         (= 1 schema-cnt) (let [schema-version (-> schemas first second)]
+                                                                            (case schema-version
+                                                                              1 (prn (str "Schema version " schema-version))
+                                                                              (js/alert (js/Error (str "No matching case clause for schema version: " schema-version)))))
+                                                         (< 1 schema-cnt)
+                                                         (js/alert (js/Error (str "Multiple schema versions: " schemas))))
 
-                                                      (dispatch [:loading/unset])))
-                                     :halt?       true}]}}))
+                                                       (dispatch [:loading/unset])))
+                                      :halt?       true}]}}))
 
 
   (reg-event-fx
