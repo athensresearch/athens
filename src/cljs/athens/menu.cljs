237f74d6e666d845487f1edf843fca3bf4615986
shanberg <98312+shanberg@users.noreply.github.com>
2021-05-23 07:14:58 -0400

    chore: fix linter issues

--- src/cljs/athens/menu.cljs
+++ src/cljs/athens/menu.cljs
@@ -8,14 +8,14 @@
 
 (def menu-template
   (clj->js (into [] (concat
-                     (when isMac [{:role "appMenu"}])
-                     [{:role "fileMenu"}
-                      {:role "editMenu"}
-                      {:label "View"
-                       :submenu [{:role "reload"}
-                                 {:role "forceReload"}
-                                 {:role "toggleDevTools"}
-                                 {:type "separator"}
+                      (when isMac [{:role "appMenu"}])
+                      [{:role "fileMenu"}
+                       {:role "editMenu"}
+                       {:label "View"
+                        :submenu [{:role "reload"}
+                                  {:role "forceReload"}
+                                  {:role "toggleDevTools"}
+                                  {:type "separator"}
                                  ;; Default zoom tools disabled so we can own
                                  ;; zoom control internally. It would be better
                                  ;; to remap these items to commands which
@@ -24,13 +24,13 @@
                                  ;;  {:role "resetZoom"}
                                  ;;  {:role "zoomIn"}
                                  ;;  {:role "zoomOut"}
-                                 {:type "separator"}
-                                 {:role "togglefullscreen"}]}
-                      {:role "windowMenu"}]
-                     [(if isMac
-                        {:role "help"
-                         :submenu [{:label "Learn More"
-                                    :click #(.openExternal shell "https://github.com/athensresearch/athens")}]}
-                        {:label "Help"
-                         :submenu [{:label "Learn More"
-                                    :click #(.openExternal shell "https://github.com/athensresearch/athens")}]})]))))
+                                  {:type "separator"}
+                                  {:role "togglefullscreen"}]}
+                       {:role "windowMenu"}]
+                      [(if isMac
+                         {:role "help"
+                          :submenu [{:label "Learn More"
+                                     :click #(.openExternal shell "https://github.com/athensresearch/athens")}]}
+                         {:label "Help"
+                          :submenu [{:label "Learn More"
+                                     :click #(.openExternal shell "https://github.com/athensresearch/athens")}]})]))))
