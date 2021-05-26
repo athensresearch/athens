ae265992cd0af0d693b19f41420c608d61a011c4
julio <julioocz@gmail.com>
2021-05-26 08:11:17 -0500

    refactor(keybindings): Re-write mousetrap using reagent/create-class

--- src/cljs/athens/hotkeys.cljs
+++ src/cljs/athens/hotkeys.cljs
@@ -12,45 +12,45 @@
 ;; Returns a sequence with functions that unbind the bindings.
 (defn mousetrap-bind-all
   [mousetrap-instance bindings]
-  (doall
-    (map
-      (fn [[binding callback]]
-        (.bind mousetrap-instance binding
-               (fn [event]
-                 (.stopPropagation event)
-                 (callback event)))
-        #(.unbind mousetrap-instance binding))
-      bindings)))
+  (let [unbind-fns (doall
+                     (map
+                       (fn [[binding callback]]
+                         (let [binding-converted (clj->js binding)]
+                           (.bind mousetrap-instance binding-converted
+                                  (fn [event]
+                                    (.stopPropagation event)
+                                    (callback event)))
+                           #(.unbind mousetrap-instance binding-converted)))
+                       bindings))]
+    #(doall (map apply unbind-fns))))
 
-
-;; Not really sure if this does the trick.
-;; The idea is that it returns the same JS array
-;; for the same cljs vector (using = for equality)
-(def deps (memoize clj->js))
-
-;; Ugly but working mousetrap version using hooks.
 (defn mousetrap
-  [propsjs]
-  (let [mouse-trap (react/useRef)
-        props (js->clj propsjs)]
-    (react/useEffect
-      (fn []
-        (let [m (.-current mouse-trap)
-              bindings (get props "bindings")
-              cleanup-fns (mousetrap-bind-all m bindings)]
-          #(doall
-             (map apply cleanup-fns))))
-      (deps [(get props "bindings")]))
-    (react/createElement "span"
-                         #js{:ref #(set! (.-current mouse-trap) (new Mousetrap %))}
-                         (.-children propsjs))))
+  [bindings _]
+  (let [mousetrap-instance (r/atom nil)
+        unbind-hotkeys (r/atom #())
+        bind-hotkeys #(reset! unbind-hotkeys
+                              (mousetrap-bind-all @mousetrap-instance bindings))]
+    (r/create-class
+      {:display-name
+       "mousetrap"
 
+       :component-did-mount
+       bind-hotkeys
 
-(def modkey
-  (let [os (util/get-os)]
-    (or (and (= os :mac) "meta")
-        (and (= os :windows) "ctrl")
-        (and (= os :linux) "ctrl"))))
+       :component-did-update
+       (fn [this old-argv]
+         (let [bindings (second (r/argv this))
+               prev-bindings (second old-argv)]
+           (when (not= bindings prev-bindings)
+             (@unbind-hotkeys)
+             (bind-hotkeys))))
+
+       :reagent-render
+       (fn [_ children]
+         [:span
+          {:ref #(reset! mousetrap-instance (new Mousetrap %))}
+          children])})))
+
 
 
 (defn not-editing
@@ -78,27 +78,21 @@
 ;; :10x-toggle            "mod+t"
 ;; :nav-back              "alt+left"
 ;; :nav-forward           "alt+right"
-;; :nav-daily-notes       "alt+d"
 ;; :undo                  "mod+z"
 ;; :redo                  "mod+shift+z")
 ;; :left-sidebar-toggle   "mod+\\"
 ;; :right-sidebar-toggle  "mod+shift+\\"
 ;; TODO: Integrate with re-frame
 (mousetrap-bind-all Mousetrap
-                    {"mod+k"       #(dispatch [:athena/toggle])
+                    {["up" "down"] #(prn "test")
+                     "mod+k"       #(dispatch [:athena/toggle])
                      "mod+g"       #(dispatch [:devtool/toggle])
                      "mod+s"       #(dispatch [:save])
                      "mod+t"       util/toggle-10x
                      "alt+left"    #(when (not-editing) (.back js/window.history))
                      "alt+right"   #(when (not-editing) (.forward js/window.history))
-                     "mod+1"       #(dispatch [:left-sidebar/toggle]) ;; TODO: Change to "mod+\\"
-                     "mod+2"       #(dispatch [:right-sidebar/toggle]) ;; TODO: Change to "mod+shift+\\"
+                     "mod+1"       #(dispatch [:left-sidebar/toggle])         ;; TODO: Change to "mod+\\"
+                     "mod+2"       #(dispatch [:right-sidebar/toggle])        ;; TODO: Change to "mod+shift+\\"
                      "mod+z"       #(maybe-undo-or-redo :undo)
                      "mod+shift+z" #(maybe-undo-or-redo :redo)
                      "alt+d"       router/nav-daily-notes})
-
-;; Maybe change name or remove
-;; TODO: Remove this
-(defn hotkeys
-  [children]
-  [:span children])
