3889d6318228f02dedbdea632efe4af7c6ae7ab7
shanberg <shanberg@users.noreply.github.com>
2021-06-01 22:04:39 -0400

    fix: child drop-area-indicator shoudln't squish or be misplaced

--- src/cljs/athens/views/blocks/drop_area_indicator.cljs
+++ src/cljs/athens/views/blocks/drop_area_indicator.cljs
@@ -1,6 +1,6 @@
 (ns athens.views.blocks.drop-area-indicator
   (:require
-    [athens.style :as style :refer [OPACITIES]]
+    [athens.style :refer [color OPACITIES]]
     [stylefy.core :as stylefy]))
 
 
@@ -13,9 +13,9 @@
 
 (stylefy/keyframes "drop-area-color-pulse"
                    [:from
-                    {:opacity (:opacity-lower style/OPACITIES)}]
+                    {:opacity (:opacity-lower OPACITIES)}]
                    [:to
-                    {:opacity (:opacity-med style/OPACITIES)}])
+                    {:opacity (:opacity-med OPACITIES)}])
 
 
 (def drop-area-indicator-style
@@ -24,34 +24,39 @@
    :pointer-events "none"
    :margin-bottom "-1px"
    :opacity (:opacity-high OPACITIES)
-   :color (style/color :link-color)
-   :animation "drop-area-appear 0.2s both"
+   :color (color :link-color)
    :position "relative"
    :transform-origin "left"
    :z-index 3
    :width "100%"
+   ;; Can't animate this, because the animation
+   ;; fires at too many states. If drop-area-indicator
+   ;; location is determined by order in the current
+   ;; page or block then we can reactivate animation.
+   ;; :animation "drop-area-appear 0.2s both"
    ::stylefy/manual [["&:after" {:position "absolute"
                                  :content "''"
                                  :top "-0.5px"
                                  :right "0"
                                  :bottom "-0.5px"
-                                 :left "2em"
+                                 :left "calc(2em - 4px)"
                                  :border-radius "100px"
                                  :background "currentColor"}]
-                     ["&.child" {:--indent "2rem"
+                     ["&.child" {:--indent "1.95em"
                                  :width "calc(100% - var(--indent))"
                                  :margin-left "var(--indent)"}]
                      ["&.child:after" {:border-top-left-radius 0
                                        :border-bottom-left-radius 0}]
                      ["&.child:before" {:position "absolute"
                                         :content "''"
-                                        :width "0.3rem"
-                                        :height "0.3rem"
                                         :border-radius "10em"
+                                        :border "2px solid "
+                                        :--size "4px"
+                                        :width "var(--size)"
+                                        :height "var(--size)"
                                         :left "var(--indent)"
                                         :top "50%"
-                                        :transform "translateY(-50%) translateX(-0.3rem))"
-                                        :border "2px solid "}]]})
+                                        :transform "translateY(-50%) translateX(-100%) translateX(-2px)"}]]})
 
 
 (defn drop-area-indicator
