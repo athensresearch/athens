8b41a2672275e02c88c4ec73b7068d5f5da464e5
jeff <tangj1122@gmail.com>
2021-06-22 19:12:59 -0400

    send-editing only if remote

--- src/cljs/athens/events.cljs
+++ src/cljs/athens/events.cljs
@@ -289,9 +289,11 @@
 (reg-event-fx
   :editing/uid
   (fn [{:keys [db]} [_ uid index]]
-    {:db            (assoc db :editing/uid uid)
-     :editing/focus [uid index]
-     :presence/send-editing uid}))
+    (let [remote? (client/open?)]
+      (cond->
+        {:db                    (assoc db :editing/uid uid)
+         :editing/focus         [uid index]}
+        remote? (assoc :presence/send-editing uid)))))
 
 
 (reg-event-fx
