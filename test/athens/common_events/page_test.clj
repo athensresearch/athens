9f66cd90b514d8a30893733775a8e917ed7fad37
Alex Hubert Iwaniuk <alex@happyhacking.nl>
2021-07-05 12:17:35 +0200

    Lan-Party: problematic query after page rename test.

--- test/athens/common_events/page_test.clj
+++ test/athens/common_events/page_test.clj
@@ -1,5 +1,6 @@
 (ns athens.common-events.page-test
   (:require
+    [athens.common-db              :as common-db]
     [athens.common-events          :as common-events]
     [athens.common-events.fixture  :as fixture]
     [athens.common-events.resolver :as resolver]
@@ -27,6 +28,66 @@
                         @@fixture/connection test-uid)]
       (test/is (seq e-by-title))
       (test/is (= e-by-title e-by-uid)))))
+
+
+(test/deftest rename-page
+  (test/testing "simple case, no string representations need updating"
+    (let [test-page-uid   "test-page-1-1-uid"
+          test-title-from "test page 1 title from"
+          test-title-to   "test page 1 title to"
+          test-block-uid  "test-block-1-1-uid"
+          setup-txs       [{:db/id          -1
+                            :node/title     test-title-from
+                            :block/uid      test-page-uid
+                            :block/children [{:db/id          -2
+                                              :block/uid      test-block-uid
+                                              :block/string   ""
+                                              :block/order    0
+                                              :block/children []}]}]]
+      ;; need to apply linkmaker, so resolving page-rename event can follow references for :block/string changes
+      (d/transact @fixture/connection (common-db/linkmaker @@fixture/connection setup-txs))
+      (let [uid-by-title    (common-db/v-by-ea @@fixture/connection [:node/title test-title-from] :block/uid)
+            rename-page-txs (resolver/resolve-event-to-tx @@fixture/connection
+                                                          (common-events/build-page-rename-event -1
+                                                                                                 test-page-uid
+                                                                                                 test-title-from
+                                                                                                 test-title-to))]
+        (test/is (= test-page-uid uid-by-title))
+        (d/transact @fixture/connection rename-page-txs)
+        (let [uid-by-title (common-db/v-by-ea @@fixture/connection [:node/title test-title-to] :block/uid)]
+          (test/is (= test-page-uid uid-by-title))))))
+
+  (test/testing "complex case, where we need to update string representations as well"
+    (let [test-page-uid    "test-page-2-1-uid"
+          test-title-from  "test page 2 title from"
+          test-title-to    "test page 2 title to"
+          test-block-uid   "test-block-2-1-uid"
+          test-string-from (str "[[" test-title-from "]]")
+          test-string-to   (str "[[" test-title-to "]]")
+          setup-txs        [{:db/id          -1
+                             :node/title     test-title-from
+                             :block/uid      test-page-uid
+                             :block/children [{:db/id          -2
+                                               :block/uid      test-block-uid
+                                               :block/string   test-string-from
+                                               :block/order    0
+                                               :block/children []}]}]]
+      ;; need to apply linkmaker, so resolving page-rename event can follow references for :block/string changes
+      (d/transact @fixture/connection (common-db/linkmaker @@fixture/connection setup-txs))
+      (let [uid-by-title    (common-db/v-by-ea @@fixture/connection [:node/title test-title-from] :block/uid)
+            rename-page-txs (resolver/resolve-event-to-tx @@fixture/connection
+                                                          (common-events/build-page-rename-event -1
+                                                                                                 test-page-uid
+                                                                                                 test-title-from
+                                                                                                 test-title-to))]
+        (test/is (= test-page-uid uid-by-title))
+        (d/transact @fixture/connection rename-page-txs)
+        (let [uid-by-old-title (common-db/v-by-ea @@fixture/connection [:node/title test-title-from] :block/uid)
+              uid-by-title     (common-db/v-by-ea @@fixture/connection [:node/title test-title-to] :block/uid)
+              block-string     (common-db/v-by-ea @@fixture/connection [:block/uid test-block-uid] :block/string)]
+          (test/is (not= uid-by-title uid-by-old-title))
+          (test/is (= test-page-uid uid-by-title))
+          (test/is (= test-string-to block-string)))))))
 
 
 (test/deftest delete-page
