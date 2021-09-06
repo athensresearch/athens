a4ffe4726b92dea2ad1f03db87d291c6ece28f59
shanberg <shanberg@users.noreply.github.com>
2021-09-06 10:12:33 -0400

    fix(storybook): fix block recurse limiter

--- src/js/utils/recurseBlocks.ts
+++ src/js/utils/recurseBlocks.ts
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BlockProps } from '../components/Block/Block';
 
 interface recurseBlocksProps {
   tree: any[];
   content: any;
   setBlockState?: Function;
-  blockProps?: Object;
+  blockProps?: BlockProps;
   blockComponent: any;
-  currentLength?: number;
   lengthLimit?: number;
-  returnLimit?: number;
   depthLimit?: number;
-  currentDepth?: number;
+  incCurrentLength?(): void;
+  incCurrentDepth?(): void;
+  getCurrentLength?(): number;
+  getCurrentDepth?(): number;
 }
+
+
+// makeBlocks
+// set currentLenght = 0
+// const incCurrentLength = (currentLength) => currentLength++
+// 
+// recurseBlocks(...stuff, incCurrentLength)
 
 export const recurseBlocks = ({
   tree,
@@ -21,20 +30,57 @@ export const recurseBlocks = ({
   blockComponent,
   lengthLimit = Infinity, // FIXME: this doesn't work properly
   depthLimit = Infinity,
-  currentLength = 1,
-  currentDepth = 1,
 }: recurseBlocksProps) => {
-  return tree.map(block => {
-    currentLength += 1;
+  let currentLength = 0;
+  let currentDepth = 0;
+  const incCurrentLength = () => currentLength++;
+  const incCurrentDepth = () => currentDepth++;
+  const getCurrentLength = () => currentLength;
+  const getCurrentDepth = () => currentDepth;
 
-    if (lengthLimit >= currentLength) {
-    return (
-      React.cloneElement(blockComponent,
+  return recurseBlocksFn({
+    tree,
+    content,
+    setBlockState,
+    blockProps,
+    blockComponent,
+    lengthLimit,
+    depthLimit,
+    incCurrentLength,
+    incCurrentDepth,
+    getCurrentLength,
+    getCurrentDepth,
+  });
+}
+
+
+export const recurseBlocksFn = ({
+  tree,
+  content,
+  setBlockState,
+  blockProps,
+  blockComponent,
+  lengthLimit,
+  depthLimit,
+  incCurrentLength,
+  incCurrentDepth,
+  getCurrentLength,
+  getCurrentDepth,
+}: recurseBlocksProps) => {
+  console.log(incCurrentLength);
+
+  return tree.map((block) => {
+    incCurrentLength();
+
+    if (lengthLimit >= getCurrentLength()) {
+      incCurrentDepth();
+      return (
+        React.cloneElement(blockComponent,
         {
           key: block.uid,
           ...content[block.uid],
           ...blockProps,
-          children: block.children && (lengthLimit >= currentLength && depthLimit >= currentDepth) && recurseBlocks({
+          children: block.children && (lengthLimit >= getCurrentLength() && depthLimit >= getCurrentDepth()) && recurseBlocksFn({
             tree: block.children,
             content: content,
             setBlockState: setBlockState,
@@ -42,12 +88,15 @@ export const recurseBlocks = ({
             blockComponent: blockComponent,
             lengthLimit: lengthLimit,
             depthLimit: depthLimit,
-            currentLength: currentLength++,
-            currentDepth: currentDepth++,
+              incCurrentLength,
+              incCurrentDepth,
+              getCurrentLength,
+              getCurrentDepth,
           }),
         },
         ))
     } else {
+      console.log('exiting');
       return
     }
   });
