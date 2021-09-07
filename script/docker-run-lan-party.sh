14e3f235c5547f0476bba5442143e3aa71b6feaf
Alex Iwaniuk <alex@happyhacking.nl>
2021-09-07 17:22:47 +0200

    A bit more memory.
    64 was just to get OOME faster.

--- script/docker-run-lan-party.sh
+++ script/docker-run-lan-party.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xms64m -Xmx64m -verbose:gc -XX:-UseParallelGC \
+java -Xms256m -Xmx512m -verbose:gc -XX:-UseParallelGC \
      -XX:OnOutOfMemoryError="kill -9 %p" -XX:+HeapDumpOnOutOfMemoryError \
      -XX:HeapDumpPath=/srv/athens/logs/ -jar target/athens-lan-party-standalone.jar
