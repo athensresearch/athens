1bcd729a9a973e444badde05219022af4220c807
shanberg <shanberg@users.noreply.github.com>
2021-09-11 12:44:31 -0400

    chore: merge from upstream changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,21 @@
 module.exports = {
-  "stories": [
-    "../src/js/**/*.stories.mdx",
-    "../src/js/**/*.stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    'storybook-dark-mode',
-    '@geometricpanda/storybook-addon-badges'
-  ],
-  webpackFinal: async (config, { configType }) => {
-    // Always prefer .tsx files when resolving modules.
-    // We output the .js files from tsc directly on the same folder, but want
-    // storybook to process the original .tsx for the additional TS tooling.
-    config.resolve.extensions.unshift(".tsx");
-    
-    // Return the altered config.
-    return config;
-  }
+    "stories": [
+        "../src/js/**/*.stories.mdx",
+        "../src/js/**/*.stories.tsx"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials"
+    ],
+    webpackFinal: async (config, { configType }) => {
+        // Always prefer .tsx files when resolving modules.
+        // We output the .js files from tsc directly on the same folder, but want
+        // storybook to process the original .tsx for the additional TS tooling.
+        config.resolve.extensions.unshift(".tsx");
+        config.resolve.extensions.unshift(".ts");
+
+
+        // Return the altered config.
+        return config;
+    }
 }
