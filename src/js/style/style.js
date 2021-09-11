a2b006d5d4dbe1927df42dcd58e633c6d52b404e
shanberg <shanberg@users.noreply.github.com>
2021-09-10 22:12:10 -0400

    chore: formatting updates

--- src/js/style/style.js
+++ src/js/style/style.js
@@ -2,83 +2,83 @@ import { createGlobalStyle } from 'styled-components';
 import { transparentize, readableColor } from 'polished';
 
 export const permuteColorOpacities = (theme) =>
-  Object.keys(theme).map((color) => {
-    return (
-      `--${color}: ${theme[color]};` +
-      `--${color}---contrast: ${readableColor(theme[color])};` +
-      Object.keys(opacities)
-        .map((opacity) => {
-          return `--${color}---${opacity}: ${transparentize(
-            1 - opacities[opacity],
-            theme[color]
-          )};`;
-        })
-        .join("")
-    );
-  });
+    Object.keys(theme).map((color) => {
+        return (
+            `--${color}: ${theme[color]};` +
+            `--${color}---contrast: ${readableColor(theme[color])};` +
+            Object.keys(opacities)
+                .map((opacity) => {
+                    return `--${color}---${opacity}: ${transparentize(
+                        1 - opacities[opacity],
+                        theme[color]
+                    )};`;
+                })
+                .join("")
+        );
+    });
 
 export const opacityStyles = () => Object.keys(opacities).map(opacity => `--${opacity}: ${opacities[opacity]};`).join("")
 
 const opacities = {
-  "opacity-lower": 0.1,
-  "opacity-low": 0.25,
-  "opacity-med": 0.5,
-  "opacity-high": 0.75,
-  "opacity-higher": 0.85,
-  "opacity-05": 0.05,
-  "opacity-10": 0.10,
-  "opacity-15": 0.15,
-  "opacity-20": 0.20,
-  "opacity-25": 0.25,
-  "opacity-30": 0.30,
-  "opacity-80": 0.80,
-  "opacity-90": 0.9,
+    "opacity-lower": 0.1,
+    "opacity-low": 0.25,
+    "opacity-med": 0.5,
+    "opacity-high": 0.75,
+    "opacity-higher": 0.85,
+    "opacity-05": 0.05,
+    "opacity-10": 0.10,
+    "opacity-15": 0.15,
+    "opacity-20": 0.20,
+    "opacity-25": 0.25,
+    "opacity-30": 0.30,
+    "opacity-80": 0.80,
+    "opacity-90": 0.9,
 };
 
 const themeLight = {
-  "link-color": "#0071DB",
-  "highlight-color": "#F9A132",
-  "text-highlight-color": "#ffdb8a",
-  "warning-color": "#D20000",
-  "confirmation-color": "#009E23",
-  "header-text-color": "#322F38",
-  "body-text-color": "#433F38",
-  "border-color": "hsla(32, 81%, 10%, 0.08)",
-  "background-plus-2": "#fff",
-  "background-plus-1": "#fbfbfb",
-  "background-color": "#F6F6F6",
-  "background-minus-1": "#FAF8F6",
-  "background-minus-2": "#EFEDEB",
-  "graph-control-bg": "#f9f9f9",
-  "graph-control-color": "black",
-  "graph-node-normal": "#909090",
-  "graph-node-hlt": "#0075E1",
-  "graph-link-normal": "#cfcfcf",
-  "error-color": "#fd5243", // Deprecate. Replace with "danger-color"
-  "shadow-color": "#000"
+    "link-color": "#0071DB",
+    "highlight-color": "#F9A132",
+    "text-highlight-color": "#ffdb8a",
+    "warning-color": "#D20000",
+    "confirmation-color": "#009E23",
+    "header-text-color": "#322F38",
+    "body-text-color": "#433F38",
+    "border-color": "hsla(32, 81%, 10%, 0.08)",
+    "background-plus-2": "#fff",
+    "background-plus-1": "#fbfbfb",
+    "background-color": "#F6F6F6",
+    "background-minus-1": "#FAF8F6",
+    "background-minus-2": "#EFEDEB",
+    "graph-control-bg": "#f9f9f9",
+    "graph-control-color": "black",
+    "graph-node-normal": "#909090",
+    "graph-node-hlt": "#0075E1",
+    "graph-link-normal": "#cfcfcf",
+    "error-color": "#fd5243", // Deprecate. Replace with "danger-color"
+    "shadow-color": "#000"
 }
 
 const themeDark = {
-  "link-color": "#0071DB",
-  "highlight-color": "#FBBE63",
-  "text-highlight-color": "#FBBE63",
-  "warning-color": "#DE3C21",
-  "confirmation-color": "#189E36",
-  "header-text-color": "#BABABA",
-  "body-text-color": "#AAA",
-  "border-color": "hsla(32, 81%, 90%, 0.08)",
-  "background-minus-1": "#151515",
-  "background-minus-2": "#111",
-  "background-color": "#1A1A1A",
-  "background-plus-1": "#222",
-  "background-plus-2": "#333",
-  "graph-control-bg": "#272727",
-  "graph-control-color": "white",
-  "graph-node-normal": "#909090",
-  "graph-node-hlt": "#FBBE63",
-  "graph-link-normal": "#323232",
-  "error-color": "#fd5243", // Deprecate. Replace with "danger-color"
-  "shadow-color": "#000"
+    "link-color": "#0071DB",
+    "highlight-color": "#FBBE63",
+    "text-highlight-color": "#FBBE63",
+    "warning-color": "#DE3C21",
+    "confirmation-color": "#189E36",
+    "header-text-color": "#BABABA",
+    "body-text-color": "#AAA",
+    "border-color": "hsla(32, 81%, 90%, 0.08)",
+    "background-minus-1": "#151515",
+    "background-minus-2": "#111",
+    "background-color": "#1A1A1A",
+    "background-plus-1": "#222",
+    "background-plus-2": "#333",
+    "graph-control-bg": "#272727",
+    "graph-control-color": "white",
+    "graph-node-normal": "#909090",
+    "graph-node-hlt": "#FBBE63",
+    "graph-link-normal": "#323232",
+    "error-color": "#fd5243", // Deprecate. Replace with "danger-color"
+    "shadow-color": "#000"
 }
 
 const lightThemeColors = permuteColorOpacities(themeLight);
