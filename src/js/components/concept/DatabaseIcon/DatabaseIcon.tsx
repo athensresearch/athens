7755cefb07ae97963ff3ada8dbdb38ead14fffce
shanberg <shanberg@users.noreply.github.com>
2021-09-10 22:12:30 -0400

    feat(db icon): now support emoji and custom colors

--- src/js/components/concept/DatabaseIcon/DatabaseIcon.tsx
+++ src/js/components/concept/DatabaseIcon/DatabaseIcon.tsx
@@ -1,43 +1,59 @@
+import { CSSProperties } from 'react';
 import styled from 'styled-components';
+import { readableColor } from 'polished';
 
 const IconWrap = styled.svg`
-  height: var(--size, 1.5rem);
-  width: var(--size, 1.5rem);
-
-  rect {
-    fill: var(--link-color);
-  }
+  font-size: var(--size, 1.5em);
+  height: 1em;
+  width: 1em;
 
   text {
     text-transform: uppercase;
-    fill: var(--link-color---contrast);
-    font-size: 100%;
     font-weight: bold;
-    text-anchor: middle;
-  }`;
+    user-select: none;
+  }
+`;
+
+export interface DatabaseIcon {
+    name: string;
+    color?: CSSProperties["color"];
+    size?: CSSProperties["width"];
+}
 
 /**
  * Icon representing a database
  */
-export const DatabaseIcon = ({ name, size }) => {
+export const DatabaseIcon = ({
+    name,
+    color,
+    size
+}: DatabaseIcon): React.ReactElement => {
+    const isEmojiIcon = /\p{Emoji}/u.test(name.trim());
+
+    console.log(color);
+
     return (
-    <IconWrap
-        viewBox="0 0 24 24"
-        style={{ "--size": size ? size : undefined }}
-    >
-    <rect
-      fill="var(--link-color)"
-      height={24}
-      width={24}
-      rx={4}
-      x={0}
-      y={0}
-    />
-    <text
-      x="12"
-      y="75%"
-    >{name ? name.charAt(0) : 'x'}</text>
-  </IconWrap>)
+        <IconWrap
+            viewBox="0 0 24 24"
+            style={{ "--size": size ? size : undefined }}
+        >
+            <rect
+                fill={color ? color : "var(--link-color)"}
+                height={24}
+                width={24}
+                rx={4}
+                x={0}
+                y={0}
+            />
+            <text
+                x={12}
+                y={13.5}
+                fontSize={isEmojiIcon ? 16 : 20}
+                textAnchor="middle"
+                dominantBaseline="middle"
+                fill={color ? readableColor(color) : "var(--link-color---contrast)"}
+            >{isEmojiIcon ? name : name.trim().charAt(0)}</text>
+        </IconWrap>)
 };
 
 DatabaseIcon.Wrap = IconWrap;
