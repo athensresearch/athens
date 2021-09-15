6994de51dc9bbd0be63b758d4c16f43de35326a4
sid597 <siddharthdv77@gmail.com>
2021-09-15 15:57:46 +0530

    Fixed tests

--- test/athens/common_events/atomic_ops_test.clj
+++ test/athens/common_events/atomic_ops_test.clj
@@ -78,22 +78,23 @@
 
 
 (t/deftest page-new-test
-              (let [test-title        "test page title"
-                    test-page-uid     "test-page-uid-1"
-                    test-block-uid    "test-block-uid-1"
-                    page-new-event   (atomic-graph-ops/make-page-new-op test-title
-                                                                        test-page-uid
-                                                                        test-block-uid)
-                    page-new-txs              (atomic-resolver/resolve-atomic-op-to-tx @@fixture/connection
-                                                                                       page-new-event)]
-                (d/transact @fixture/connection page-new-txs)
-                (let [e-by-title (d/q '[:find ?e
-                                        :where [?e :node/title ?title]
-                                        :in $ ?title]
-                                      @@fixture/connection test-title)
-                      e-by-uid (d/q '[:find ?e
-                                      :where [?e :block/uid ?uid]
-                                      :in $ ?uid]
-                                    @@fixture/connection test-page-uid)]
-                  (t/is (seq e-by-title))
-                  (t/is (= e-by-title e-by-uid)))))
+  (t/testing "Page new test"
+    (let [test-title        "test page title"
+          test-page-uid     "test-page-uid-1"
+          test-block-uid    "test-block-uid-1"
+          page-new-event   (atomic-graph-ops/make-page-new-op test-title
+                                                              test-page-uid
+                                                              test-block-uid)
+          page-new-txs     (atomic-resolver/resolve-atomic-op-to-tx @@fixture/connection
+                                                                    page-new-event)]
+      (d/transact @fixture/connection page-new-txs)
+      (let [e-by-title (d/q '[:find ?e
+                              :where [?e :node/title ?title]
+                              :in $ ?title]
+                            @@fixture/connection test-title)
+            e-by-uid (d/q '[:find ?e
+                            :where [?e :block/uid ?uid]
+                            :in $ ?uid]
+                          @@fixture/connection test-page-uid)]
+        (t/is (seq e-by-title))
+        (t/is (= e-by-title e-by-uid))))))
