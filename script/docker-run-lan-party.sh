44e9672004561fa5c10fb493b453ba644ad23ba3
Filipe Silva <filipematossilva@gmail.com>
2021-10-13 13:04:29 +0100

    build: increase max memory for athens server

--- script/docker-run-lan-party.sh
+++ script/docker-run-lan-party.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xms512m -Xmx1536m -verbose:gc -XX:-UseParallelGC \
+java -Xms512m -Xmx2048m -verbose:gc -XX:-UseParallelGC \
      -XX:OnOutOfMemoryError="kill -9 %p" -XX:+HeapDumpOnOutOfMemoryError \
      -XX:HeapDumpPath=/srv/athens/logs/ -jar athens-lan-party-standalone.jar
