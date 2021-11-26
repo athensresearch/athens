c301a388a527e01f4d1c986a61cbeefb4eb5213b
Alex Iwaniuk <alex@happyhacking.nl>
2021-11-26 23:18:54 +0100

    fix: use same semantics for event replay as processing.

--- src/clj/athens/self_hosted/components/datascript.clj
+++ src/clj/athens/self_hosted/components/datascript.clj
@@ -5,8 +5,12 @@
     [athens.common-events.resolver.atomic :as atomic]
     [athens.common.logging                :as log]
     [athens.self-hosted.event-log         :as event-log]
+    [clojure.pprint                       :as pp]
     [com.stuartsierra.component           :as component]
-    [datascript.core                      :as d]))
+    [datascript.core                      :as d])
+  (:import
+    (clojure.lang
+      ExceptionInfo)))
 
 
 (defrecord Datascript
@@ -27,8 +31,19 @@
                             event-log/initial-events
                             (event-log/events fluree))]
           (log/info "Processing" (pr-str id) "with" (common-events/find-event-or-atomic-op-type data))
-          (atomic/resolve-transact! conn data)
-          (swap! total inc))
+          (try
+            (atomic/resolve-transact! conn data)
+            (swap! total inc)
+            (catch Error err
+              (log/warn "Event that we've failed to process was:"
+                        (with-out-str
+                          (pp/pprint data)))
+              (log/error err "Error during processing" (pr-str id)))
+            (catch ExceptionInfo ex
+              (log/warn "Event that we've failed to process was:"
+                        (with-out-str
+                          (pp/pprint data)))
+              (log/error ex "Exception during processing" (pr-str id)))))
         (log/info "✅ Replayed" @total "events."))
       (common-db/health-check conn)
       (assoc component :conn conn)))
