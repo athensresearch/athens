9c1397f7caa3ffb7008ddcc8a8c27fd165e02cb0
Filipe Silva <filipematossilva@gmail.com>
2021-12-15 15:58:43 +0000

    test: add client e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,45 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import * as path from 'path';
+
+// Electron setup taken from https://gist.github.com/UberMouse/facbe751c3ecb9b31e8b4f6221567b7a
+// mentioned in https://github.com/microsoft/playwright/issues/8208#issuecomment-948093888.
+//
+// Some more useful sources on this topic:
+// - replacing spectron with playwright: https://github.com/electron-userland/spectron/issues/896
+// - playwright repo electron tests: https://github.com/microsoft/playwright/tree/main/tests/electron
+// - playwright electron support: https://playwright.dev/docs/api/class-electronapplication
+//   and https://playwright.dev/docs/api/class-electron
+// - playwright+electron starter: https://github.com/spaceagetv/electron-playwright-example
+
+const outputDir = path.join(__dirname, 'test-results');
+const config: PlaywrightTestConfig = {
+  outputDir,
+  testDir: './test/e2e',
+  timeout: 30000,
+  globalTimeout: 5400000,
+  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: !!process.env.CI,
+  preserveOutput: process.env.CI ? 'failures-only' : 'always',
+  retries: process.env.CI ? 3 : 0,
+  reporter: process.env.CI ? [
+    [ 'dot' ],
+    [ 'json', { outputFile: path.join(outputDir, 'report.json') } ],
+  ] : 'line',
+  projects: [{
+    name: 'chromium',
+    use: {
+      browserName: 'chromium',
+      trace: process.env.CI ? 'on-first-retry' : 'on'
+    },
+    metadata: {
+      platform: process.platform,
+      headful: true,
+      browserName: 'electron',
+      channel: undefined,
+      mode: 'default',
+      video: false,
+    }
+  }],
+};
+
+export default config;
---
+++ test/e2e/electron-test.ts
@@ -0,0 +1,47 @@
+import * as path from 'path';
+import type { Fixtures } from "@playwright/test";
+import { test as base } from "@playwright/test";
+import { BrowserContext, ElectronApplication, Page, _electron } from "playwright";
+
+// athens.listeners/prevent-save checks for this variable in local storage
+// and will not present the confirmation window that stops closing athens.
+const disableSaveCheck = () => window.localStorage.setItem("E2E_IGNORE_SAVE_CHECK", "true");
+const enableSaveCheck = () => window.localStorage.removeItem("E2E_IGNORE_SAVE_CHECK");
+
+type ElectronTestFixtures = {
+  electronApp: ElectronApplication;
+  page: Page;
+  context: BrowserContext;
+};
+
+export const electronFixtures: Fixtures<ElectronTestFixtures> = {
+  electronApp: async ({}, run) => {
+    // This env prevents 'Electron Security Policy' console message.
+    process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+    const electronApp = await _electron.launch({
+      // matches ../../package.json#main
+      args: [path.join(__dirname, '../../resources/main.js')],
+    });
+    await run(electronApp);
+    await electronApp.close();
+  },
+  page: async ({ electronApp }, run) => {
+    const page = await electronApp.firstWindow();
+
+    // Always disable the save check on exit, because it needs special handling and
+    // would delay every test by 15s.
+    // If you want to test it, call `page.evaluate(enableSaveCheck)` on your test.
+    page.evaluate(disableSaveCheck);
+
+    await run(page);
+  },
+  context: async ({ electronApp }, run) => {
+    const context = electronApp.context();
+
+    await run(context);
+  },
+};
+
+// @ts-ignore some error about a string type now having `undefined` as part of it's union
+export const test = base.extend<ElectronTestFixtures>(electronFixtures);
+
