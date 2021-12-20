9e362d5079f292be014192aba69879099be766a7
Alex Iwaniuk <alex@happyhacking.nl>
2021-12-20 12:01:42 +0100

    lint: happy

--- src/cljs/athens/parse_renderer.cljs
+++ src/cljs/athens/parse_renderer.cljs
@@ -98,7 +98,7 @@
 
 
 (defn render-block-ref
-  [{:keys [from title] :as attr} ref-uid uid]
+  [{:keys [from title]} ref-uid uid]
   (let [block (pull db/dsdb '[*] [:block/uid ref-uid])]
     (if @block
       [:span (assoc (use-style block-ref {:class "block-ref"})
