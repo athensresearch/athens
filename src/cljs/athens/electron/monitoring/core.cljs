f09dc67456f8350a2bf280c5b7b32d3e52129bd0
jeff <tangj1122@gmail.com>
2022-01-13 15:41:57 +0300

    add electron-build

--- src/cljs/athens/electron/monitoring/core.cljs
+++ src/cljs/athens/electron/monitoring/core.cljs
@@ -2,16 +2,20 @@
   (:require
     [athens.electron.db-picker :as db-picker]
     [athens.electron.utils :as utils]
+    [athens.util :as util]
     [re-frame.core :as rf]))
 
 
+;; https://posthog.com/docs/integrate/client/js#super-properties
 (rf/reg-event-fx
   :posthog/set-super-properties
   (fn [{:keys [db]} _]
-    (let [selected-db      (db-picker/selected-db db)
-          remote-db?       (utils/remote-db? selected-db)
-          graph-props      (if remote-db?
-                             {:graph {:type :self-hosted}}
-                             {:graph {:type :local}})
-          super-properties (merge graph-props)]
-      (.. js/posthog (register (clj->js super-properties))))))
+    (let [selected-db         (db-picker/selected-db db)
+          remote-db?          (utils/remote-db? selected-db)
+          graph-type          {:graph-type (if remote-db?
+                                             :self-hosted
+                                             :local)}
+          electron-build      {:electron-build (util/athens-version)}
+          super-properties    (merge graph-type electron-build)
+          js-super-properties (clj->js super-properties)]
+      (.. js/posthog (register js-super-properties)))))
