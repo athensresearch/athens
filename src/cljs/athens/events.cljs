e81e806df1a813fcdfb0a0d95dbd8801c93ac6af
jeff <tangj1122@gmail.com>
2022-01-21 16:07:22 +0300

    create split-block-with-children, redo doesn't always work

--- src/cljs/athens/events.cljs
+++ src/cljs/athens/events.cljs
@@ -996,6 +996,28 @@
                           [:editing/uid (str new-uid (when embed-id
                                                        (str "-embed-" embed-id)))]]]]})))
 
+(declare block-move-chain)
+
+(reg-event-fx
+  :enter/split-block-with-children
+  (fn [_ [_ {:keys [uid new-uid value index embed-id relation children] :as args}]]
+    ;; Triggered when a block is split and the block has children
+    ;; New block from split is :after and gets the children
+    (log/debug ":enter/split-block-with-children" (pr-str args))
+    (let [split-block-op              (graph-ops/build-block-split-op @db/dsdb {:old-block-uid uid
+                                                                                :new-block-uid new-uid
+                                                                                :string        value
+                                                                                :index         index
+                                                                                :relation      relation})
+          children                    (mapv :block/uid children)
+          add-children-op             (block-move-chain new-uid children :first)
+          split-block-add-children-op (composite-ops/make-consequence-op {:op/type :split-block-add-children}
+                                                                         [split-block-op
+                                                                          add-children-op])
+          event                       (common-events/build-atomic-event split-block-add-children-op)]
+      {:fx [[:dispatch-n [[:resolve-transact-forward event]
+                          [:editing/uid (str new-uid (when embed-id
+                                                       (str "-embed-" embed-id)))]]]]})))
 
 (reg-event-fx
   :enter/bump-up
@@ -1111,6 +1133,16 @@
                                                    :parent   parent
                                                    :new-uid  new-uid
                                                    :embed-id embed-id}]
+
+                                (and (not (zero? start))
+                                     (:block/children block))
+                                [:enter/split-block-with-children {:uid        uid
+                                                                   :value      value
+                                                                   :index      start
+                                                                   :new-uid    new-uid
+                                                                   :embed-id   embed-id
+                                                                   :relation   :after
+                                                                   :children (:block/children block)}]
 
                                 (not (zero? start))
                                 [:enter/split-block {:uid        uid
