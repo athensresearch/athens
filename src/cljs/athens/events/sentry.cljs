94e93d2f80ffacaa38d3f56aa913c243a91754e1
Alex Iwaniuk <alex@happyhacking.nl>
2022-02-09 14:17:53 +0100

    chore: cleanup log/info

--- src/cljs/athens/events/sentry.cljs
+++ src/cljs/athens/events/sentry.cljs
@@ -1,13 +1,11 @@
 (ns athens.events.sentry
   (:require
-    [athens.common.logging :as log]
-    [athens.utils.sentry   :as sentry]
-    [re-frame.core         :as rf]))
+    [athens.utils.sentry :as sentry]
+    [re-frame.core       :as rf]))
 
 
 (rf/reg-event-fx
   :sentry/end-tx
   (fn [_ [_ tx]]
-    (log/info "Ending Sentry TX")
     (sentry/transaction-finish tx)
     {}))
