1b43d7405ec01fd1b6b58d098b3de1aca27927a5
jeff <tangj1122@gmail.com>
2022-02-17 20:27:17 +0300

    e2e multiple blocks selected context menu right click

---
+++ test/e2e/block-context-menu.spec.ts
@@ -0,0 +1,61 @@
+import { expect } from '@playwright/test';
+import { test } from './electron-test';
+import { indentLastBlock, saveLastBlockAndEnter, unindentLastBlock, waitReady } from "./utils";
+
+const rightClickFirstBullet = async (page:Page) => {
+    await page.click('.block-body >> nth=0 >> svg', {
+        button: 'right'
+    });
+};
+
+test.describe("no blocks selected", () => {
+
+    const testSetup = async (page:Page) => {
+        await waitReady(page);
+        await saveLastBlockAndEnter(page, "alice");
+        await rightClickFirstBullet(page);
+    }
+
+    test('right-click one block', async ({ page }) => {
+        await testSetup(page);
+        expect(await page.isVisible("text='Copy block ref'")).toBe(true);
+    });
+
+    // // this currently fails because clicking out of the context menu doesn't work if you click on the block itself
+    // test('clicking out of the context menu closes it', async ({ page }) => {
+    //     await testSetup(page)
+    //     await page.click('text=alice');
+    //     expect(await page.isVisible("text='Copy block ref'")).toBe(false);
+    // });
+
+    test('clicking out of the context menu closes it', async ({ page }) => {
+        await testSetup(page)
+        await page.click('#daily-notes');
+        expect(await page.isVisible("text='Copy block ref'")).toBe(false);
+    });
+
+})
+
+// test('right-click when one block is selected', async ({ page }) => {
+//     expect(await page.isVisible("text='Copy block ref'"));
+// });
+
+test.describe("multiple blocks selected", () => {
+    const testSetup = async (page:Page) => {
+        await waitReady(page);
+        await saveLastBlockAndEnter(page, "alice");
+        await saveLastBlockAndEnter(page, "bob");
+        await page.dragAndDrop("textarea >> text=bob", "textarea >> text=alice");
+        await rightClickFirstBullet(page);
+    };
+
+    test('right-click when multiple blocks selected', async ({ page }) => {
+        await testSetup(page);
+        await page.pause();
+        // this top expect is true, but it should be false
+        // expect(await page.isVisible("text='Copy block ref'")).toBe(true);
+        // this top expect is false, but it should be true
+        // expect(await page.isVisible("text='Copy block refs'")).toBe(true);
+    });
+
+})
