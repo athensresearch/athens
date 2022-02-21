399a08bfe232c17aa581901b45641fc309a117e7
Filipe Silva <filipematossilva@gmail.com>
2022-02-21 20:32:23 +0000

    fix: athena tests should cleanup page

--- test/e2e/athena-navigation.spec.ts
+++ test/e2e/athena-navigation.spec.ts
@@ -1,29 +1,34 @@
 import { expect } from '@playwright/test';
 import { test } from './electron-test';
+import { deleteCurrentPage } from "./utils";
+
 
 test('athena create new page then enter', async ({ page }) => {
-
+  const title = 'create-enter'
   // Click button:has-text("Find or create a page")
   await page.click('button:has-text("Find or create a page")');
   // Fill [placeholder="Find or Create Page"]
-  await page.fill('[placeholder="Find or Create Page"]', 'arst');
+  await page.fill('[placeholder="Find or Create Page"]', title);
   // Press Enter
   await Promise.all([
     page.press('[placeholder="Find or Create Page"]', 'Enter')]);
-  await expect(page.locator(".node-page > header > h1 > textarea")).toHaveValue('arst');
+  await expect(page.locator(".node-page > header > h1 > textarea")).toHaveValue(title);
 
+  await deleteCurrentPage(page);
 });
 
 test('athena create new page then click create page', async ({ page }) => {
-
+  const title = 'create-click';
 // Click button:has-text("Find or create a page")
   await page.click('button:has-text("Find or create a page")');
   // Fill [placeholder="Find or Create Page"]
-  await page.fill('[placeholder="Find or Create Page"]', 'arst');
+  await page.fill('[placeholder="Find or Create Page"]', title);
   // Click text=Create Page: arst
   await Promise.all([
-    page.click('text=Create Page: arst')]);
-  await expect(page.locator(".node-page > header > h1 > textarea")).toHaveValue('arst');
+    page.click('text=Create Page: ' + title)]);
+  await expect(page.locator(".node-page > header > h1 > textarea")).toHaveValue(title);
+
+  await deleteCurrentPage(page);
 });
 
 test('athena search block then enter on result', async ({ page }) => {
