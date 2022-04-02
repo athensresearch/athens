73f0b6c44ef01429e9aa6d29ff32182e5b40214d
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-04-02 16:23:40 -0400

    chore: cleanup unused

--- src/cljs/athens/views/blocks/autocomplete_search.cljs
+++ src/cljs/athens/views/blocks/autocomplete_search.cljs
@@ -1,56 +1,6 @@
-;; (ns athens.views.blocks.autocomplete-search
-;;   (:require
-;;    ["@chakra-ui/react" :refer [Portal Menu MenuItem MenuList Text Box]]
-;;    [athens.views.blocks.textarea-keydown :as textarea-keydown]
-;;    [clojure.string :as string]))
-
-
-;; (defn inline-item-click
-;;   [state uid expansion]
-;;   (let [id     (str "#editable-uid-" uid)
-;;         target (.. js/document (querySelector id))
-;;         f      (case (:search/type @state)
-;;                  :hashtag  textarea-keydown/auto-complete-hashtag
-;;                  :template textarea-keydown/auto-complete-template
-;;                  textarea-keydown/auto-complete-inline)]
-;;     (f state target expansion)))
-
-
-;; (defn inline-search-el
-;;   [_block]
-;;   (fn [block state]
-;;     (let [{:search/keys [index results type query] caret-position :caret-position} @state
-;;           {:keys [left top]} caret-position]
-;;       (println type)
-;;       (println (when type (symbol type)))
-;;       (when (some #(= % type) [:page :block :hashtag :template])
-;;         [:> Menu {:isOpen true
-;;                   :autoSelect false}
-;;          [:> Portal
-;;           [:> MenuList {;; don't blur textarea when clicking to auto-complete
-;;                         :on-mouse-down (fn [e] (.. e preventDefault))
-;;                         :position "absolute"
-;;                         :overflow "auto"
-;;                         :width "max-content"
-;;                         :maxHeight "20rem"
-;;                         :top (str (+ 24 top) "px")
-;;                         :left (str (+ 24 left) "px")}
-;;            (if (or (string/blank? query)
-;;                    (empty? results))
-;;              [:> Text {:py "0.4rem" :px "0.8rem" :fontStyle "italics"} (str "Search for a " (symbol type))]
-;;              (doall
-;;               (for [[i {:keys [node/title block/string block/uid]}] (map-indexed list results)]
-;;                 [:> MenuItem {:key      (str "inline-search-item" uid)
-;;                               :id       (str "dropdown-item-" i)
-;;                               :width "100%"
-;;                               :isActive   (= index i)
-;;                                                          ;; if page link, expand to title. otherwise expand to uid for a block ref
-;;                               :onClick (fn [_] (inline-item-click state (:block/uid block) (or title uid)))}
-;;                  (or title string)])))]]]))))
-
 (ns athens.views.blocks.autocomplete-search
   (:require
-    ["@chakra-ui/react" :refer [Portal Popover PopoverTrigger PopoverBody Button PopoverContent Menu MenuItem MenuList Text Box]]
+    ["@chakra-ui/react" :refer [Portal Popover PopoverTrigger PopoverBody Button PopoverContent Text Box]]
     [athens.views.blocks.textarea-keydown :as textarea-keydown]
     [clojure.string :as string]))
 
