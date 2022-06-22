c936bef2145d0d5de32387f394007e925750dd0a
tangjeff0 <tangj1122@gmail.com>
2022-06-22 12:02:05 +0200

    add gallery view and tweets

--- src/cljs/athens/views/pages/all_pages.cljs
+++ src/cljs/athens/views/pages/all_pages.cljs
@@ -5,7 +5,12 @@
                                 Box
                                 Button
                                 Stack
+                                Text
+                                Heading
+                                Radio RadioGroup
+                                SimpleGrid
                                 Checkbox CheckboxGroup]]
+    ["react-twitter-embed" :refer [TwitterTweetEmbed]]
     [athens.common-db          :as common-db]
     [athens.dates              :as dates]
     [athens.db                 :as db]
@@ -83,130 +88,142 @@
 ;;  Options
 
 
-(def entities
+(def entity-map
   {:pages {:title "Pages"
-           :query-fn #()}})
+           :query-fn (fn []
+                       (->>
+                        (common-db/get-all-pages @db/dsdb)
+                        (filter #(not (athens.dates.is-daily-note (:block/uid %))))))}
+   :daily-notes {:title "Daily Notes"
+                 :query-fn (fn []
+                             (->> (common-db/get-all-pages  @db/dsdb)
+                                  (filter #(athens.dates.is-daily-note (:block/uid %)))))}
+   :blocks {:title "Blocks"
+            :query-fn  #(common-db/get-all-blocks @db/dsdb)}
+   :tweets {:title "Tweets"
+            :query-fn #(->> (common-db/get-all-blocks @db/dsdb)
+                            (filter (fn [{:keys [block/string]} x]
+                                      (re-find #"https://twitter.com/\w+/status/\d+" string))))}
+   :urls {:title "URLs"
+          :query-fn #() #_(->> (common-db/get-all-blocks @db/dsdb)
+                          (filter (fn [{:keys [block/string]} x]
+                                    (when (re-find #"" string)))))}})
 
-(athens.dates.is-daily-note "01-01-1996")
+
+
+
+;; (re-find #"\p{Emoji_Presentation}" "123")
+;; (re-find #"\p{Emoji}" "👍123")
+;; (re-pattern "\p{Emoji}")
+;; (re-find (js/RegExp "\p{Emoji}") "👍")
+;; (re-find #"foo" "FOO")
+
+
+
 
 (defn page
   []
-  (let [
-        get-pages-fn      (fn []
-                            (->>
-                             (common-db/get-all-pages @db/dsdb)
-                             (filter #(not (athens.dates.is-daily-note (:block/uid %))))))
-        get-blocks-fn     #(common-db/get-all-blocks @db/dsdb)
-        get-daily-notes-fn     (fn []
-                                 (->> (common-db/get-all-pages  @db/dsdb)
-                                      (filter #(athens.dates.is-daily-note (:block/uid %)))))
-        get-everything-fn #(concat (get-pages-fn) (get-blocks-fn))
-
-        checked-types     (r/atom {:pages false
-                                   :blocks false
-                                   :daily-notes false})
-        page-columns      ["Title" "Links Count" "Modified" "Created"]
-        block-columns     ["String" "Links Count" "Modified" "Created"]
-        all-columns (set (concat block-columns page-columns))
-        properties        (r/atom [])
-        ]
-
+  (let [checked-types     (-> (keys entity-map)
+                              (zipmap (repeat false))
+                              (merge {:tweets true})
+                              (r/atom))
+        query-view (r/atom "gallery")]
     (fn []
       (let [count-checked (-> @checked-types vals frequencies (get true))
             all-checked             (and (pos? count-checked)
-                                     (= (count @checked-types) count-checked))
+                                         (= (count @checked-types) count-checked))
             handle-click-everything (fn []
                                       (if all-checked
                                         (reset! checked-types {:pages false :blocks false :daily-notes false})
                                         (reset! checked-types {:pages true :blocks true :daily-notes true})))
-            entities          (cond-> []
-                                (:pages @checked-types) (concat (get-pages-fn))
-                                (:daily-notes @checked-types) (concat (get-daily-notes-fn))
-                                (:blocks @checked-types) (concat (get-blocks-fn)))]
+            properties ["UID" "Title" "String" "Links Count" "Modified" "Created"]
+            entities          (->> @checked-types
+                                   (map (fn [[k v]]
+                                          (if v
+                                            ((get-in entity-map [k :query-fn]))
+                                            [])))
+                                   flatten)]
 
         [:> Box {:margin-top "40px"}
          #_[:h1 (str @entities)]
+
          [:> Box
 
-          [:> Stack {:spacing 5 :direction "row" :colorScheme "blue"}
-           [:> Checkbox {:is-checked all-checked :on-change handle-click-everything} "Everything"]
-           [:> Checkbox {:is-checked (:pages @checked-types)
-                         :on-change  (fn [e]
-                                       (swap! checked-types assoc :pages (.. e -target -checked))
-                                       (reset! properties page-columns)
-                                       )}
-            "Pages"]
-           [:> Checkbox {:is-checked (:daily-notes @checked-types)
-                         :on-change  (fn [e]
-                                       (swap! checked-types assoc :daily-notes (.. e -target -checked))
-                                       (reset! properties page-columns))}
-            "Daily Notes"]
-           [:> Checkbox {:is-checked (:blocks @checked-types)
-                         :on-change  (fn [e]
-                                       (swap! checked-types assoc :blocks (.. e -target -checked))
-                                       (reset! properties block-columns))}
-            "Blocks"]]
+          [:> Stack {:spacing 10 :direction "column" :padding-left 5}
+           [:> Stack {:spacing 10 :direction "row" :colorScheme "blue"}
+            [:> Text {:as "u" :width 100} "Entities"]
+            [:> Checkbox {:is-checked all-checked :on-change handle-click-everything} "All entities"]
+            (doall
+             (for [[k v] entity-map]
+               [:> Checkbox {:key k
+                             :is-checked (k @checked-types)
+                             :on-change  (fn [e] (swap! checked-types assoc k (.. e -target -checked)))}
+                ( :title v )]))]
 
-          #_[:> Stack {:spacing 5 :direction "row" :colorScheme "blue"}
-           [:> Checkbox {:is-checked all-checked :on-change handle-click-everything} "Everything"]
-           [:> Checkbox {:is-checked (:pages @checked-types)
-                         :on-change  (fn [e]
-                                      (swap! checked-types assoc :pages (.. e -target -checked))
-                                      (reset! properties page-columns)
-                                      (reset! entities (get-pages-fn)))}
-            "Pages"]
-           [:> Checkbox {:is-checked (:blocks @checked-types)
-                         :on-change  (fn [e]
-                                      (swap! checked-types assoc :blocks (.. e -target -checked))
-                                      (reset! properties block-columns)
-                                      (reset! entities (get-blocks-fn)))}"Blocks"]]]
+           [:> Stack {:spacing 10 :direction "row" :colorScheme "blue"}
+            [:> Text {:as "u" :width 100} "Properties"]
+            [:> Checkbox {} ""]]
 
-         ;; how do we dynamically show what tables
-         [:> TableContainer
-          [:> Table {:variant "striped"}
-           [:> Thead
-            [:> Tr
-             (for [p @properties]
-               [:> Th {:key p} p])]]
-           [:> Tbody
-            (for [{:keys [:node/title :block/_refs :block/string]
-                   create-time :create/time edit-time :edit/time} entities]
+           [:> RadioGroup {:defaultValue @query-view :on-change #(reset! query-view %)}
+            [:> Stack {:spacing 10 :direction "row" :colorScheme "blue"}
+             [:> Text {:as "u" :width 100} "Views"]
+             [:> Radio {:value "table"} "Table"]
+             [:> Radio {:value "gallery" :on-click #()} "Gallery"]
+             ;; [:> Radio {:value "table"} "Board"]
+             ;; [:> Radio {:value "table"} "Calendar"]
+             ;; [:> Radio {:value "table"} "Outline"]
+             ;; [:> Radio {:value "table"} "Graph"]
+             ]]]]
+
+         ;; how do we dynamically show propertis
+         (case @query-view
+           "gallery"
+           [:> SimpleGrid {:columns 4 :spacing 10}
+            (for [{:keys [:block/string]} entities
+                  :let [tweet-id (->> string
+                                      (re-find #"https://twitter.com/\w+/status/(\d+)")
+                                      second)]]
+              [:> TwitterTweetEmbed {:tweetId tweet-id}])]
+
+           "table"
+           [:> TableContainer {:margin-top "30px"}
+            [:> Table {:variant "striped"}
+             [:> Thead
               [:> Tr
-               [:> Td title]
-               ;; [:> Td string]
-               [:> Td (count _refs)]
-               [:> Td  create-time]
-               [:> Td  edit-time]]
-              )]]]]))))
+               (for [p properties]
+                 [:> Th {:key p} p])]]
+             [:> Tbody
+              (for [{:keys [:node/title :block/_refs :block/string :block/uid]
+                     create-time :create/time edit-time :edit/time} entities]
+                [:> Tr {:key uid}
+                 [:> Td uid]
+                 [:> Td [:> Text {:max-width "600px" :noOfLines 1} title]]
+                 [:> Td [:> Text {:max-width "600px" :noOfLines 1} string]]
+                 [:> Td (count _refs)]
+                 [:> Td create-time]
+                 [:> Td edit-time]]
+                )]]])])))
 
-[:> TableContainer
- [:> Table
-  [:> Thead
-   [:> Tr
-    [:> Td]]]
-  [:> Tbody]
-  [:> Tfoot]]]
 
-
-#_(defn page
-  []
-  (let [all-pages (common-db/get-all-pages @db/dsdb)]
-    (fn []
-      (let [sorted-pages @(rf/subscribe [:all-pages/sorted all-pages])]
-        [:> AllPagesTable {:sortedPages (clj->js sorted-pages :keyword-fn str)
-                           :sortedBy @(rf/subscribe [:all-pages/sorted-by])
-                           :dateFormatFn #(dates/date-string %)
-                           :sortDirection (if @(rf/subscribe [:all-pages/sort-order-ascending?]) "asc" "desc")
-                           :onClickSort #(rf/dispatch [:all-pages/sort-by (cond
-                                                                            (= % "title") :title
-                                                                            (= % "links-count") :links-count
-                                                                            (= % "modified") :modified
-                                                                            (= % "created") :created)])
-                           :onClickItem (fn [e title]
-                                          (let [shift? (.-shiftKey e)]
-                                            (rf/dispatch [:reporting/navigation {:source :all-pages
-                                                                                 :target :page
-                                                                                 :pane   (if shift?
-                                                                                           :right-pane
-                                                                                           :main-pane)}])
-                                            (router/navigate-page title e)))}]))))
+  #_(defn page
+      []
+      (let [all-pages (common-db/get-all-pages @db/dsdb)]
+        (fn []
+          (let [sorted-pages @(rf/subscribe [:all-pages/sorted all-pages])]
+            [:> AllPagesTable {:sortedPages (clj->js sorted-pages :keyword-fn str)
+                               :sortedBy @(rf/subscribe [:all-pages/sorted-by])
+                               :dateFormatFn #(dates/date-string %)
+                               :sortDirection (if @(rf/subscribe [:all-pages/sort-order-ascending?]) "asc" "desc")
+                               :onClickSort #(rf/dispatch [:all-pages/sort-by (cond
+                                                                                (= % "title") :title
+                                                                                (= % "links-count") :links-count
+                                                                                (= % "modified") :modified
+                                                                                (= % "created") :created)])
+                               :onClickItem (fn [e title]
+                                              (let [shift? (.-shiftKey e)]
+                                                (rf/dispatch [:reporting/navigation {:source :all-pages
+                                                                                     :target :page
+                                                                                     :pane   (if shift?
+                                                                                               :right-pane
+                                                                                               :main-pane)}])
+                                                (router/navigate-page title e)))}])))))
