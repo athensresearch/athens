0a40a152ad614b347ce13182a18747da685dc191
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-06-26 17:36:47 -0400

    feat: can reorder kanban cards

--- src/js/components/KanbanBoard/KanbanBoard.tsx
+++ src/js/components/KanbanBoard/KanbanBoard.tsx
@@ -1,29 +1,61 @@
 import React from 'react';
 import { VStack, Grid, HStack, Box, Text, Heading } from '@chakra-ui/react';
+import { Reorder } from 'framer-motion';
 
 export const KanbanCard = (props) => {
-  const { children } = props;
-  return (
-    <Box draggable borderRadius="sm" minHeight="4rem" p={2} bg="background.floor">
-      <Text>{children}</Text>
-    </Box>
-  );
+  const { card, isSelected } = props;
+
+  return <Box
+    as={Reorder.Item}
+    key={card.toString()}
+    value={card}
+    borderRadius="sm"
+    minHeight="4rem"
+    listStyleType={"none"}
+    border="1px solid transparent"
+    p={2}
+    bg="background.floor"
+    _hover={{
+      bg: 'background.upper',
+      border: "1px solid",
+      borderColor: "background.floor"
+    }}
+  >
+    <Text>{card}</Text>
+  </Box>;
 }
 
 export const KanbanColumn = (props) => {
-  const { name, id, children } = props;
+  const { name } = props;
+  const [items, setItems] = React.useState(["Card 1", "Card 2", "Card 3"]);
+
   return (
-    <VStack draggable spacing={2} p={2} align="stretch" borderRadius="md" bg="background.upper">
-      <Heading color="foreground.secondary" size="sm">{name}</Heading>
-      {children}
-    </VStack>
+    <Box>
+      <VStack
+        as={Reorder.Group}
+        align="stretch"
+        listStyleType={"none"}
+        spacing={2}
+        p={2}
+        borderRadius="md"
+        bg="background.upper"
+        axis="y"
+        values={items}
+        onReorder={setItems}
+      >
+        <Heading color="foreground.secondary" size="sm">{name}</Heading>
+        {items.map((card) => (
+          <KanbanCard key={card.toString()} card={card} isSelected={false} />
+        ))}
+      </VStack>
+    </Box>
   );
 }
 
 export const KanbanSwimlane = (props) => {
   const { name, children } = props;
   return (
-    <VStack draggable align="stretch" borderRadius="md" py={2}>
+    <VStack align="stretch" borderRadius="md" py={2}>
       <Heading color="foreground.secondary" size="md">{name}</Heading>
       <Grid gap={2} p={0} templateRows="1" templateColumns="repeat(auto-fill, 15rem)" borderRadius="sm">
         {children}
