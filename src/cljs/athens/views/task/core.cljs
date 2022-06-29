9f44475aa7da140997c3da6cdf1932e24edad031
Alex Iwaniuk <alex@happyhacking.nl>
2022-06-29 18:05:34 +0200

    style: fix

--- src/cljs/athens/views/task/core.cljs
+++ src/cljs/athens/views/task/core.cljs
@@ -1,62 +1,63 @@
 (ns athens.views.task.core
-  (:require [athens.common-events.bfs :as bfs]
-            [athens.common.utils :as common.utils]
-            [athens.common-events.graph.composite :as composite]
-            [athens.common-db :as common-db]
-            [athens.db :as db]
-            [athens.common-events :as common-events]))
+  (:require
+    [athens.common-db :as common-db]
+    [athens.common-events :as common-events]
+    [athens.common-events.bfs :as bfs]
+    [athens.common-events.graph.composite :as composite]
+    [athens.common.utils :as common.utils]
+    [athens.db :as db]))
 
 
 ;; Create a new task
 (defn new-task
   [db block-uid position title description priority creator assignee due-date status project-relation]
   (->> (bfs/internal-representation->atomic-ops
-        db
-        [#:block{:uid        (common.utils/gen-block-uid)
-                 :string     ""
-                 :properties {":block/type"
-                              #:block{:string "athens/task"
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/title"
-                              #:block{:string title
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/description"
-                              #:block{:string description
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/priority"
-                              #:block{:string priority
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/creator"
-                              #:block{:string creator
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/assignee"
-                              #:block{:string assignee
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/due-date"
-                              #:block{:string due-date
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":task/status"
-                              #:block{:string status
-                                      :uid    (common.utils/gen-block-uid)}
-                              ":comment/project-relation"
-                              #:block{:string project-relation
-                                      :uid    (common.utils/gen-block-uid)}}}]
-        {:block/uid block-uid
-         :relation  position})
+         db
+         [#:block{:uid        (common.utils/gen-block-uid)
+                  :string     ""
+                  :properties {":block/type"
+                               #:block{:string "athens/task"
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/title"
+                               #:block{:string title
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/description"
+                               #:block{:string description
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/priority"
+                               #:block{:string priority
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/creator"
+                               #:block{:string creator
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/assignee"
+                               #:block{:string assignee
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/due-date"
+                               #:block{:string due-date
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":task/status"
+                               #:block{:string status
+                                       :uid    (common.utils/gen-block-uid)}
+                               ":comment/project-relation"
+                               #:block{:string project-relation
+                                       :uid    (common.utils/gen-block-uid)}}}]
+         {:block/uid block-uid
+          :relation  position})
        (composite/make-consequence-op {:op/type :new-type})))
-
 
 
 ;; Update the task properties
 (defn update-task-properties
   [db task-block-uid new-properties-map]
   (let [task-properties       (common-db/get-block-property-document @db/dsdb [:block/uid task-block-uid])
-        updated-properties    (merge task-properties new-properties-map)
         ops                   (concat
-                               (for [[prop-name prop-value] new-properties-map]
-                                 (let [[new-uid prop-ops] (graph-ops/build-property-path @db/dsdb task-block-uid [prop-name])
-                                       save-op            (graph-ops/build-block-save-op @db/dsdb new-uid prop-value)]
-                                   (conj prop-ops save-op))))
+                                (for [[prop-name prop-value] new-properties-map]
+                                  (let [[new-uid prop-ops] (graph-ops/build-property-path db task-block-uid [prop-name])
+                                        save-op            (graph-ops/build-block-save-op db new-uid prop-value)]
+                                    (if-not (= (get task-properties prop-name) prop-value)
+                                      (conj prop-ops save-op)
+                                      []))))
         updated-properties-op (composite/make-consequence-op {:op/type :update-task-properties}
                                                              ops)
         event                 (common-events/build-atomic-event updated-properties-op)]
