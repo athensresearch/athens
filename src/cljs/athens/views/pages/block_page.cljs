57ead8fb90ae56c94142360986fdea19654626b0
sid597 <siddharthdv77@gmail.com>
2022-07-09 14:54:38 +0530

    comments on block page

--- src/cljs/athens/views/pages/block_page.cljs
+++ src/cljs/athens/views/pages/block_page.cljs
@@ -2,15 +2,18 @@
   (:require
     ["/components/Page/Page" :refer [PageHeader PageBody PageFooter TitleContainer]]
     ["/components/References/References" :refer [PageReferences ReferenceBlock ReferenceGroup]]
-    ["@chakra-ui/react" :refer [Breadcrumb BreadcrumbItem BreadcrumbLink]]
+    ["@chakra-ui/react" :refer [Breadcrumb Box BreadcrumbItem BreadcrumbLink]]
     [athens.parse-renderer :as parse-renderer]
     [athens.reactive :as reactive]
     [athens.router :as router]
+    [athens.db :as db]
     [athens.views.blocks.core :as blocks]
     [athens.views.pages.node-page :as node-page]
     [komponentit.autosize :as autosize]
     [re-frame.core :as rf :refer [dispatch subscribe]]
-    [reagent.core :as r]))
+    [reagent.core :as r]
+    [athens.views.comments.inline :as inline-comments]
+    [athens.views.comments.core :as comments]))
 
 
 ;; Helpers
@@ -96,7 +99,7 @@
         (when (not= string (:string/previous @state))
           (swap! state assoc :string/previous string :string/local string))
 
-        [:<>
+        [:> Box
 
          ;; Header
          [:> PageHeader {:onClickOpenInMainView (when-not is-current-route?
@@ -132,11 +135,21 @@
              [:span [:wbr]]
              [parse-renderer/parse-and-render (:string/local @state) uid])]]
 
+         ;; Show comments when the toggle is on
+         [:> Box {:ml "4%"
+                  :w "100%"}
+          (when (or @(rf/subscribe [:comment/show-comment-textarea? uid])
+                    (and @(rf/subscribe [:comment/show-inline-comments?])
+                         (comments/get-comment-thread-uid @db/dsdb uid)))
+            [inline-comments/inline-comments (comments/get-comments-in-thread @db/dsdb (comments/get-comment-thread-uid @db/dsdb uid)) uid false])]
+
+
          ;; Children
          [:> PageBody
           (for [child children]
             (let [{:keys [db/id]} child]
               ^{:key id} [blocks/block-el child]))]
+
 
          ;; Refs
          [:> PageFooter
