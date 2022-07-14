255ef6c19caf2fc01c4b88d824a24bbab7011557
Filipe Silva <filipematossilva@gmail.com>
2022-07-14 15:23:43 +0100

    fix: ignore IDB errors from emoji-picker-element

--- src/js/components/EmojiPicker/EmojiPicker.tsx
+++ src/js/components/EmojiPicker/EmojiPicker.tsx
@@ -14,6 +14,19 @@ import {
 } from "@chakra-ui/react";
 import { PlusIcon } from '@/Icons/Icons'
 
+// Workaround for https://github.com/nolanlawson/emoji-picker-element/issues/220
+// Without this, the console gets spammed with uncaught errors when editing blocks.
+// I can reproduce it by creating several blocks, then clicking on the dev console, then
+// clicking on a block.
+// This is a bad workaround because it might cause it to ignore other IDB errors, but
+// we don't use IDB anywhere else for now.
+window.addEventListener("unhandledrejection", event => {
+  if (event.reason.message == "Failed to execute 'transaction' on 'IDBDatabase': The database connection is closing.") {
+    event.preventDefault();
+  }
+});
+
+
 interface EmojiPickerProps {
   onEmojiSelected: (event) => void;
 }
