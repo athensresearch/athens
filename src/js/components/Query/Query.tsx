a1c09364c1d35b42a7d241f1663787f9ca67073c
jeff <tangj1122@gmail.com>
2022-07-16 12:04:08 +0100

    viewicon

--- src/js/components/Query/Query.tsx
+++ src/js/components/Query/Query.tsx
@@ -10,18 +10,23 @@ import {
   MenuList,
   MenuOptionGroup
 } from "@chakra-ui/react";
-import { ChevronDownIcon } from '@/Icons/Icons';
+import { ChevronDownIcon, ViewIcon } from '@/Icons/Icons';
 
 export const Controls = (props) => {
     const { isCheckedFn, properties, hiddenProperties, onChange, menuOptionGroupValue } = props
   return (
     <Menu closeOnSelect={false}>
-      <MenuButton><Heading size="md" as={Button} rightIcon={<ChevronDownIcon/>}>Hidden Properties</Heading></MenuButton>
+      <MenuButton><Heading size="sm" as={Button} rightIcon={<ChevronDownIcon/>}>Hidden Properties</Heading></MenuButton>
       <MenuList>
         <MenuOptionGroup type="checkbox" onChange={(e) => console.log(e) } value={menuOptionGroupValue}>
           {properties.map((property) => {
             const isActive = hiddenProperties[property]
-            return <MenuItemOption color={isActive ? "gray" : "black"} value={property} onClick={(e) => onChange(e.target.innerHTML)}>{property}</MenuItemOption>
+            return <MenuItemOption
+                color={isActive ? "gray" : "black"}
+                icon={<ViewIcon/>}
+                value={property}
+                onClick={(e) => onChange(e.target.innerHTML)}>{property}
+            </MenuItemOption>
           })}
         </MenuOptionGroup>
       </MenuList>
