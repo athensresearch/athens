bf6f8d4ca9d658f23f5a66403200e5732e5eeba6
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-08-14 09:46:45 -0400

    feat: basic implementation of updated layout

--- src/cljs/athens/views.cljs
+++ src/cljs/athens/views.cljs
@@ -1,7 +1,10 @@
 (ns athens.views
   (:require
     ["/theme/theme" :refer [theme]]
-    ["@chakra-ui/react" :refer [ChakraProvider Flex Grid Spinner Center]]
+    ["/components/Layout/useLayoutState" :refer [LayoutProvider]]
+    ["/components/Layout/RightSidebar2" :refer [RightSidebar]]
+    ["/components/Layout/MainContent" :refer [MainContent]]
+    ["@chakra-ui/react" :refer [ChakraProvider Flex Grid VStack HStack Spinner Center]]
     [athens.config]
     [athens.electron.db-modal :as db-modal]
     [athens.style :refer [zoom]]
@@ -28,52 +31,42 @@
 
 (defn main
   []
-  (let [loading    (rf/subscribe [:loading?])
-        modal      (rf/subscribe [:modal])
+  (let [loading        (rf/subscribe [:loading?])
+        modal          (rf/subscribe [:modal])
         settings-open? (rf/subscribe [:settings/open?])]
     (fn []
       [:div (merge {:style {:display "contents"}}
                    (zoom))
        [:> ChakraProvider {:theme theme,
                            :bg "background.basement"}
-        [help-popup]
-        [alert]
-        [athena-component]
-        (cond
-          (and @loading @modal) [db-modal/window]
+        [:> LayoutProvider
+         [help-popup]
+         [alert]
+         [athena-component]
+         (cond
+           (and @loading @modal) [db-modal/window]
 
-          @loading
-          [:> Center {:height "100vh"}
-           [:> Flex {:width 28
-                     :flexDirection "column"
-                     :gap 2
-                     :color "foreground.secondary"
-                     :borderRadius "lg"
-                     :placeItems "center"
-                     :placeContent "center"
-                     :height 28}
-            [:> Spinner {:size "xl"}]]]
+           @loading
+           [:> Center {:height "100vh"}
+            [:> Flex {:width 28
+                      :flexDirection "column"
+                      :gap 2
+                      :color "foreground.secondary"
+                      :borderRadius "lg"
+                      :placeItems "center"
+                      :placeContent "center"
+                      :height 28}
+             [:> Spinner {:size "xl"}]]]
 
-          :else [:<>
-                 (when @modal
-                   [db-modal/window])
-                 (when @settings-open?
-                   [settings/page])
-
-                 [:> Grid
-                  {:gridTemplateColumns "auto 1fr auto"
-                   :gridTemplateRows "auto 1fr auto"
-                   :grid-template-areas
-                   "'app-header app-header app-header'
-                      'left-sidebar main-content secondary-content'"
-                   :height "100vh"
-                   :overflow "hidden"
-                   :sx {"WebkitAppRegion" "drag"
-                        "--app-toolbar-height" "3.25rem"
-                        ".os-mac &" {"--app-header-height" "52px"}
-                        ".os-windows &" {"--toolbar-height" "44px"}
-                        ".os-linux &" {"--toolbar-height" "44px"}}}
-                  [app-toolbar/app-toolbar]
-                  [left-sidebar/left-sidebar]
-                  [pages/view]
-                  [right-sidebar/right-sidebar]]])]])))
+           :else [:<>
+                  (when @modal
+                    [db-modal/window])
+                  (when @settings-open?
+                    [settings/page])
+                  [:> VStack {:spacing 0 :height "100vh" :align "stretch" :position "relative"}
+                   [app-toolbar/app-toolbar]
+                   [:> HStack {:align "stretch" :spacing 0 :flex 1}
+                    [left-sidebar/left-sidebar]
+                    [:> MainContent
+                     [pages/view]]
+                    [right-sidebar/right-sidebar]]]])]]])))
