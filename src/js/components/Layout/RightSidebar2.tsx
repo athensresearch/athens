33fcf551207a64efbe6269d6640376ee2254c00d
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-08-14 12:38:39 -0400

    chore: saving progress

--- src/js/components/Layout/RightSidebar2.tsx
+++ src/js/components/Layout/RightSidebar2.tsx
@@ -1,39 +1,91 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack, HStack } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import * as React from "react";
 import { AnimatePresence } from "framer-motion";
 import { LayoutContext, layoutAnimationProps } from "./useLayoutState";
 
+const MIN_WIDTH = 100;
+const MAX_WIDTH = 500;
+
 /** Right Sidebar */
-export const RightSidebar = () => {
+export const RightSidebar = ({ children, onResizeSidebar }) => {
   const {
     rightSidebarWidth,
+    setRightSidebarWidth,
     hasRightSidebar,
     toolbarHeight
   } = React.useContext(LayoutContext);
 
+  const [isDragging, setIsDragging] = React.useState(false);
+  const draggedWidth = React.useRef(0)
+
+  const defaultAnimationProps = layoutAnimationProps(rightSidebarWidth + "px");
+
+  const animationProps = {
+    ...defaultAnimationProps,
+    animate: {
+      ...defaultAnimationProps.animate,
+      transition: isDragging
+        ? { type: "tween", duration: 0 }
+        : defaultAnimationProps.animate.transition
+    }
+  }
+
   return (
     <AnimatePresence initial={false}>
       {hasRightSidebar && (
-        <Box
+        <HStack
           as={motion.div}
-          {...layoutAnimationProps(rightSidebarWidth)}
+          {...animationProps}
+          spacing={0}
           overflowY="auto"
           position="fixed"
           height="100vh"
-          borderLeft="1px solid"
-          borderColor="separator.border"
           inset={0}
           pt={`calc(${toolbarHeight} + 1rem)`}
           left="auto"
-          width={rightSidebarWidth}
         >
-          <VStack width={rightSidebarWidth}>
-            {new Array(5).fill(true).map((_, index) => (
-              <Text key={index}>Cool thing</Text>
-            ))}
-          </VStack>
-        </Box>
+          <Box
+            alignSelf="stretch"
+            zIndex={10}
+            width="5px"
+            background="white"
+            position="relative"
+            _hover={{
+              bg: "foreground.primary"
+            }}
+            bg={
+              isDragging ? "link" :
+                "separator.divider"
+            }
+          >
+            <motion.div
+              drag="x"
+              style={{
+                position: "absolute",
+                opacity: 0,
+                inset: 0
+              }}
+              dragElastic={false}
+              dragMomentum={false}
+              onDrag={
+                (event, info) => {
+                  if (!isDragging) { setIsDragging(true) };
+                  const width = window.innerWidth - info.point.x;
+                  if (width !== draggedWidth.current) {
+                    console.log(width)
+                    setRightSidebarWidth(width);
+                  }
+                  draggedWidth.current = width;
+                }
+              }
+              onDragEnd={() => setIsDragging(false)}
+            />
+          </Box>
+          <Box overflow="hidden" flex={1}>
+            {children}
+          </Box>
+        </HStack>
       )}
     </AnimatePresence>
   );
