0bea69dc63568381435a4b427ab2602629275b59
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-08-14 18:19:16 -0400

    fix: brighter notification badge

--- src/cljs/athens/views/notifications/popover.cljs
+++ src/cljs/athens/views/notifications/popover.cljs
@@ -132,7 +132,10 @@
                                               (when (.. e -shiftKey)
                                                 (rf/dispatch [:right-sidebar/open-page user-page-title])))
                              :icon          (r/as-element [:> BellFillIcon])}]
-             [:> Badge {:position "absolute" :right "-3px" :bottom "-1px" :variant "ghost" :z-index -1} num-notifications]]]
+             [:> Badge {:position "absolute"
+                        :bg "highlight"
+                        :color "highlightContrast"
+                        :right "-3px" :bottom "-1px" :variant "ghost" :zIndex 1} num-notifications]]]
 
 
            [:> PopoverContent {:maxWidth  "max-content"
