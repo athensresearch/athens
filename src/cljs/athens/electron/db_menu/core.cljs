7431c3631c736f3563a28a36438ab2e4a49ab861
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-08-19 09:10:57 -0400

    fix lint

--- src/cljs/athens/electron/db_menu/core.cljs
+++ src/cljs/athens/electron/db_menu/core.cljs
@@ -56,56 +56,56 @@
       ;; Dropdown menu
       [:> Portal
        [:> PopoverContent {:overflow-y "auto"}
+        [:> VStack {:align "stretch"
+                    :overflow "hidden"
+                    :spacing 0}
+         ;; Show active DB first
+         [:> Box {:bg "background.floor"
+                  :pb 4}
+          [db-list-item {:db active-db
+                         :is-current true
+                         :key (:id active-db)}]
+          [current-db-tools {:db active-db} all-dbs merge-open?]]
+         ;; Show all inactive DBs and a separator
+         [:> Heading {:fontSize "xs"
+                      :py 4
+                      :pb 3
+                      :borderTop "1px solid"
+                      :borderTopColor "separator.divider"
+                      :px 10
+                      :letterSpacing "wide"
+                      :textTransform "uppercase"
+                      :fontWeight "bold"
+                      :color "foreground.secondary"}
+          "Other databases"]
          [:> VStack {:align "stretch"
-                     :overflow "hidden"
-                     :spacing 0}
-          ;; Show active DB first
-          [:> Box {:bg "background.floor"
-                   :pb 4}
-           [db-list-item {:db active-db
-                          :is-current true
-                          :key (:id active-db)}]
-           [current-db-tools {:db active-db} all-dbs merge-open?]]
-          ;; Show all inactive DBs and a separator
-          [:> Heading {:fontSize "xs"
-                       :py 4
-                       :pb 3
-                       :borderTop "1px solid"
-                       :borderTopColor "separator.divider"
-                       :px 10
-                       :letterSpacing "wide"
-                       :textTransform "uppercase"
-                       :fontWeight "bold"
-                       :color "foreground.secondary"}
-           "Other databases"]
-          [:> VStack {:align "stretch"
-                      :position "relative"
-                      :spacing 0
-                      :overflow-y "auto"}
-           (doall
-             (for [[key db] inactive-dbs]
-               [db-list-item {:db db
-                              :is-disabled (= sync-status :synchronising)
-                              :is-current false
-                              :key key}]))
-           (when (= :synchronising sync-status)
-             [:> VStack {:align "center"
-                         :background "background.vibrancy"
-                         :backdropFilter "blur(0.25ch)"
-                         :justify "center"
-                         :position "absolute"
-                         :inset 0}
-              [:> Spinner]
-              [:> Text "Syncing..."]])]
-          ;; Add DB control
-          [:> ButtonGroup {:borderTop "1px solid"
-                           :borderTopColor "separator.divider"
-                           :p 2
-                           :pt 0
-                           :pl 10
-                           :size "sm"
-                           :width "100%"
-                           :ml 10
-                           :justifyContent "flex-start"}
-           [:> Button {:onClick #(dispatch [:modal/toggle])}
-            "Add Database"]]]]]]]))
+                     :position "relative"
+                     :spacing 0
+                     :overflow-y "auto"}
+          (doall
+            (for [[key db] inactive-dbs]
+              [db-list-item {:db db
+                             :is-disabled (= sync-status :synchronising)
+                             :is-current false
+                             :key key}]))
+          (when (= :synchronising sync-status)
+            [:> VStack {:align "center"
+                        :background "background.vibrancy"
+                        :backdropFilter "blur(0.25ch)"
+                        :justify "center"
+                        :position "absolute"
+                        :inset 0}
+             [:> Spinner]
+             [:> Text "Syncing..."]])]
+         ;; Add DB control
+         [:> ButtonGroup {:borderTop "1px solid"
+                          :borderTopColor "separator.divider"
+                          :p 2
+                          :pt 0
+                          :pl 10
+                          :size "sm"
+                          :width "100%"
+                          :ml 10
+                          :justifyContent "flex-start"}
+          [:> Button {:onClick #(dispatch [:modal/toggle])}
+           "Add Database"]]]]]]]))
