99b58e86c5937856b5bb035dd6a0dfe1cd03b83b
Stuart Hanberg <shanberg@users.noreply.github.com>
2022-09-19 19:04:08 -0400

    feat: restyle things shown in sidebar

--- src/js/components/Layout/RightSidebar.tsx
+++ src/js/components/Layout/RightSidebar.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { LayoutContext, layoutAnimationProps, layoutAnimationTransition } from "./useLayoutState";
 import { AnimatePresence, motion } from 'framer-motion';
 import { RightSidebarResizeControl } from "./RightSidebarResizeControl";
-import { XmarkIcon, ChevronRightIcon, PageIcon, PageFillIcon, BlockIcon, BlockFillIcon, GraphIcon, ArrowLeftOnBoxIcon } from '@/Icons/Icons';
-import { Button, IconButton, Box, Collapse, VStack, BoxProps } from '@chakra-ui/react';
+import { XmarkIcon, ChevronDownVariableIcon, ArrowLeftOnBoxIcon } from '@/Icons/Icons';
+import { Flex, ButtonGroup, Button, IconButton, Box, Collapse, VStack, BoxProps, Tooltip } from '@chakra-ui/react';
 import { useInView } from 'react-intersection-observer';
 
 /** Right Sidebar */
@@ -75,11 +75,11 @@ export const RightSidebar = (props: RightSidebarProps) => {
           />
           <Box
             pt={`calc(${toolbarHeight} + 1rem)`}
+            width={unsavedRightSidebarWidth + "vw"}
             overflowX="hidden"
             overflowY="auto"
             height="100%"
             position="relative"
-            width={unsavedRightSidebarWidth + "vw"}
           >
             <Box
               aria-hidden
@@ -96,102 +96,79 @@ export const RightSidebar = (props: RightSidebarProps) => {
   );
 };
 
-const typeIcon = (type, isOpen) => {
-  return isOpen ? { "page": <PageFillIcon />, "graph": <GraphIcon />, "block": <BlockFillIcon /> }[type]
-    : { "page": <PageIcon />, "graph": <GraphIcon />, "block": <BlockIcon /> }[type];
-};
-
-export const SidebarItem = ({ title, type, isOpen, onToggle, onRemove, onNavigate, children, ...props }) => {
+export const SidebarItem = ({ title, type, isOpen, onToggle, onRemove, onNavigate, children }) => {
   const className = { "page": "node-page", "block": "block-page", "graph": "graph-page" }[type];
   return (
     <VStack
+      layerStyle="card"
+      overflow="hidden"
       align="stretch"
       position="relative"
       spacing={0}
-      ml="1px" // Account for the floating separator
+      mx={4}
       sx={{
         "--page-padding": "0rem",
         "--page-left-gutter-width": "1em",
         "--page-right-gutter-width": "3em",
       }}
-      _notFirst={{
-        borderTop: "1px solid",
-        borderColor: "separator.divider"
-      }}>
-      <Box
-        top="-1px"
-        zIndex={2}
-        position="sticky"
-        background="background.floor"
-        display="grid"
-        gridTemplateColumns="1fr 3rem 3rem"
-        pr={2}
+      mt={2}>
+      <Flex
         alignItems="center"
         justifyContent="center"
       >
         <Button
           onClick={onToggle}
+          colorScheme="subtle"
+          justifyContent="flex-start"
+          variant="ghost"
+          size="sm"
           display="flex"
-          bg="transparent"
+          flex="1 1 100%"
           borderRadius="0"
           gap={2}
-          py={3}
-          pl={5}
-          pr={0}
+          p={2}
+          px={4}
           height="auto"
           textAlign="left"
           overflow="hidden"
           whiteSpace="nowrap"
-          sx={{ maskImage: "linear-gradient(to right, black, black calc(100% - 1rem), transparent calc(100%))" }}
-        >
-          {<ChevronRightIcon
-            transform={isOpen ? "rotate(90deg)" : null}
+          leftIcon={<ChevronDownVariableIcon
+            boxSize={4}
+            mr={0}
+            transform={isOpen ? undefined : "rotate(-90deg)"}
             transitionProperty="common"
             transitionDuration="0.15s"
             transitionTimingFunction="ease-in-out"
             justifySelf="center" />}
-          {typeIcon(type, isOpen)}
-          <Box
-            flex="1 1 100%"
-            tabIndex={-1}
-            pointerEvents="none"
-            position="relative"
-            bottom="1px"
-            overflow="hidden"
-            color="foreground.secondary"
-          >{title}</Box>
+        >
+          {title}
         </Button>
-        <IconButton
-          onClick={onNavigate}
-          size="sm"
-          color="foreground.secondary"
-          alignSelf="center"
-          justifySelf="center"
-          bg="transparent"
-          aria-label="Close"
+        <ButtonGroup
+          colorScheme="subtle"
+          variant="ghost"
+          size="xs"
+          position="absolute"
+          right={0}
+          top={0}
+          px={2}
+          py={1}
         >
-          <ArrowLeftOnBoxIcon />
-        </IconButton>
-        {/* <IconButton
-            size={"sm"}
-            alignSelf="center"
-            justifySelf={"center"}
-            bg="transparent"
-            aria-label="drag">
-          <DragIcon alignSelf={"center"} justifySelf={"center"}/>
-        </IconButton> */}
-        <IconButton
-          onClick={onRemove}
-          size="sm"
-          color="foreground.secondary"
-          alignSelf="center"
-          justifySelf="center"
-          bg="transparent"
-          aria-label="Close"
-        >
-          <XmarkIcon />
-        </IconButton>
-      </Box>
+          <Tooltip label="Open">
+            <IconButton
+              onClick={onNavigate}
+              aria-label="Close"
+              icon={<ArrowLeftOnBoxIcon />}
+            />
+          </Tooltip>
+          <Tooltip label="Remove from Sidebar">
+            <IconButton
+              onClick={onRemove}
+              aria-label="Close"
+              icon={<XmarkIcon />}
+            />
+          </Tooltip>
+        </ButtonGroup>
+      </Flex>
       <Box
         as={Collapse}
         in={isOpen}
@@ -204,6 +181,6 @@ export const SidebarItem = ({ title, type, isOpen, onToggle, onRemove, onNavigat
       >
         {children}
       </Box>
-    </VStack>
+    </VStack >
   );
 };
