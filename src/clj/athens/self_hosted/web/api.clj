8914d37c9aeb3b943686045d9ed6f79b1ab4289b
Filipe Silva <filipematossilva@gmail.com>
2022-09-24 19:07:22 +0100

    feat: add basic content negotiation to api

--- src/clj/athens/self_hosted/web/api.clj
+++ src/clj/athens/self_hosted/web/api.clj
@@ -1,14 +1,13 @@
 (ns athens.self-hosted.web.api
   (:require
-    [athens.common-db :as common-db]
-    [athens.common-events :as common-events]
-    [athens.common-events.graph.atomic :as atomic]
-    [athens.common.utils :as utils]
-    [athens.self-hosted.web.datascript :as web.datascript]
-    [clojure.edn :as edn]
-    [clojure.pprint :as pp]
-    [compojure.core :as c]
-    [ring.util.request :as rr]))
+   [athens.common-db :as common-db]
+   [athens.common-events :as common-events]
+   [athens.common-events.graph.atomic :as atomic]
+   [athens.common.utils :as utils]
+   [athens.self-hosted.web.datascript :as web.datascript]
+   [clojure.pprint :as pp]
+   [compojure.core :as c]
+   [muuntaja.middleware :as muuntaja.mw]))
 
 ;; TODO
 ;; how content negotiation
@@ -51,20 +50,6 @@
 
 ;; Middleware
 
-(defn wrap-body-str
-  [handler]
-  (fn [request]
-    (let [body-str (rr/body-string request)]
-      (-> request (assoc :body-str body-str) handler))))
-
-
-(defn wrap-arg-param
-  [handler]
-  (fn [request]
-    (let [arg (-> request :body-str edn/read-string)]
-      (-> request (assoc-in [:params :arg] arg) handler))))
-
-
 (defn wrap-get-block-by-uid
   [conn handler]
   (fn [request]
@@ -77,73 +62,77 @@
     (->> request handler (get-page-by-title conn))))
 
 
-(defn wrap-str-resp
-  [handler]
-  (fn [request]
-    (-> request handler str-resp)))
-
-
 ;; Routes with inline handlers.
 
 (defn make-routes
-  [datascript fluree in-memory? _server-password]
+  [datascript fluree config]
   (let [conn        (:conn datascript)
-        event-exec! (partial web.datascript/exec! conn fluree in-memory?)
+        ;; password    (-> config :config :password)
+        event-exec! (partial web.datascript/exec! conn fluree config)
+        ;; TODO: need to change something in make-receive-handler to send stuff in as an event, and broadcast to all
         exec!       (fn [op] (-> op common-events/build-atomic-event event-exec!))]
     (->
-      (c/routes
+     (c/routes
+      (c/context
+       "/api" []
+
+       ;; Import block/page representation.
+       ;; TODO: WIP because we don't have a wire format for the block loc body-params
+       #_(c/context
+          "/import" []
+          (c/POST "/pages" [] "pages IR")
+          (c/POST "/blocks" [] "blocks IR + loc"))
+
+       ;; Block
+       ;; TODO: allow block API after when we need more than the IR endpoint
+       (->>
         (c/context
-          "/api" []
+         "/block" []
+         (c/POST "/"       [body-params] (let [uid (utils/gen-block-uid)]
+                                           (->> body-params (atomic/make-block-new-op uid) exec! (ret uid))))
+         (c/context
+          "/:uid" [uid]
+          (c/GET  "/"     []    uid)
+          (c/DELETE "/"   []    (->> uid atomic/make-block-remove-op exec! (ret nil)))
+          (c/POST "/"     [body-params] (->> body-params (atomic/make-block-new-op uid) exec! (ret uid)))
+          (c/POST "/save" [body-params] (->> body-params (atomic/make-block-save-op uid) exec! (ret uid)))
+          (c/POST "/open" [body-params] (->> body-params (not= "false") (atomic/make-block-open-op uid) exec! (ret uid)))
+          (c/POST "/move" [body-params] (->> body-params (atomic/make-block-move-op uid) exec! (ret uid)))))
+        ;; Return a block representation for all block calls.
+        (partial wrap-get-block-by-uid conn))
 
-          ;; Import block/page representation.
-          ;; TODO: WIP because we don't have a wire format for the block loc arg
-          #_(c/context
-            "/import" []
-            (c/POST "/pages" [] "pages IR")
-            (c/POST "/blocks" [] "blocks IR + loc"))
+       ;; Page
+       ;; TODO: allow after page API seems solid
+       #_(->
+          (c/context
+           "/page" []
+           (c/GET "/"          []    (common-db/get-all-page-titles @conn))
+           (c/context
+            "/:title" [title] ; is title is url-encoded? add middleware if not, or helper
+            (c/GET "/"        []    title)
+            (c/DELETE "/"     []    (->> title atomic/make-page-remove-op exec! (ret nil)))
+            (c/POST "/"       []    (->> title atomic/make-page-new-op exec! (ret title)))
+            (c/POST "/rename" [body-params] (->> body-params (atomic/make-page-rename-op title) exec! (ret title))) ;; TODO: new title
+            (c/POST "/merge"  [body-params] (->> body-params (atomic/make-page-merge-op title) exec! (ret title)))))
+          ;; Return a page representation for all page calls.
+          (partial wrap-get-page-by-title conn))
 
-          ;; Block
-          (->>
-            (c/context
-              "/block" []
-              (c/POST "/"       [arg] (let [uid (utils/gen-block-uid)]
-                                        (->> arg (atomic/make-block-new-op uid) exec! (ret uid))))
-              (c/context
-                "/:uid" [uid]
-                (c/GET  "/"     []    uid)
-                (c/DELETE "/"   []    (->> uid atomic/make-block-remove-op exec! (ret nil)))
-                (c/POST "/"     [arg] (->> arg (atomic/make-block-new-op uid) exec! (ret uid)))
-                (c/POST "/save" [arg] (->> arg (atomic/make-block-save-op uid) exec! (ret uid)))
-                (c/POST "/open" [arg] (->> arg (not= "false") (atomic/make-block-open-op uid) exec! (ret uid)))
-                (c/POST "/move" [arg] (->> arg (atomic/make-block-move-op uid) exec! (ret uid)))))
-            ;; Return a block representation for all block calls.
-            (partial wrap-get-block-by-uid conn))
+       ;; Shortcut
+       ;; TODO: WIP because we don't really have a representation for shortcuts, and we might convert them into props.
+       #_(c/context
+          "/shortcut/:title" [title]
+          (c/DELETE "/" [] ":shortcut/remove")
+          (c/POST "/" [] ":shortcut/new")
+          (c/POST "/move" [] ":shortcut/move"))
 
-          ;; Page
-          (->
-            (c/context
-              "/page" []
-              (c/GET "/"          []    (common-db/get-all-page-titles @conn))
-              (c/context
-                "/:title" [title] ; is title is url-encoded? add middleware if not, or helper
-                (c/GET "/"        []    title)
-                (c/DELETE "/"     []    (->> title atomic/make-page-remove-op exec! (ret nil)))
-                (c/POST "/"       []    (->> title atomic/make-page-new-op exec! (ret title)))
-                (c/POST "/rename" [arg] (->> arg (atomic/make-page-rename-op title) exec! (ret title)))
-                (c/POST "/merge"  [arg] (->> arg (atomic/make-page-merge-op title) exec! (ret title)))))
-            ;; Return a page representation for all page calls.
-            (partial wrap-get-page-by-title conn))
+       ;; Raw operation.
+       #_(c/POST "/op" [body-params] (-> body-params exec! (ret {})))))
+     muuntaja.mw/wrap-format)))
 
-          ;; Shortcut
-          ;; TODO: WIP because we don't really have a representation for shortcuts.
-          #_(c/context
-            "/shortcut/:title" [title]
-            (c/DELETE "/" [] ":shortcut/remove")
-            (c/POST "/" [] ":shortcut/new")
-            (c/POST "/move" [] ":shortcut/move"))
 
-          ;; Raw operation.
-          (c/POST "/op" [arg] (-> arg exec! (ret {})))))
-      wrap-body-str
-      wrap-arg-param
-      wrap-str-resp)))
+;; curl examples
+;; curl localhost:3010/api/block/ee770c334
+;; curl -X POST localhost:3010/api/block -d '{:block/uid "ee770c334" :relation :last}'
+;; curl localhost:3010/api/block/ee770c334
+;; curl -X POST localhost:3010/api/block -d '{:block/uid "ee770c334" :relation :last}'
+;; curl localhost:3010/api/block/ee770c334
