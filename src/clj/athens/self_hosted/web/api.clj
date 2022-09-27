cc246e25304546eeb238269dc515634824d3c898
Filipe Silva <filipematossilva@gmail.com>
2022-09-27 12:30:28 +0100

    rfct: api based on path read/write

--- src/clj/athens/self_hosted/web/api.clj
+++ src/clj/athens/self_hosted/web/api.clj
@@ -4,37 +4,80 @@
     [athens.common-events :as common-events]
     [athens.common-events.bfs :as bfs]
     [athens.common-events.graph.composite :as composite-ops]
+    [athens.common-events.schema :as schema]
     [athens.self-hosted.clients :as clients]
     [athens.self-hosted.web.datascript :as web.datascript]
+    [clojure.set :as set]
     [compojure.core :as c]
     [muuntaja.middleware :as muuntaja.mw]))
 
 
 ;; Helpers
 
-(defn get-block-by-uid
-  [conn uid]
-  (if-not uid
-    {}
-    (common-db/get-internal-representation @conn [:block/uid uid])))
+(def fragment-k->eid-k
+  {:page/title :node/title
+   :block/uid  :block/uid})
 
 
-;; Only supports paths consisting of a single page title as string.
-;; TODO: support starting with uid, exact child string content, properties, today as path fragments.
-;; Will need work on graph-ops/build-path to:
-;; - create page if first fragment is eid for page and does not exist
-;; - find next block child by exact content
-;; We might want to add support to paths in atomic ops, but for now can experiment here.
-;; NB: quick capture needs paths too.
-(defn add
-  [conn [k] data]
-  (->> (bfs/internal-representation->atomic-ops @conn data {:page/title k :relation :last})
-       (composite-ops/make-consequence-op {:op/type :graph/add-in})
-       common-events/build-atomic-event))
+(defn fragment->eid
+  [x]
+  (-> x
+      (set/rename-keys fragment-k->eid-k)
+      (select-keys (vals fragment-k->eid-k))
+      vec
+      first))
+
+
+(def eid-k-map
+  {:node/title :page/title
+   :block/uid :block/uid})
+
+
+(defn eid->position-id
+  [[k v]]
+  [(eid-k-map k) v])
+
+
+;; Different from graph.ops/build-path, paths are [eid selectors...]
+;; TODO: support order/prop selectors
+(defn path->eid
+  [_db [x & _selectors]]
+  (fragment->eid x))
+
+
+;; Path examples:
+;; [{:page/title "page"}]
+;; [{:block/uid "uid"}]
+(defn read-path
+  [conn path]
+  (let [db @conn]
+    (->> path
+         (path->eid db)
+         (common-db/get-internal-representation db))))
+
+
+;; TODO: follow/create selectors
+(defn create-path
+  [_db [x & _selectors]]
+  [(fragment->eid x) []])
+
+
+(defn write-in-path-evt
+  [conn path relation data]
+  (let [db             @conn
+        [eid path-ops] (create-path db path)
+        default-pos    (into {:relation (or relation :last)}
+                             [(eid->position-id eid)])
+        write-ops      (bfs/internal-representation->atomic-ops db data default-pos)]
+    (->> (into path-ops write-ops)
+         (composite-ops/make-consequence-op {:op/type :path/write})
+         common-events/build-atomic-event)))
 
 
 (defn process-event!
   [datascript fluree config evt]
+  (when-not (schema/valid-event? evt)
+    (throw (ex-info "Invalid event" (schema/explain-event evt))))
   (when (->> evt
              (web.datascript/exec! datascript fluree config)
              :event/status
@@ -49,33 +92,47 @@
    :body x})
 
 
+;; for convenience with ->>
+(defn ret-first
+  [x _]
+  x)
+
+
 ;; Routes with inline handlers.
 
 (defn make-routes
   [datascript fluree config]
-  (let [conn        (:conn datascript)
+  (let [conn (:conn datascript)
+        ;; TODO: only if config ff is on
         ;; TODO: handle pw
         ;; password    (-> config :config :password)
         ]
+
     (->
       (c/routes
         (c/context
-          "/api" []
-
-          (c/GET
-            "/block/:uid" [uid]
-            (->> uid (get-block-by-uid conn) ok))
+          "/api/path" []
 
           (c/POST
-            "/add" {{:keys [path data]} :body-params}
-            (->> (add conn path data)
+            "/read" {{:keys [path]} :body-params}
+            (->> path
+                 (read-path conn)
+                 ok))
+
+          (c/POST
+            "/write" {{:keys [path relation data]} :body-params}
+            (->> (write-in-path-evt conn path relation data)
                  (process-event! datascript fluree config)
+                 (ret-first path)
+                 (read-path conn)
                  ok))))
       muuntaja.mw/wrap-format)))
 
 
 ;; curl examples
-;; curl localhost:3010/api/block/208a0c787
-;; curl -H "Content-Type: application/edn" -H "Accept: application/edn" localhost:3010/api/block/208a0c787
-;; curl -H "Content-Type: application/edn" -H "Accept: application/edn" -X POST localhost:3010/api/add -d '{:path ["page"] :data [{:block/string "one" :block/children [{:block/string "two"}]}]}'
-;; curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST localhost:3010/api/add -d '{"path":["page"],"data":[{"block/string":"one", "block/children":[{"block/string":"two"}]}]}'
+;; read
+;; curl -H "Content-Type: application/edn" -H "Accept: application/edn" -X POST localhost:3010/api/path/read -d '{:path [{:page/title "page"}]}'
+;; curl -H "Content-Type: application/json" -X POST localhost:3010/api/path/read -d '{"path":[{"page/title":"page"}]}'
+;; write
+;; curl -H "Content-Type: application/edn" -H "Accept: application/edn" -X POST localhost:3010/api/path/write -d '{:path [{:page/title "page"}] :data [{:block/string "one" :block/children [{:block/string "two"}]}]}'
+;; curl -H "Content-Type: application/json" -X POST localhost:3010/api/path/write -d '{"path":[{"page/title":"page"}], "data":[{"block/string":"one", "block/children":[{"block/string":"two"}]}]}'
