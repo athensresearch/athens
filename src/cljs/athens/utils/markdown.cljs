6169e9484cc6d37fbc19f8fdf4152b534edad9b9
sid597 <siddharthdv77@gmail.com>
2022-10-06 08:43:40 +0530

    style fix, now previous pr should be deployed

--- src/cljs/athens/utils/markdown.cljs
+++ src/cljs/athens/utils/markdown.cljs
@@ -1,12 +1,16 @@
 (ns athens.utils.markdown
-  (:require ["turndown" :as turndown]))
+  (:require
+    ["turndown" :as turndown]))
+
 
 (set! (.-escape (.-prototype turndown)) (fn [string] string))
+
 
 (defonce turndown-instance
   (new turndown))
 
-(defn html->md 
+
+(defn html->md
   "Transforms text to markdown."
   [text]
   (.turndown turndown-instance text))
